e37374cde8647f88ea7980176139ba5028e2b1cf
oeyoews <jyao4783@gmail.com>
2022-10-11 14:02:15 +0800

    Automatic commit

--- neotw.config.mk
+++ neotw.config.mk
@@ -41,7 +41,7 @@ Lib := lib
 CommitId := $(shell git rev-parse --short HEAD && git show --pretty=format:"%ci %cr" | head -1 && git branch)
 LongCommitId := $(shell git rev-parse HEAD)
 ShortCommitId := $(shell git rev-parse --short HEAD)
-TiddlyWiki-Git-TemplateFile := "tiddlers/neotw/git/commit-template.tid"
+TiddlyWiki-Git-TemplateFile := "src/templates/commit.tid"
 TiddlyWiki-Git-File := "tiddlers/neotw/git/commit.tid"
 
 PROJECT      := $(call GetFromPkg,name)
