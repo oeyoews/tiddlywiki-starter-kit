e23654acccb4717f59e80e14b78c75aeef7a339f
oeyoews <jyao4783@gmail.com>
2022-11-15 03:03:03 +0800

    Automatic commit

--- scripts/new-plugin.mjs
+++ scripts/new-plugin.mjs
@@ -1,7 +1,9 @@
 #!/usr/bin/env zx
 
 import { spinner } from "zx/experimental";
+// const prompts = require("prompts");
 
+console.log(chalk.green("Create new plugin"));
 const inputName = await question("Please input your new plugin name:");
 const description = await question("Please input your new plugin description:");
 
@@ -10,7 +12,10 @@ const template = "templates/new-plugin";
 const target = "dev/plugins/" + dirname;
 
 await spinner("Loading ...", async () => {
-  await $`cp -r ${template} ${target}`;
+  // await $`cp -r ${template} ${target}`;
+  await $`echo HEllo`;
   // await $`sed -i "s#PluginName#${inputName}#g" ${target}/**.tid`;
   // maybe use js to replace sed
 });
+
+console.log(chalk.green("ending"));
