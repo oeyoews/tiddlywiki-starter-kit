4a4d445381844fb583ac84cd5fd01b5e5d71dedb
oeyoews <jyao4783@gmail.com>
2022-11-15 10:34:27 +0800

    feat: update mjs file

--- scripts/new-plugin.mjs
+++ scripts/new-plugin.mjs
@@ -1,21 +1,69 @@
 #!/usr/bin/env zx
 
+// import "zx/globals";
+
+// https://github.com/terkelg/prompts#readme
 import { spinner } from "zx/experimental";
-// const prompts = require("prompts");
+const prompts = require("prompts");
+const timestamp = new Date().getTime();
 
-console.log(chalk.green("Create new plugin"));
-const inputName = await question("Please input your new plugin name:");
-const description = await question("Please input your new plugin description:");
+const questions = [
+  {
+    type: "text",
+    name: "dirname", // variable
+    message: "create dirname",
+    initial: "PluginName-" + timestamp,
+  },
+  {
+    type: "text",
+    name: "description", // variable
+    message: "description",
+    initial: "description",
+  },
+  {
+    type: "select",
+    name: "PluginName",
+    message: "Which part do you want to bump? ",
+    choices: [
+      {
+        title: "newPluginName1: ",
+        description: "This option has a description",
+      },
+      {
+        title: "Red",
+        description: "This option has a description",
+        value: "#ff0000",
+      },
+      { title: "Green", value: "#00ff00", disabled: true },
+    ],
+  },
+  {
+    type: "toggle",
+    name: "newPluginStatus",
+    message: "Can you confirm?",
+    initial: false,
+    active: "yes",
+    inactive: "no",
+  },
+];
 
-const dirname = inputName || "PluginName";
+// clean
+await $`rm -rf dev/plugins/PluginName*`;
+
+const response = await prompts(questions);
+
 const template = "templates/new-plugin";
-const target = "dev/plugins/" + dirname;
+const target = "dev/plugins/" + response.dirname;
 
-await spinner("Loading ...", async () => {
-  // await $`cp -r ${template} ${target}`;
-  await $`echo HEllo`;
+const newPluginDir = await spinner("Loading ...", async () => {
+  await $`mkdir ${target} && cp -r ${template}/* ${target} || exit`;
+  await $`sleep 1`;
   // await $`sed -i "s#PluginName#${inputName}#g" ${target}/**.tid`;
-  // maybe use js to replace sed
 });
 
-console.log(chalk.green("ending"));
+if (response.newPluginStatus) {
+  newPluginDir;
+  console.log(chalk.green("finished"));
+} else {
+  console.log(chalk.yellow("nothing to do"));
+}
