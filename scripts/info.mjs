781266c950c384e8f5ecdd5ba7b3637e2b409d0a
oeyoews <jyao4783@gmail.com>
2022-11-17 14:37:47 +0800

    Automatic commit

--- scripts/info.mjs
+++ scripts/info.mjs
@@ -1,21 +1,11 @@
 #!/usr/bin/env zx
 
-import { cyan, blue, yellow, bold, dim, green } from "kolorist";
-const signale = require("signale");
-
+import { cyan, blue, yellow } from "kolorist";
 const { description, version, name } = require("../package.json");
 const log = console.log;
 
-const platform = os.platform(); // linux
-const userInfo = os.userInfo(); // init
-const username = userInfo.username; // xxx
-
-log(`${cyan("●") + blue("■") + yellow("▲")}`);
-signale.complete({
-  prefix: "[Info]",
-  message: "Neotw Info",
-  suffix: "(@oeyoews)",
-});
-// console.log(chalk.green(name, version, platform, username));
-log(chalk.blue.underline.bold(`Descripion:`) + ` ${description}`);
-log(chalk.blue.underline.bold(`Version:`) + ` ${version}`);
+log(
+  `${cyan("●") + blue("■") + yellow("▲")}` +
+    chalk.blue.bold(` ${name}:`) +
+    chalk.blue.cyan(` ${description}(${version}):`)
+);
