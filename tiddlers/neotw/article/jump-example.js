5a048e5d8cbae678bf08d30770e0d88c6fac3cb3
oeyoews <jyao4783@gmail.com>
2022-11-18 18:42:45 +0800

    Automatic commit

--- tiddlers/neotw/article/jump-example.js
+++ tiddlers/neotw/article/jump-example.js
@@ -2,7 +2,7 @@
 document.onkeydown = function (event) {
         var e = event || window.event || arguments.callee.caller.arguments[0];
         if (e.keyCode == 76 && e.ctrlKey) {
-          // document.getElementById("message_div").innerHTML = "你按下了ctrl+l,开发的时候可以跳到一个锁屏的页面";
+          // document.getElementById("message_div").innerHTML = "Hello, Neotw";
            window.location.href = './example.html';
           return false;
         }
