ab9fa06c3d06a9e7d74ff40d7ead4d9b16b30391
oeyoews <jyao4783@gmail.com>
2022-11-26 00:59:16 +0800

    Automatic commit

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -2,24 +2,25 @@
 
 // question event
 
-import prompts from 'prompts'
-import build from './build.mjs'
-import start from './start.mjs'
+import prompts from "prompts";
+import build from "./build.mjs";
+import start from "./start.mjs";
 
-const questions = [
-  {
-    type: 'select',
-    name: 'guide',
-    message: 'guide',
-    choices: ['build', 'start'].map((i) => ({ value: i, title: i })),
-    initial: 0
-  }
-]
+// WIP TODO
+// const questions = [
+//   {
+//     type: "select",
+//     name: "guide",
+//     message: "guide",
+//     choices: ["build", "start"].map((i) => ({ value: i, title: i })),
+//     initial: 0,
+//   },
+// ];
 
-const response = await prompts(questions)
+// const response = await prompts(questions);
 
-function main (callback) {
-  callback()
-}
+// function main(callback) {
+//   callback();
+// }
 
-main(response.guide)
+// main(response.build);
