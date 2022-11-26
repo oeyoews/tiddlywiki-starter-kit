ab9a42e886c30ac2a347b6e0ec08ac30a42258f9
oeyoews <jyao4783@gmail.com>
2022-11-26 10:18:06 +0800

    feat: new index.mjs

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,26 +1,33 @@
 #!/usr/bin/env zx
 
-// question event
+$.verbose = false;
 
 import prompts from "prompts";
-import build from "./build.mjs";
-import start from "./start.mjs";
+import index from "./main.mjs";
+
+const choicesNeotw = ["service", "build", "start"];
 
 // WIP TODO
-// const questions = [
-//   {
-//     type: "select",
-//     name: "guide",
-//     message: "guide",
-//     choices: ["build", "start"].map((i) => ({ value: i, title: i })),
-//     initial: 0,
-//   },
-// ];
+const questions = [
+  {
+    type: "select",
+    name: "guide",
+    message: "guide",
+    choices: choicesNeotw.map((i) => ({ value: i, title: i })),
+    initial: 0,
+  },
+];
 
-// const response = await prompts(questions);
+const response = await prompts(questions);
+const fn = response.guide;
 
-// function main(callback) {
-//   callback();
-// }
+// user sure
+/**
+ * @param {any} callback
+ */
+function main(callback) {
+  index[callback]();
+}
 
-// main(response.build);
+/*  */
+main(fn);
