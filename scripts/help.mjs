c43e68540220e415d89be08f259ef8fcfb95807d
oeyoews <jyao4783@gmail.com>
2022-11-29 23:00:52 +0800

    Automatic commit

--- scripts/help.mjs
+++ scripts/help.mjs
@@ -1,1 +1,11 @@
 #!/usr/bin/env zx
+
+function exitWithError(errorMessage) {
+  console.error(chalk.red(errorMessage));
+  process.exit(1);
+}
+
+let targetDirectory = argv.directory;
+if (!targetDirectory) {
+  exitWithError("Error: You must specify the --directory argument");
+}
