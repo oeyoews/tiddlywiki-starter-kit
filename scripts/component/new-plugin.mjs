eed2be0d6ad111a2a4dd4e47d01060c0142bd44d
oeyoews <jyao4783@gmail.com>
2023-02-04 16:04:08 +0800

    fix: remove initial plugin description

--- scripts/component/new-plugin.mjs
+++ scripts/component/new-plugin.mjs
@@ -20,7 +20,7 @@ export default async function newPlugin() {
       type: 'text',
       name: 'description', // variable
       message: 'plugin description', // not support sed space
-      initial: 'plugin description',
+      initial: '',
     },
     {
       type: 'toggle',
