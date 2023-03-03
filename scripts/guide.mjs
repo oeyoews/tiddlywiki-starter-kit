3e01a6ac28b9e74a4f4e1b71211d223a552a774e
oeyoews <jyao4783@gmail.com>
2023-03-03 22:26:50 +0800

    Automatic commit

--- scripts/guide.mjs
+++ scripts/guide.mjs
@@ -2,22 +2,12 @@
 
 import prompts from 'prompts';
 import msg from './lib/info.mjs';
-// import service from './component/service.mjs';
-// import build from './component/build.mjs';
-// import start from './component/start.mjs';
-// import bump from './component/bump.mjs';
 import newPlugin from './component/new-plugin.mjs';
-// import clone from './component/clone.mjs';
 
 // $.verbose = false;
 
 const index = {
-  // service,
-  // build,
-  // start,
-  // bump,
   newPlugin,
-  // clone,
 };
 
 // from ob to arroy
