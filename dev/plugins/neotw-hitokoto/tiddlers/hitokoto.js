caed1d082b861c14f4f92005e785eb230d40c103
oeyoews <jyao4783@gmail.com>
2023-03-04 12:16:04 +0800

    feat: update hitokoto
    support embed to tiddlers

--- dev/plugins/neotw-hitokoto/tiddlers/hitokoto.js
+++ dev/plugins/neotw-hitokoto/tiddlers/hitokoto.js
@@ -1,20 +1,26 @@
-function hitokoto_footer() {
+function hitokoto_footer(text = '#htext', author = '#hauthor') {
   fetch('https://v1.hitokoto.cn')
     .then(response => response.json())
     .then(data => {
-      const hitokoto = document.querySelector('#h-text');
+      const hitokoto = document.querySelector(text);
       let data_ = data.hitokoto;
-      // if(data_.length > 22){
-      // data_ = data_.substring(0,22);
+      // if(data_.length > 28){
+      // data_ = data_.substring(0,28);
       // data_ = data_ + "…";
       // }
       hitokoto.innerText = data_;
-      const hitokoto_from = document.querySelector('#h-author');
+      const hitokoto_from = document.querySelector(author);
       // hitokoto_from.href = "https://hitokoto.cn/?uuid=" + data.uuid;
       hitokoto_from.innerText = '@' + data.from;
     })
     .catch(console.error);
 }
 
+const htext_example = '#htext-example';
+const hauthor_example = '#hauthor-example';
+
 hitokoto_footer();
-setInterval(hitokoto_footer, 30000);
+setInterval('hitokoto_footer()', 30000);
+
+hitokoto_footer(htext_example, hauthor_example);
+setInterval('hitokoto_footer(htext_example, hauthor_example)', 30000);
