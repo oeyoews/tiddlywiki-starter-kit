32f9e7bc315763615065601fdd1d2a0e24ab4801
oeyoews <jyao4783@gmail.com>
2023-03-05 22:36:04 +0800

    Automatic commit

--- dev/plugins/tiddlywiki-giscus/tiddlers/core/giscus/widget-giscus.js
+++ dev/plugins/tiddlywiki-giscus/tiddlers/core/giscus/widget-giscus.js
@@ -1,16 +1,6 @@
 (function () {
   'use strict';
 
-  // tmp settings
-  // var usernameTiddler = "neotw/username";
-  // var username = $tw.wiki.getTiddlerText(usernameTiddler);
-  // if(username !== "oeyoews")
-  // {
-  //   alert("This giscus tiddlywiki plugin just for oeyoews build after version 0.7.0")
-  //   console.error("This giscus tiddlywiki plugin just for oeyoews build after version 0.7.0")
-  //   return;
-  // }
-
   var Widget = require('$:/core/modules/widgets/widget.js').widget;
   var GiscusNodeWidget = function (parseTreeNode, options) {
     this.initialise(parseTreeNode, options);
@@ -19,6 +9,7 @@
   var giscusConfigTiddler = '$:/plugins/oeyoews/tiddlywiki-giscus/config';
   var giscusConfigTiddlerGet = $tw.wiki.getTiddler(giscusConfigTiddler);
   var config = giscusConfigTiddlerGet ? giscusConfigTiddlerGet.fields : {};
+
   GiscusNodeWidget.prototype = new Widget();
   GiscusNodeWidget.prototype.render = function (parent, nextSibling) {
     this.parentDomNode = parent;
@@ -31,8 +22,22 @@
       console.log(`💎 请传入正确的评论区id`);
       return;
     }
-    // TODO 重复唤起
-    console.log(`💎 当前评论区为 ${id}`);
+    // 如果已经存在相同 id 的评论区，直接返回
+    var existingCommentNode = this.document.querySelector(
+      '.oeyoews-giscus[tiddler-title="' + id.replace('"', '\\"') + '"]',
+    );
+    if (existingCommentNode) {
+      console.warn(`💎 评论区 "${id}" 已存在`);
+      // 如果已经存在评论区，则在插件所在的位置添加一个 span 元素，内容为警告信息
+      var warnNode = this.document.createElement('center');
+      warnNode.className = 'o-giscus-warn';
+      warnNode.textContent = '此评论区已存在，请勿重复渲染！';
+      parent.insertBefore(warnNode, nextSibling);
+      this.domNodes.push(warnNode);
+      console.warn('此评论区已存在，请勿重复渲染！');
+      return;
+    }
+    console.log(`💎 当前评论区为 "${id}"`);
     var scriptNode = this.document.createElement('script');
     scriptNode.setAttribute('src', 'https://giscus.app/client.js');
     var { repo, repoId, categoryId } = config;
@@ -51,22 +56,18 @@
     scriptNode.setAttribute('data-lang', lang);
     scriptNode.setAttribute('crossorigin', 'anonymous');
     scriptNode.setAttribute('async', 'true');
-    // Clear other comment nodes' giscus class
+    // 清除其他评论区节点的 giscus class
     var commentNodes = this.document.querySelectorAll('.giscus');
     for (var i = 0, len = commentNodes.length; i < len; i++) {
       commentNodes[i].classList.remove('giscus');
     }
-    // Find or create
-    var commentNode = this.document.querySelector(
-      '.oeyoews-giscus[tiddler-title="' + id.replace('"', '\\"') + '"]',
-    );
-    if (!commentNode) {
-      commentNode = this.document.createElement('div');
-      commentNode.setAttribute('class', 'giscus oeyoews-giscus');
-      commentNode.setAttribute('tiddler-title', id);
-      parent.insertBefore(commentNode, nextSibling);
-      this.domNodes.push(commentNode);
-    }
+    // 创建新的评论区节点
+    var commentNode = this.document.createElement('div');
+    commentNode.setAttribute('class', 'giscus oeyoews-giscus');
+    commentNode.setAttribute('tiddler-title', id);
+    parent.insertBefore(commentNode, nextSibling);
+    this.domNodes.push(commentNode);
+
     parent.insertBefore(scriptNode, nextSibling);
     this.domNodes.push(scriptNode);
   };
