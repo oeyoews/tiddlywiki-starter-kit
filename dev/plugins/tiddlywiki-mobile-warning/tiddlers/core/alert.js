7a47ad28c6a051970cbe4b3ccbd5da76b09c156e
oeyoews <jyao4783@gmail.com>
2023-03-07 12:02:11 +0800

    Automatic commit

--- dev/plugins/tiddlywiki-mobile-warning/tiddlers/core/alert.js
+++ dev/plugins/tiddlywiki-mobile-warning/tiddlers/core/alert.js
@@ -1,9 +1,10 @@
-// alert mobile
+/* alert mobile */
 (function () {
   var isMobileTiddler = '$:/info/browser/is/mobile';
   var twmTiddler = '$:/plugins/oeyoews/tiddlywiki-mobile-warning/alert-text';
   var isMobileTiddlerGet = $tw.wiki.getTiddlerText(isMobileTiddler);
   var twmTiddlerGet = $tw.wiki.getTiddlerText(twmTiddler);
+  // console.log('🐛alert test');
   if (isMobileTiddlerGet === 'yes') {
     alert(twmTiddlerGet);
   }
