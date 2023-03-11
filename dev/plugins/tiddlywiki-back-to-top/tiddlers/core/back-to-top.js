b7f1234e72ced315ef713877f2e57e79d8e1554f
oeyoews <jyao4783@gmail.com>
2023-03-11 12:59:05 +0800

    feat: add new plugin (back-to-top)

---
+++ dev/plugins/tiddlywiki-back-to-top/tiddlers/core/back-to-top.js
@@ -0,0 +1,45 @@
+/*\
+title: $:/plugins/my-plugin/back-to-top.js
+type: application/javascript
+module-type: global
+
+Adds a "back to top" button to the page which allows users to quickly scroll to the top of the page.
+\*/
+(function () {
+  /*jslint node: true, browser: true */
+  /*global $tw: false */
+
+  'use strict';
+
+  if (!$tw.browser) return;
+
+  // Define the "back to top" button
+  const buttonHtml = `<button class="back-to-top-btn" style="display: none;">
+  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M8 11h3v10h2V11h3l-4-4-4 4zM4 3v2h16V3H4z"></path></svg>
+</button>`;
+  // Define the amount of pixels from the top of the page at which to show the button
+  const showButtonAt = 200;
+
+  // Define the function that will be called when the button is clicked
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  // Create the button element and attach the click listener
+  const button = document.createElement('div');
+  button.id = 'oeyoews-story-top';
+  button.innerHTML = buttonHtml;
+  button.firstElementChild.addEventListener('click', scrollToTop);
+
+  // Add the button to the page
+  document.body.appendChild(button);
+
+  // Add a scroll event listener to show/hide the button based on the user's scroll position
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > showButtonAt) {
+      button.firstElementChild.style.display = 'block';
+    } else {
+      button.firstElementChild.style.display = 'none';
+    }
+  });
+})();
