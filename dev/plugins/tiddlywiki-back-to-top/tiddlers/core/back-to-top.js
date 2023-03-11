b88fec555976c89894c11d87abb3f952d8c5a56b
oeyoews <jyao4783@gmail.com>
2023-03-11 13:45:21 +0800

    Automatic commit

--- dev/plugins/tiddlywiki-back-to-top/tiddlers/core/back-to-top.js
+++ dev/plugins/tiddlywiki-back-to-top/tiddlers/core/back-to-top.js
@@ -14,9 +14,7 @@ Adds a "back to top" button to the page which allows users to quickly scroll to 
   if (!$tw.browser) return;
 
   // Define the "back to top" button
-  const buttonHtml = `<button class="back-to-top-btn" style="display: none;">
-  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M8 11h3v10h2V11h3l-4-4-4 4zM4 3v2h16V3H4z"></path></svg>
-</button>`;
+  const svgButton = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M8 11h3v10h2V11h3l-4-4-4 4zM4 3v2h16V3H4z"></path></svg>`;
   // Define the amount of pixels from the top of the page at which to show the button
   const showButtonAt = 200;
 
@@ -27,8 +25,11 @@ Adds a "back to top" button to the page which allows users to quickly scroll to 
 
   // Create the button element and attach the click listener
   const button = document.createElement('div');
+  const backToTopButton = document.createElement('button');
   button.id = 'oeyoews-story-top';
-  button.innerHTML = buttonHtml;
+  backToTopButton.className = 'back-to-top-btn';
+  backToTopButton.innerHTML = svgButton;
+  button.appendChild(backToTopButton);
   button.firstElementChild.addEventListener('click', scrollToTop);
 
   // Add the button to the page
@@ -36,14 +37,12 @@ Adds a "back to top" button to the page which allows users to quickly scroll to 
 
   // Add a scroll event listener to show/hide the button based on the user's scroll position
   window.addEventListener('scroll', () => {
-    // const buttonElem = button.firstElementChild;
-    // if (window.scrollY > showButtonAt) {
-    //   buttonElem.classList.remove('is-visible');
-    //   console.log(buttonElem);
-    // } else {
-    //   buttonElem.classList.add('is-visible');
-    // }
-    button.firstElementChild.style.display =
-      window.scrollY > showButtonAt ? 'block' : 'none';
+    if (window.scrollY > showButtonAt) {
+      backToTopButton.classList.add('is-visible');
+    } else {
+      backToTopButton.classList.remove('is-visible');
+    }
+    // button.firstElementChild.style.display =
+    //   window.scrollY > showButtonAt ? 'block' : 'none';
   });
 })();
