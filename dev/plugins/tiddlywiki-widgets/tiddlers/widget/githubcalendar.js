c0ce21b22dc08c34c4642a0cfb6f49d268350add
oeyoews <jyao4783@gmail.com>
2023-03-11 17:56:16 +0800

    perf: builtin githubcalendar.js

--- dev/plugins/tiddlywiki-widgets/tiddlers/widget/githubcalendar.js
+++ dev/plugins/tiddlywiki-widgets/tiddlers/widget/githubcalendar.js
@@ -16,27 +16,30 @@ github-calendar js
       super(parseTreeNode, options);
       this.githubCalendarScriptSrc =
         'https://unpkg.com/github-calendar@latest/dist/github-calendar.min.js';
-      this.githubCalendarCssSrc =
-        'https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css';
+      // this.githubCalendarCssSrc =
+      //   'https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css';
     }
 
     render(parent, nextSibling) {
       this.parentDomNode = parent;
+      this.computeAttributes();
+      this.execute();
+
+      const username = this.getAttribute('username', 'oeyoews');
+      const GitHubCalendar = require('githubcalendar.js');
 
       // Create the script element and append it to the head of the document
-      const githubCalendarScript = this.document.createElement('script');
-      githubCalendarScript.src = this.githubCalendarScriptSrc;
-      githubCalendarScript.async = true;
-      this.document.head.appendChild(githubCalendarScript);
+      // const githubCalendarScript = this.document.createElement('script');
+      // githubCalendarScript.src = this.githubCalendarScriptSrc;
+      // githubCalendarScript.async = true;
+      // this.document.head.appendChild(githubCalendarScript);
 
       // Create the CSS link element and append it to the head of the document
-      const githubCalendarCss = this.document.createElement('link');
-      githubCalendarCss.rel = 'stylesheet';
-      githubCalendarCss.href = this.githubCalendarCssSrc;
-      this.document.head.appendChild(githubCalendarCss);
+      // const githubCalendarCss = this.document.createElement('link');
+      // githubCalendarCss.rel = 'stylesheet';
+      // githubCalendarCss.href = this.githubCalendarCssSrc;
+      // this.document.head.appendChild(githubCalendarCss);
 
-      this.computeAttributes();
-      const username = this.getAttribute('username', 'oeyoews');
       const options = {};
       if (this.getAttribute('responsive', 'yes') === 'yes') {
         options.responsive = true;
@@ -51,14 +54,14 @@ github-calendar js
       // Create a div element to hold the calendar
       const githubCalendarDiv = this.document.createElement('div');
       githubCalendarDiv.className = 'calendar';
-      githubCalendarDiv.textContent = 'Loading the data just for you.';
+      githubCalendarDiv.textContent = `Loading the data just for ${username}.`;
       parent.insertBefore(githubCalendarDiv, nextSibling);
       this.domNodes.push(githubCalendarDiv);
 
       // Initialize GitHubCalendar with options
-      githubCalendarScript.addEventListener('load', () => {
-        GitHubCalendar(githubCalendarDiv, username, options);
-      });
+      // githubCalendarScript.addEventListener('load', () => {
+      GitHubCalendar(githubCalendarDiv, username, options);
+      // });
 
       console.log(
         '%c🎉 GitHub contributions calendar is now a widget! 🎉',
@@ -70,25 +73,25 @@ github-calendar js
       return false;
     }
 
-    detach() {
-      // Remove the GitHubCalendar script and CSS link and calendar div
-      const githubCalendarScript = this.document.querySelector(
-        `[src="${this.githubCalendarScriptSrc}"]`,
-      );
-      if (githubCalendarScript) {
-        githubCalendarScript.remove();
-      }
-      const githubCalendarCss = this.document.querySelector(
-        `[href="${this.githubCalendarCssSrc}"]`,
-      );
-      if (githubCalendarCss) {
-        githubCalendarCss.remove();
-      }
-      const githubCalendarDiv = this.document.querySelector('.calendar');
-      if (githubCalendarDiv) {
-        githubCalendarDiv.remove();
-      }
-    }
+    // detach() {
+    // Remove the GitHubCalendar script and CSS link and calendar div
+    // const githubCalendarScript = this.document.querySelector(
+    //   `[src="${this.githubCalendarScriptSrc}"]`,
+    // );
+    // if (githubCalendarScript) {
+    //   githubCalendarScript.remove();
+    // }
+    // const githubCalendarCss = this.document.querySelector(
+    //   `[href="${this.githubCalendarCssSrc}"]`,
+    // );
+    // if (githubCalendarCss) {
+    //   githubCalendarCss.remove();
+    // }
+    // const githubCalendarDiv = this.document.querySelector('.calendar');
+    // if (githubCalendarDiv) {
+    //   githubCalendarDiv.remove();
+    // }
+    // }
   }
 
   exports['github-calendar'] = GithubCalendarWidget;
