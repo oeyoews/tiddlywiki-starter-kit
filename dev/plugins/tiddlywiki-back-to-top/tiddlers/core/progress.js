88c32b2e2e0465aeb92289ddc42fc276e148b34c
oeyoews <jyao4783@gmail.com>
2023-03-15 12:33:51 +0800

    fix: compatiable ios

--- dev/plugins/tiddlywiki-back-to-top/tiddlers/core/progress.js
+++ dev/plugins/tiddlywiki-back-to-top/tiddlers/core/progress.js
@@ -6,7 +6,7 @@ module-type: startup
 progress
 
 \*/
-// TODO: ios not update processbar
+// CSS-based progress bar that works on iOS devices
 (function () {
   /*jslint node: true, browser: true */
   /*global $tw: false */
@@ -14,19 +14,15 @@ progress
 
   if (!$tw.browser) return;
 
-  if (navigator.userAgent.match(/Mobile/i)) return;
-
   // Export name and synchronous status
   exports.name = 'progressbar';
   exports.platforms = ['browser'];
   exports.after = ['startup'];
   exports.synchronous = true;
 
-  var progressBar = document.createElement('progress');
+  var progressBar = document.createElement('div');
   progressBar.id = 'page-progress';
-  progressBar.setAttribute('value', '0');
-  progressBar.max = '100';
-
+  progressBar.innerHTML = '<div id="page-progress-bar"></div>';
   document.body.appendChild(progressBar);
 
   function progressbarListener() {
@@ -39,8 +35,8 @@ progress
       const denominator = pageHeight - windowHeight;
       const scrollProgress =
         denominator !== 0 ? (scrollTop / denominator) * 100 : 0;
-      const progressBar = document.getElementById('page-progress');
-      progressBar.value = scrollProgress;
+      const progressBar = document.getElementById('page-progress-bar');
+      progressBar.style.width = scrollProgress + '%';
     });
   }
 
