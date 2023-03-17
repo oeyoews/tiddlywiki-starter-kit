ff66e9be9af9ea9d6a957c8a090cbc4b1d70ecd1
oeyoews <jyao4783@gmail.com>
2023-03-18 01:34:31 +0800

    feat: add transition

--- plugins/tiddlywiki-keyboard-hint/tiddlers/core/startup.js
+++ plugins/tiddlywiki-keyboard-hint/tiddlers/core/startup.js
@@ -74,12 +74,17 @@ show pressed key
       }
     }
 
+    /* 在更新按键时，先使用 while 循环将 keysEl 的所有子元素都移除，然后使用 createElement() 方法创建按键元素，并使用 appendChild() 方法将其添加到 keysEl 中。这样就可以避免使用 innerHTML 属性而带来的安全问题。
+需要注意的是，innerHTML 属性具有更高的性能，因为它可以一次性将多个 DOM 元素添加到页面中。因此，在元素数量较多的情况下，使用 innerHTML 属性可能会更快。但是，在元素数量较少的情况下，使用 createElement() 和 appendChild() 方法的性能差异可以忽略不计。另外，使用 createElement() 和 appendChild() 方法可以更好地控制页面中的 DOM 结构，从而避免潜在的安全问题。 */
     function updateKeys() {
-      keysEl.innerHTML = '';
+      while (keysEl.firstChild) {
+        keysEl.removeChild(keysEl.firstChild);
+      }
       pressedKeys.forEach(key => {
         const keyEl = document.createElement('span');
         keyEl.classList.add('key');
-        keyEl.textContent = key;
+        const keyName = key === ' ' ? 'Space' : key;
+        keyEl.textContent = keyName;
         keysEl.appendChild(keyEl);
       });
       if (pressedKeys.length > 0) {
