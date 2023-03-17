2a6bc24ad96a06c8e6acf1553cd337470a50af35
oeyoews <jyao4783@gmail.com>
2023-03-17 11:10:38 +0800

    Automatic commit

---
+++ archive/plugins/tiddlywiki-keyboard-hint/tiddlers/core/widget.js
@@ -0,0 +1,53 @@
+/*\
+title: general-widget
+type: application/javascript
+module-type: widget
+
+general widget
+
+\*/
+(function () {
+  /*jslint node: true, browser: true */
+  /*global $tw: false */
+  'use strict';
+
+  if (!$tw.browser) {
+    return;
+  }
+
+  const Widget = require('$:/core/modules/widgets/widget.js').widget;
+
+  class DivWidget extends Widget {
+    constructor(parseTreeNode, options) {
+      super(parseTreeNode, options);
+    }
+
+    render(parent, nextSibling) {
+      this.parentDomNode = parent;
+      this.computeAttributes();
+      this.execute();
+
+      const param = this.getAttribute('param', 'Test Param');
+
+      const divNode = this.document.createElement('div');
+      divNode.id = 'divId';
+      divNode.textContent = param;
+      divNode.className = 'divClass';
+      parent.insertBefore(divNode, nextSibling);
+      this.domNodes.push(divNode);
+    }
+
+    refresh() {
+      var changedAttributes = this.computeAttributes();
+      // changedAttributes.title;
+      if (Object.keys(changedAttributes).length > 0) {
+        this.refreshSelf();
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  exports['div-widget'] = DivWidget;
+})();
