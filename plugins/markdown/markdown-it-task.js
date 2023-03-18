56818de4cdfa40f4bd13cec7d31a843b30c9673c
oeyoews <jyao4783@gmail.com>
2023-03-18 22:37:35 +0800

    Automatic commit

--- plugins/markdown/markdown-it-task.js
+++ plugins/markdown/markdown-it-task.js
@@ -8,169 +8,136 @@ markdown-it-task-lists 2.1.0 https://github.com/revin/markdown-it-task-lists#rea
 
 \*/
 
-!(function (n) {
-  if ('object' == typeof exports && 'undefined' != typeof module)
-    module.exports = n();
-  else if ('function' == typeof define && define.amd) define([], n);
-  else {
-    var e;
-    (e =
-      'undefined' != typeof window
-        ? window
-        : 'undefined' != typeof global
-        ? global
-        : 'undefined' != typeof self
-        ? self
-        : this),
-      (e.markdownitTaskLists = n());
-  }
-})(function () {
-  return (function () {
-    function n(e, t, i) {
-      function r(c, l) {
-        if (!t[c]) {
-          if (!e[c]) {
-            var f = 'function' == typeof require && require;
-            if (!l && f) return f(c, !0);
-            if (o) return o(c, !0);
-            var u = new Error("Cannot find module '" + c + "'");
-            throw ((u.code = 'MODULE_NOT_FOUND'), u);
-          }
-          var a = (t[c] = { exports: {} });
-          e[c][0].call(
-            a.exports,
-            function (n) {
-              var t = e[c][1][n];
-              return r(t ? t : n);
-            },
-            a,
-            a.exports,
-            n,
-            e,
-            t,
-            i,
-          );
-        }
-        return t[c].exports;
+// Markdown-it plugin to render GitHub-style task lists; see
+//
+// https://github.com/blog/1375-task-lists-in-gfm-issues-pulls-comments
+// https://github.com/blog/1825-task-lists-in-all-markdown-documents
+// https://github.com/linsir/markdown-it-task-checkbox
+
+module.exports = function (md, options) {
+  var defaults;
+  defaults = {
+    disabled: true,
+    divWrap: false,
+    divClass: 'checkbox',
+    idPrefix: 'cbx_',
+    ulClass: 'task-list',
+    liClass: 'task-list-item',
+  };
+  options = Object.assign({}, defaults, options);
+  md.core.ruler.after('inline', 'github-task-lists', function (state) {
+    var tokens = state.tokens;
+    var lastId = 0;
+    for (var i = 2; i < tokens.length; i++) {
+      if (isTodoItem(tokens, i)) {
+        todoify(tokens[i], lastId, options, state.Token);
+        lastId += 1;
+        attrSet(tokens[i - 2], 'class', options.liClass);
+        attrSet(tokens[parentToken(tokens, i - 2)], 'class', options.ulClass);
       }
-      for (
-        var o = 'function' == typeof require && require, c = 0;
-        c < i.length;
-        c++
-      )
-        r(i[c]);
-      return r;
     }
-    return n;
-  })()(
-    {
-      1: [
-        function (n, e, t) {
-          function i(n, e, t) {
-            var i = n.attrIndex(e),
-              r = [e, t];
-            0 > i ? n.attrPush(r) : (n.attrs[i] = r);
-          }
-          function r(n, e) {
-            for (var t = n[e].level - 1, i = e - 1; i >= 0; i--)
-              if (n[i].level === t) return i;
-            return -1;
-          }
-          function o(n, e) {
-            return s(n[e]) && d(n[e - 1]) && h(n[e - 2]) && p(n[e]);
-          }
-          function c(n, e) {
-            if (
-              (n.children.unshift(l(n, e)),
-              (n.children[1].content = n.children[1].content.slice(3)),
-              (n.content = n.content.slice(3)),
-              b)
-            )
-              if (v) {
-                n.children.pop();
-                var t = 'task-item-' + Math.ceil(1e7 * Math.random() - 1e3);
-                (n.children[0].content =
-                  n.children[0].content.slice(0, -1) + ' id="' + t + '">'),
-                  n.children.push(a(n.content, t, e));
-              } else n.children.unshift(f(e)), n.children.push(u(e));
-          }
-          function l(n, e) {
-            var t = new e('html_inline', '', 0),
-              i = x ? ' disabled="" ' : '';
-            return (
-              0 === n.content.indexOf('[ ] ')
-                ? (t.content =
-                    '<input class="task-list-item-checkbox"' +
-                    i +
-                    'type="checkbox">')
-                : (0 === n.content.indexOf('[x] ') ||
-                    0 === n.content.indexOf('[X] ')) &&
-                  (t.content =
-                    '<input class="task-list-item-checkbox" checked=""' +
-                    i +
-                    'type="checkbox">'),
-              t
-            );
-          }
-          function f(n) {
-            var e = new n('html_inline', '', 0);
-            return (e.content = '<label>'), e;
-          }
-          function u(n) {
-            var e = new n('html_inline', '', 0);
-            return (e.content = '</label>'), e;
-          }
-          function a(n, e, t) {
-            var i = new t('html_inline', '', 0);
-            return (
-              (i.content =
-                '<label class="task-list-item-label" for="' +
-                e +
-                '">' +
-                n +
-                '</label>'),
-              (i.attrs = [{ for: e }]),
-              i
-            );
-          }
-          function s(n) {
-            return 'inline' === n.type;
-          }
-          function d(n) {
-            return 'paragraph_open' === n.type;
-          }
-          function h(n) {
-            return 'list_item_open' === n.type;
-          }
-          function p(n) {
-            return (
-              0 === n.content.indexOf('[ ] ') ||
-              0 === n.content.indexOf('[x] ') ||
-              0 === n.content.indexOf('[X] ')
-            );
-          }
-          var x = !0,
-            b = !1,
-            v = !1;
-          e.exports = function (n, e) {
-            e && ((x = !e.enabled), (b = !!e.label), (v = !!e.labelAfter)),
-              n.core.ruler.after('inline', 'github-task-lists', function (n) {
-                for (var e = n.tokens, t = 2; t < e.length; t++)
-                  o(e, t) &&
-                    (c(e[t], n.Token),
-                    i(
-                      e[t - 2],
-                      'class',
-                      'task-list-item' + (x ? '' : ' enabled'),
-                    ),
-                    i(e[r(e, t - 2)], 'class', 'contains-task-list'));
-              });
-          };
-        },
-        {},
-      ],
-    },
-    {},
-    [1],
-  )(1);
-});
+  });
+};
+
+function attrSet(token, name, value) {
+  var index = token.attrIndex(name);
+  var attr = [name, value];
+
+  if (index < 0) {
+    token.attrPush(attr);
+  } else {
+    token.attrs[index] = attr;
+  }
+}
+
+function parentToken(tokens, index) {
+  var targetLevel = tokens[index].level - 1;
+  for (var i = index - 1; i >= 0; i--) {
+    if (tokens[i].level === targetLevel) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function isTodoItem(tokens, index) {
+  return (
+    isInline(tokens[index]) &&
+    isParagraph(tokens[index - 1]) &&
+    isListItem(tokens[index - 2]) &&
+    startsWithTodoMarkdown(tokens[index])
+  );
+}
+
+function todoify(token, lastId, options, TokenConstructor) {
+  var id;
+  id = options.idPrefix + lastId;
+  token.children[0].content = token.children[0].content.slice(3);
+  // label
+  token.children.unshift(beginLabel(id, TokenConstructor));
+  token.children.push(endLabel(TokenConstructor));
+  // checkbox
+  token.children.unshift(makeCheckbox(token, id, options, TokenConstructor));
+  if (options.divWrap) {
+    token.children.unshift(beginWrap(options, TokenConstructor));
+    token.children.push(endWrap(TokenConstructor));
+  }
+}
+
+function makeCheckbox(token, id, options, TokenConstructor) {
+  var checkbox = new TokenConstructor('checkbox_input', 'input', 0);
+  checkbox.attrs = [
+    ['type', 'checkbox'],
+    ['id', id],
+  ];
+  var checked = /^\[[xX]\][ \u00A0]/.test(token.content); // if token.content starts with '[x] ' or '[X] '
+  if (checked === true) {
+    checkbox.attrs.push(['checked', 'true']);
+  }
+  if (options.disabled === true) {
+    checkbox.attrs.push(['disabled', 'true']);
+  }
+
+  return checkbox;
+}
+
+function beginLabel(id, TokenConstructor) {
+  var label = new TokenConstructor('label_open', 'label', 1);
+  label.attrs = [['for', id]];
+  return label;
+}
+
+function endLabel(TokenConstructor) {
+  return new TokenConstructor('label_close', 'label', -1);
+}
+
+// these next two functions are kind of hacky; probably should really be a
+// true block-level token with .tag=='label'
+function beginWrap(options, TokenConstructor) {
+  var token = new TokenConstructor('checkbox_open', 'div', 0);
+  token.attrs = [['class', options.divClass]];
+  return token;
+}
+
+function endWrap(TokenConstructor) {
+  var token = new TokenConstructor('checkbox_close', 'div', -1);
+  // token.content = '</label>';
+  return token;
+}
+
+function isInline(token) {
+  return token.type === 'inline';
+}
+function isParagraph(token) {
+  return token.type === 'paragraph_open';
+}
+function isListItem(token) {
+  return token.type === 'list_item_open';
+}
+
+function startsWithTodoMarkdown(token) {
+  // The leading whitespace in a list item (token.content) is already trimmed off by markdown-it.
+  // The regex below checks for '[ ] ' or '[x] ' or '[X] ' at the start of the string token.content,
+  // where the space is either a normal space or a non-breaking space (character 160 = \u00A0).
+  return /^\[[xX \u00A0]\][ \u00A0]/.test(token.content);
+}
