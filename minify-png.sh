95ef50628ece143a06e16320e26972eb3915d7fb
oeyoews <jyao4783@gmail.com>
2023-03-29 15:04:04 +0800

    perf: minify all images

---
+++ minify-png.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for f in "$@"
+do
+  if [[ $(file --mime-type -b "$f") == image/*g ]]; then
+    pngquant 64 --skip-if-larger --ext=.png --force "$f"
+    zopflipng -y "$f" "$f"
+  fi
+done
+
+# useage
+# sh minify-png.sh files/*
