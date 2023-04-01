f8a1771271d52d3eb09ba4fb95700876bad92943
oeyoews <jyao4783@gmail.com>
2023-04-02 00:09:32 +0800

    fix: better template workflows

--- scripts/build-oeyoews.mjs
+++ scripts/build-oeyoews.mjs
@@ -2,6 +2,7 @@
 
 import msg from './lib/info.mjs';
 import * as dotenv from 'dotenv';
+// import { makeTemplate } from './make-template.mjs';
 
 dotenv.config();
 
@@ -56,9 +57,12 @@ await spinner('Building ...', async () => {
   await $`npx html-minifier-terser -c ./config/html-minifier-terser-config.json -o dist/index.html dist/index.html`;
   // misc
   await $`cp -r files vercel.json ${dist}`;
+  // make template at the end of
   // copy readme file
   // TODO: add cn readme
   // await $`cp plugins/oeyoews/neotw/files/README.md README.md`;
   // msg
   msg.finish('Building Finished(for oyeoews)');
 });
+
+// makeTemplate();
