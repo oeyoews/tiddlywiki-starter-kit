b09e00df514d0bcbf6cf22be68b84bb06724e309
oeyoews <jyao4783@gmail.com>
2023-04-09 17:59:58 +0800

    feat: migrate to tailwindcss-plus

--- plugins/oeyoews/tiddlywiki-tailwindcss-plus/tiddlers/startup.js
+++ plugins/oeyoews/tiddlywiki-tailwindcss-plus/tiddlers/startup.js
@@ -17,6 +17,34 @@ module.exports = {
     try {
       globalThis.tailwindcss = require('tailwindcss.min.js');
       tailwind.config = {
+        theme: {
+          extend: {
+            typography: ({ theme }) => ({
+              DEFAULT: {
+                css: {
+                  // a: {
+                  //   textDecoration: `none`,
+                  // },
+                  'blockquote p:first-of-type::before': null,
+                  'blockquote p:last-of-type::after': null,
+                  code: {
+                    backgroundColor: theme('colors.slate.100'),
+                    borderRadius: theme('borderRadius.sm'),
+                    padding: `${theme('padding.1')} ${theme('padding.1.5')}`,
+                    border: `none`,
+                  },
+                  'code::before': {
+                    content: 'normal',
+                  },
+                  'code::after': {
+                    content: 'normal',
+                  },
+                },
+              },
+            }),
+          },
+        },
+        darkMode: 'class',
         important: true,
         corePlugins: {
           preflight: false,
