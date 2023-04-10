1d1bef384538a479f96b44a31ccff46fc1da6d83
oeyoews <jyao4783@gmail.com>
2023-04-10 09:26:27 +0800

    perf: split tailwindcss config

--- plugins/oeyoews/tiddlywiki-tailwindcss-plus/tiddlers/startup.js
+++ plugins/oeyoews/tiddlywiki-tailwindcss-plus/tiddlers/startup.js
@@ -16,43 +16,49 @@ hide-body: yes
   exports.after = ['load-modules'];
   exports.synchronous = true;
 
+  // tailwindConfig
+  var tailwindConfig = {
+    theme: {
+      extend: {
+        typography: function (_ref) {
+          var theme = _ref.theme;
+          return {
+            DEFAULT: {
+              css: {
+                // a: {
+                //   textDecoration: `none`,
+                // },
+                'blockquote p:first-of-type::before': null,
+                'blockquote p:last-of-type::after': null,
+                code: {
+                  backgroundColor: theme('colors.slate.100'),
+                  borderRadius: theme('borderRadius.sm'),
+                  padding: theme('padding.1') + ' ' + theme('padding.1.5'),
+                  border: 'none',
+                },
+                'code::before': {
+                  content: 'normal',
+                },
+                'code::after': {
+                  content: 'normal',
+                },
+              },
+            },
+          };
+        },
+      },
+    },
+    darkMode: 'class',
+    important: true,
+    corePlugins: {
+      preflight: false,
+    },
+  };
+
   exports.startup = function () {
     try {
       globalThis.tailwindcss = require('tailwindcss.min.js');
-      tailwind.config = {
-        theme: {
-          extend: {
-            typography: ({ theme }) => ({
-              DEFAULT: {
-                css: {
-                  // a: {
-                  //   textDecoration: `none`,
-                  // },
-                  'blockquote p:first-of-type::before': null,
-                  'blockquote p:last-of-type::after': null,
-                  code: {
-                    backgroundColor: theme('colors.slate.100'),
-                    borderRadius: theme('borderRadius.sm'),
-                    padding: `${theme('padding.1')} ${theme('padding.1.5')}`,
-                    border: `none`,
-                  },
-                  'code::before': {
-                    content: 'normal',
-                  },
-                  'code::after': {
-                    content: 'normal',
-                  },
-                },
-              },
-            }),
-          },
-        },
-        darkMode: 'class',
-        important: true,
-        corePlugins: {
-          preflight: false,
-        },
-      };
+      tailwind.config = tailwindConfig;
     } catch (r) {
       console.error(r);
     }
