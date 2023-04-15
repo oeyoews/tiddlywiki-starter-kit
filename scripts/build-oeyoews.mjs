8f1a9fee2aa9bdd6d8f963ffe2204e347d515661
oeyoews <jyao4783@gmail.com>
2023-04-15 13:46:19 +0800

    BREAKING CHANGE: remove static build

--- scripts/build-oeyoews.mjs
+++ scripts/build-oeyoews.mjs
@@ -48,16 +48,16 @@ await spinner('Building ...', async () => {
   // await $`npx ${bin} --build neotw-empty`;
 
   // build static
-  await $`cp -r static-patch tiddlers/static-patch`;
-  await $`npx ${bin} --build static`;
+  // await $`cp -r static-patch tiddlers/static-patch`;
+  // await $`npx ${bin} --build static`;
   // await $`npx tiddlywiki --output dist --render '[!is[system]]' '[addprefix[static/]addsuffix[.html]]'`;
   // await $`npx tiddlywiki  --output dist --rendertiddler "$:/core/templates/static.template.css" "static/static.css"`;
-  await $`rm -rf tiddlers/static-patch`;
+  // await $`rm -rf tiddlers/static-patch`;
   // move static to root
-  await $`cp dist/static/* dist/`;
+  // await $`cp dist/static/* dist/`;
 
   // minify index.html
-  await $`npx html-minifier-terser -c ./config/html-minifier-terser-config.json -o dist/main.html dist/main.html`;
+  await $`npx html-minifier-terser -c ./config/html-minifier-terser-config.json -o dist/index.html dist/index.html`;
   // misc
   await $`cp -r files vercel.json ${dist}`;
   // await $`cp dist/static/index.html ${dist}`;
