0ecccda74375d32fc38db13c515775267842bc1b
oeyoews <jyao4783@gmail.com>
2023-04-19 21:55:00 +0800

    feat: add tiddlywiki-videos

---
+++ plugins/oeyoews/tiddlywiki-videos/tiddlers/core/widget.js
@@ -0,0 +1,45 @@
+/*\
+title: $:/plugins/oeyoews/tiddlywiki-videos/widget
+type: application/javascript
+module-type: widget
+
+youtube widget
+
+\*/
+
+(function () {
+  const Widget = require('$:/core/modules/widgets/widget.js').widget;
+
+  class YoutubeWidget extends Widget {
+    constructor(parseTreeNode, options) {
+      super(parseTreeNode, options);
+    }
+
+    render(parent, nextSibling) {
+      this.parentDomNode = parent;
+      this.computeAttributes();
+      this.execute();
+
+      var youtubeId = this.getAttribute('youtubeId', '');
+
+      const container = this.document.createElement('div');
+      container.className = 'youtube-container';
+      parent.insertBefore(container, nextSibling);
+      this.domNodes.push(container);
+
+      const iframeSrc = `https://www.youtube.com/embed/${youtubeId}`;
+
+      const iframe = this.document.createElement('iframe');
+      iframe.className = 'border-none shadow-lg rounded-md';
+      iframe.src = iframeSrc;
+      iframe.width = '560';
+      iframe.height = '315';
+      iframe.frameborder = '0';
+      iframe.allowfullscreen = true;
+
+      container.appendChild(iframe);
+    }
+  }
+
+  exports.youtube = YoutubeWidget;
+})();
