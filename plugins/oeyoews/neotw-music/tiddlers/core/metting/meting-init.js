e15148b55f4838285c40976c9acaade2d46d4144
oeyoews <jyao4783@gmail.com>
2023-04-19 23:00:16 +0800

    perf: split config for options

--- plugins/oeyoews/neotw-music/tiddlers/core/metting/meting-init.js
+++ plugins/oeyoews/neotw-music/tiddlers/core/metting/meting-init.js
@@ -12,19 +12,25 @@ meting-init
   if (!config.id) {
     alert('检测到你没有设置TWM id, 默认使用 2916766519');
   }
-  metingjs.setAttribute('server', config.server);
-  metingjs.setAttribute('id', config.id);
-  metingjs.setAttribute('type', config.type);
-  metingjs.setAttribute('order', config.order);
-  metingjs.setAttribute('loop', config.loop);
-  metingjs.setAttribute('preload', config.preload);
-  metingjs.setAttribute('mutex', config.mutex);
-  metingjs.setAttribute('fixed', config.fixed);
+
+  const options = {
+    server: 'netease',
+    id: '',
+    type: '',
+    order: '',
+    loop: '',
+    preload: '',
+    mutex: '',
+    fixed: '',
+  };
+
+  for (const key in options) {
+    metingjs.setAttribute(key, config[key]);
+  }
+
   // BUG
   if (config.enablelrc === 'no') {
-    // metingjs.setAttribute('lrc-type', config.lrc_type);
     metingjs.setAttribute('lrc-type', 'yes');
-    // console.log('disable lrc');
   }
   metingjs.setAttribute('list-olded', config.list_olded);
   document.body.appendChild(metingjs);
