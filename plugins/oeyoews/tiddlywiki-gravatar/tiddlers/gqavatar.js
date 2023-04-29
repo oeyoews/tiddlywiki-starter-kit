552f7e2f6a67f4c92b17712e85614a10292a2354
oeyoews <jyao4783@gmail.com>
2023-04-29 14:38:54 +0800

    feat: remove extra container and styles for gravatar

--- plugins/oeyoews/tiddlywiki-gravatar/tiddlers/gqavatar.js
+++ plugins/oeyoews/tiddlywiki-gravatar/tiddlers/gqavatar.js
@@ -28,12 +28,12 @@ Gravatar and QQ  Github Avatar Widget(Lastest gqg)
       this.computeAttributes();
       this.execute();
 
-      var sound = this.getAttribute('sound', 'no');
       var getDefaultEmail = $tw.wiki.getTiddlerText(
         '$:/config/plugins/oeyoews/tiddlywiki-gravatar/email',
       );
       const Username = $tw.wiki.getTiddlerText('$:/status/UserName');
       const username = this.getAttribute('username', Username || 'oeyoews');
+      var isCenter = this.getAttribute('center', false);
       var width = this.getAttribute('width', '56');
       var type = this.getAttribute('type', 'qq');
       var email = this.getAttribute(
@@ -41,10 +41,6 @@ Gravatar and QQ  Github Avatar Widget(Lastest gqg)
         getDefaultEmail || '2956398608@qq.com',
       );
 
-      var gclass = this.getAttribute(
-        'gclass',
-        'gravatar-56 object-cover object-center',
-      );
       var size = this.getAttribute('size', '100');
       var alt = this.getAttribute('alt', 'Avatar');
       var src = `https://q1.qlogo.cn/g?b=qq&nk=${email}&s=${size}`;
@@ -69,23 +65,19 @@ Gravatar and QQ  Github Avatar Widget(Lastest gqg)
         const hash = md5(email.trim().toLowerCase());
         src = `https://cn.gravatar.com/avatar/${hash}.png?s=${size}`;
       }
-      // Create element
-      const avatarContainer = this.document.createElement('div');
-      avatarContainer.className = gclass;
-      avatarContainer.setAttribute('style', `max-width: ${width}px`);
-      avatarContainer.onclick = () => sound === 'yes' && howler('click01.mp3');
 
       var img = this.document.createElement('img');
-      img.setAttribute('class', 'shadow-lg');
+      img.className =
+        'rounded-full align-middle m-1 duration-200 transition shadow-lg';
+      if (isCenter) {
+        img.className += ' ' + 'block mx-auto';
+      }
+      img.setAttribute('style', `max-width: ${width}px`);
       img.setAttribute('src', src);
       img.setAttribute('alt', alt);
-      // img.setAttribute('class', gclass);
-      img.setAttribute('style', `max-width: ${width}px`);
 
-      avatarContainer.appendChild(img);
-      // Insert element
-      parent.insertBefore(avatarContainer, nextSibling);
-      this.domNodes.push(avatarContainer);
+      parent.insertBefore(img, nextSibling);
+      this.domNodes.push(img);
     };
   }
 
