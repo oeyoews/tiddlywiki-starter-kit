baa265f0481c22c3d0df1d3283e6df61cafedc81
oeyoews <jyao4783@gmail.com>
2023-05-03 09:55:00 +0800

    perf: overwrite console.warn

--- plugins/oeyoews/tiddlywiki-tailwindcss-plus/tiddlers/startup.js
+++ plugins/oeyoews/tiddlywiki-tailwindcss-plus/tiddlers/startup.js
@@ -7,7 +7,7 @@ hide-body: yes
 tailwindcss startup
 
 \*/
-(function () {
+(function() {
   /*jslint node: true, browser: true */
   /*global $tw: false */
   'use strict';
@@ -17,6 +17,23 @@ tailwindcss startup
   exports.after = ['load-modules'];
   exports.synchronous = true;
   exports.startup = () => {
+    // 保存原始的console.warn()方法
+    const originalWarn = console.warn;
+
+    // 重写console.warn()方法
+    console.warn = function(...args) {
+      // 获取调用栈信息
+      const stackTrace = new Error().stack;
+      // 判断调用栈信息中是否包含"script.js"字符串
+      if (stackTrace.includes('tailwindcss.min.js')) {
+        // 自定义逻辑：拦截console.warn()信息，记录日志等
+        // console.log('Intercepted warning from script.js:', ...args);
+        console.warn('Tailwindcss is using cdn on production env');
+      } else {
+        // 调用原始的console.warn()方法输出警告信息
+        originalWarn(...args);
+      }
+    };
     tailwind = require('tailwindcss.min.js');
     var tailwindConfig = require('tailwind.config.js');
     tailwind.config = tailwindConfig;
