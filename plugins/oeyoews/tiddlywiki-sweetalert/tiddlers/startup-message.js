ba83314552ec1898d9db9421aeda8add124a5a3b
oeyoews <jyao4783@gmail.com>
2023-05-03 12:48:47 +0800

    feat: add versio for swal startup

--- plugins/oeyoews/tiddlywiki-sweetalert/tiddlers/startup-message.js
+++ plugins/oeyoews/tiddlywiki-sweetalert/tiddlers/startup-message.js
@@ -12,7 +12,7 @@ const neotw = JSON.parse(localStorage.getItem('neotw')) || {};
 const currentDate = new Date().toDateString();
 const noNotifyDate = neotw.noNotifyDate;
 
-// online tip
+/* // online tip
 if (window.location.protocol === 'https:' && noNotifyDate !== currentDate) {
   swal({
     title: '👋 Welcome to neotw !',
@@ -35,4 +35,41 @@ if (window.location.protocol === 'https:' && noNotifyDate !== currentDate) {
       localStorage.setItem('neotw', JSON.stringify(neotw));
     }
   });
-}
+} */
+
+axios
+  .get('https://raw.githubusercontent.com/oeyoews/neotw/main/package.json')
+  .then(function (response) {
+    const version = response.data.version;
+
+    if (window.location.protocol === 'https:' && noNotifyDate !== currentDate) {
+      swal({
+        title: 'Welcom to Neotw 👋',
+        text: 'The version of neotw is ' + version,
+        icon: 'success',
+        buttons: {
+          no: {
+            text: 'Silent',
+            value: false,
+          },
+          yes: {
+            text: 'Close',
+            value: true,
+            visible: true,
+          },
+        },
+      }).then(result => {
+        if (!result) {
+          neotw.noNotifyDate = currentDate;
+          localStorage.setItem('neotw', JSON.stringify(neotw));
+        }
+      });
+    }
+  })
+  .catch(function (error) {
+    swal({
+      title: 'Error',
+      text: 'Failed to fetch version: ' + error.message,
+      icon: 'error',
+    });
+  });
