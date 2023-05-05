3cf1a817bd0ba990cdeb3a4401c1296a3b66c53a
oeyoews <jyao4783@gmail.com>
2023-05-06 01:32:16 +0800

    Automatic commit

--- plugins/oeyoews/tiddlywiki-tid2pdf/tiddlers/startup.js
+++ plugins/oeyoews/tiddlywiki-tid2pdf/tiddlers/startup.js
@@ -18,5 +18,31 @@ tid2pdf module
   exports.startup = () => {
     window.jspdf = require('jspdf.umd.min.js');
     window.html2canvas = require('html2canvas.min.js');
+
+    /* $tw.rootWidget.addEventListener('om-export-png', () => {
+      const title = getvariable('currentTiddler');
+      // const param = this.getAttribute('param', `Download ${title}.png`);
+      const selector = `[data-tiddler-title="${title}"]`;
+
+      var element = document.querySelector(selector);
+
+      html2canvas(element).then(canvas => {
+        const imgData = canvas.toDataURL('image/png'); // 转换canvas为PNG格式的数据URL
+
+        // 创建一个<a>元素，并指定其href属性为图像数据URL
+        const linkNode = document.createElement('a');
+        linkNode.href = imgData;
+
+        // 指定文件名并将<a>元素添加到页面上
+        linkNode.download = `${title}`;
+        document.body.appendChild(linkNode);
+
+        // 模拟单击事件以触发下载
+        linkNode.click();
+
+        // 将<a>元素从页面上移除
+        document.body.removeChild(linkNode);
+      });
+    }); */
   };
 })();
