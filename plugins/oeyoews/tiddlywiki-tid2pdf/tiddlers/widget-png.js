147bdc4f6d6f03efebc86e9fec1fe23b82977e77
oeyoews <jyao4783@gmail.com>
2023-05-06 13:49:41 +0800

    feat: support swal for html2canvas

--- plugins/oeyoews/tiddlywiki-tid2pdf/tiddlers/widget-png.js
+++ plugins/oeyoews/tiddlywiki-tid2pdf/tiddlers/widget-png.js
@@ -27,11 +27,29 @@ tid2png/widget
 
       const title = this.getVariable('currentTiddler');
       const param = this.getAttribute('param', `Download ${title}.png`);
+      const preview = this.getAttribute('preview') === 'true';
 
       const buttonNode = this.document.createElement('button');
       buttonNode.textContent = param;
       buttonNode.className =
         'bg-lime-200 hover:bg-lime-300 duration-200 transition rounded-sm py-1 px-2 transition duration-200 m-1';
+
+      const downloadPng = imgData => {
+        // 创建一个<a>元素，并指定其href属性为图像数据URL
+        const linkNode = document.createElement('a');
+        linkNode.href = imgData;
+
+        // 指定文件名并将<a>元素添加到页面上
+        linkNode.download = `${title}`;
+        document.body.appendChild(linkNode);
+
+        // 模拟单击事件以触发下载
+        linkNode.click();
+
+        // 将<a>元素从页面上移除
+        document.body.removeChild(linkNode);
+      };
+
       buttonNode.onclick = () => {
         const selector = `[data-tiddler-title="${title}"]`;
         var element = document.querySelector(selector);
@@ -40,30 +58,32 @@ tid2png/widget
         html2canvas(element).then(canvas => {
           const imgData = canvas.toDataURL('image/png');
 
-          // 创建一个<a>元素，并指定其href属性为图像数据URL
-          const linkNode = document.createElement('a');
-          linkNode.href = imgData;
+          // 创建一个<img>元素，并将PNG格式的数据URL赋值给src属性
+          const imgNode = document.createElement('img');
+          imgNode.src = imgData;
 
-          // 指定文件名并将<a>元素添加到页面上
-          linkNode.download = `${title}`;
-          document.body.appendChild(linkNode);
-
-          if (this.getAttribute('preview')) {
-            // 如果传入了preview参数，则在新窗口中显示该图片
-            const previewWindow = window.open('', '_blank');
-            previewWindow.document.title = title;
-
-            // 创建一个<img>元素，并将PNG格式的数据URL赋值给src属性
-            const imgNode = previewWindow.document.createElement('img');
-            imgNode.src = imgData;
-            previewWindow.document.body.appendChild(imgNode);
+          if (preview) {
+            swal({
+              title: `${title}`,
+              content: {
+                element: imgNode,
+              },
+              buttons: {
+                download: {
+                  text: 'Download',
+                  value: true,
+                },
+                cancel: 'Cancel',
+              },
+              className: 'w-auto',
+            }).then(value => {
+              if (value) {
+                downloadPng(imgData);
+              }
+            });
           } else {
-            // 否则直接下载该图片
-            linkNode.click();
+            downloadPng(imgData);
           }
-
-          // 将<a>元素从页面上移除
-          document.body.removeChild(linkNode);
         });
       };
       parent.insertBefore(buttonNode, nextSibling);
