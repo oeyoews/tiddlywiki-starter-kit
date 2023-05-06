47052f01cb0e2b7530218e081f2a7cb4a6117287
oeyoews <jyao4783@gmail.com>
2023-05-06 20:03:54 +0800

    perf: add serif for code style

--- plugins/oeyoews/tiddlywiki-tailwindcss-plus/files/tailwind.config.js
+++ plugins/oeyoews/tiddlywiki-tailwindcss-plus/files/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
             'blockquote p:first-of-type::before': null,
             'blockquote p:last-of-type::after': null,
             code: {
+              fontFamily: 'serif',
               backgroundColor: theme('colors.slate.100'),
               borderRadius: theme('borderRadius.sm'),
               padding: `${theme('padding.1')} ${theme('padding.1.5')}`,
