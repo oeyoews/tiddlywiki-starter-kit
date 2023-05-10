0aa57584e63a5e37a204d8d3f7d5354d498789eb
oeyoews <jyao4783@gmail.com>
2023-05-10 12:30:59 +0800

    feat: add echarts

--- plugins/oeyoews/tiddlywiki-echarts/tiddlers/core/widget.js
+++ plugins/oeyoews/tiddlywiki-echarts/tiddlers/core/widget.js
@@ -6,7 +6,7 @@ module-type: widget
 tiddlywiki-echarts widget
 
 \*/
-(function() {
+(function () {
   /*jslint node: true, browser: true */
   /*global $tw: false */
   'use strict';
@@ -14,8 +14,9 @@ tiddlywiki-echarts widget
   if (!$tw.browser) return;
 
   const Widget = require('$:/core/modules/widgets/widget.js').widget;
+  const echarts = require('echarts.min.js');
 
-  class DivWidget extends Widget {
+  class EchartWidget extends Widget {
     constructor(parseTreeNode, options) {
       super(parseTreeNode, options);
     }
@@ -25,27 +26,41 @@ tiddlywiki-echarts widget
       this.computeAttributes();
       this.execute();
 
-      const param = this.getAttribute('param', 'Test Param');
+      // const time = new Date().getTime();
+      const id = 'echartsId';
+      const div = this.document.createElement('div');
+      div.id = id;
+      div.textContent = 'Loading ...';
+      div.style.width = '600px';
+      div.style.height = '400px';
+      div.className = '';
+      parent.insertBefore(div, nextSibling);
+      this.domNodes.push(div);
 
-      const divNode = this.document.createElement('div');
-      divNode.id = 'divId';
-      divNode.textContent = param;
-      divNode.className = 'divClass';
-      parent.insertBefore(divNode, nextSibling);
-      this.domNodes.push(divNode);
-    }
+      const chartContainer = document.getElementById(id);
+      const myChart = echarts.init(chartContainer);
 
-    refresh() {
-      var changedAttributes = this.computeAttributes();
-      // changedAttributes.title;
-      if (Object.keys(changedAttributes).length > 0) {
-        this.refreshSelf();
-        return true;
-      } else {
-        return false;
-      }
+      const option = {
+        title: {
+          text: '销量统计',
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      };
+
+      myChart.setOption(option);
     }
   }
 
-  exports['div-widget'] = DivWidget;
+  exports.oecharts = EchartWidget;
 })();
