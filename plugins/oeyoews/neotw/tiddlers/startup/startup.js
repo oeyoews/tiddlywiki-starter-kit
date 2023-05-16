a575c50d6fa120565444d8d9c0b9722b8c3bc15c
oeyoews <jyao4783@gmail.com>
2023-05-16 12:12:56 +0800

    Automatic commit

--- plugins/oeyoews/neotw/tiddlers/startup/startup.js
+++ plugins/oeyoews/neotw/tiddlers/startup/startup.js
@@ -16,10 +16,10 @@ neotw startup descption
       return;
     }
 
-    fetch('https://api.github.com/repos/oeyoews/neotw/commits/main')
-      /* 这行代码是一个箭头函数，用于将响应体转换成一个JSON格式的对象。在fetch函数中，响应体返回的是一个ReadableStream对象，需要通过调用json()方法来获取JSON格式的数据。
+    /* 这行代码是一个箭头函数，用于将响应体转换成一个JSON格式的对象。在fetch函数中，响应体返回的是一个ReadableStream对象，需要通过调用json()方法来获取JSON格式的数据。
 response.json()方法返回一个Promise对象，该Promise对象会在解析完成后，resolve一个JSON格式的对象。由于Promise对象的then方法可以接受一个回调函数作为参数，因此我们可以直接将response.json()作为回调函数传递给第一个then方法，让它自动处理响应数据并返回一个JSON格式的对象。
 简单来说，这行代码是对响应数据进行了一次转换操作，可以使得后续的代码更加方便地使用响应数据，同时也提高了代码的可读性和易用性。 */
+    /* fetch('https://api.github.com/repos/oeyoews/neotw/commits/main')
       .then(response => response.json())
       .then(data =>
         console.log(
@@ -27,7 +27,7 @@ response.json()方法返回一个Promise对象，该Promise对象会在解析完
           'background: linear-gradient(to right, #12c2e9, #c471ed); color: black;padding: 3px;',
         ),
       )
-      .catch(error => console.error(error));
+      .catch(error => console.error(error)); */
     // 获取仓库的 tag 列表并选择最新的那个 tag
     fetch('https://api.github.com/repos/oeyoews/neotw/tags')
       .then(response => response.json())
