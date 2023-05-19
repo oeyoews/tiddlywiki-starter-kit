b31b3d9aa8679ba4166156616fb89e6de6db86a9
oeyoews <jyao4783@gmail.com>
2023-05-19 19:10:30 +0800

    fix: add missing #

--- tiddlers/content/doc1.js
+++ tiddlers/content/doc1.js
@@ -3,6 +3,6 @@ docsearch({
     appId: '#J3KXNDI6EY',
     apiKey: 'd5a043d7612fcecb99fb85cbdce1653f',
     indexName: 'neotw-oeyoewl',
-    container: 'docsearch'
+    container: '#docsearch'
   });
 ``</script>`
