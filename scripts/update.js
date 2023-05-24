5f4ee69e68654f0e3ffdd8c61e27538aae861670
oeyoews <jyao4783@gmail.com>
2023-05-24 12:59:08 +0800

    Updated scripts/update.js

--- scripts/update.js
+++ scripts/update.js
@@ -3,32 +3,48 @@ const simpleGit = require('simple-git');
 // 初始化 simple-git
 const git = simpleGit();
 
-// 检查文件状态
-git.status((err, status) => {
-  if (err) {
-    console.log(err);
-  } else {
-    // 如果有修改或新增的文件
-    if (status.modified.length || status.not_added.length) {
-      // 添加所有文件到暂存区
-      git
-        .add('.')
-        .then(() => {
-          // 提交所有文件
-          git
-            .commit('Commit message')
-            .then(() => {
-              console.log('Changes committed!');
-              git
-                .push('origin', 'main')
-                .then(() => console.log('Changes pushed to remote repository.'))
-                .catch(err => console.error('Failed to push changes:', err));
-            })
-            .catch(err => console.error(err));
-        })
-        .catch(err => console.error(err));
-    } else {
+// 获取修改过的文件列表
+git
+  .status()
+  .then(status => {
+    const modifiedFiles = status.modified;
+    const addedFiles = status.not_added;
+    const allFiles = [...modifiedFiles, ...addedFiles];
+
+    // 如果没有修改或新增的文件
+    if (allFiles.length === 0) {
       console.log('No changes to commit.');
+      return;
     }
-  }
-});
+
+    // 添加所有文件到暂存区
+    return git
+      .add('.')
+      .then(() => console.log('Files added to staging area.'))
+      .then(() => {
+        // 依次提交所有文件
+        return allFiles.reduce((promise, file) => {
+          return promise.then(() => {
+            console.log(`Committing changes for file ${file}...`);
+            return git
+              .commit(`Updated ${file}`)
+              .then(() => console.log(`Changes for file ${file} committed!`))
+              .catch(err =>
+                console.error(
+                  `Failed to commit changes for file ${file}:`,
+                  err,
+                ),
+              );
+          });
+        }, Promise.resolve());
+      })
+      .then(() => {
+        // 推送到远程仓库
+        return git
+          .push('origin', 'main')
+          .then(() => console.log('Changes pushed to remote repository.'))
+          .catch(err => console.error('Failed to push changes:', err));
+      })
+      .catch(err => console.error('Failed to commit and push changes:', err));
+  })
+  .catch(err => console.error('Failed to get status:', err));
