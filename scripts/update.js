9bc002b8271779f5efbec4c714c7d5b53febf580
oeyoews <jyao4783@gmail.com>
2023-05-24 13:00:37 +0800

    Updated scripts/update.js

--- scripts/update.js
+++ scripts/update.js
@@ -3,6 +3,50 @@ const simpleGit = require('simple-git');
 // 初始化 simple-git
 const git = simpleGit();
 
+// 定义一个异步函数来提交所有修改过的文件
+async function commitChanges() {
+  try {
+    // 获取修改过的文件列表
+    const status = await git.status();
+    const modifiedFiles = status.modified;
+    const addedFiles = status.not_added;
+    const allFiles = [...modifiedFiles, ...addedFiles];
+
+    // 如果没有修改或新增的文件
+    if (allFiles.length === 0) {
+      console.log('No changes to commit.');
+      return;
+    }
+
+    // 添加所有文件到暂存区
+    await git.add('.');
+
+    console.log('Files added to staging area.');
+
+    // 依次提交所有文件
+    for (const file of allFiles) {
+      console.log(`Committing changes for file ${file}...`);
+      await git.commit(`Updated ${file}`);
+      console.log(`Changes for file ${file} committed!`);
+    }
+
+    // 推送到远程仓库
+    await git.push('origin', 'main');
+
+    console.log('Changes pushed to remote repository.');
+  } catch (err) {
+    console.error('Failed to commit and push changes:', err);
+  }
+}
+
+// 调用异步函数来提交所有修改过的文件
+commitChanges();
+
+/* const simpleGit = require('simple-git');
+
+// 初始化 simple-git
+const git = simpleGit();
+
 // 获取修改过的文件列表
 git
   .status()
@@ -47,4 +91,4 @@ git
       })
       .catch(err => console.error('Failed to commit and push changes:', err));
   })
-  .catch(err => console.error('Failed to get status:', err));
+  .catch(err => console.error('Failed to get status:', err)); */
