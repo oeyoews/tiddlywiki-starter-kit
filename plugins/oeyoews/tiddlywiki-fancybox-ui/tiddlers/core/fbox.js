3b89614b78de9629aed4be265243ca650ee8c2f3
oeyoews <jyao4783@gmail.com>
2023-05-27 20:08:12 +0800

    perf: remove aNode

--- plugins/oeyoews/tiddlywiki-fancybox-ui/tiddlers/core/fbox.js
+++ plugins/oeyoews/tiddlywiki-fancybox-ui/tiddlers/core/fbox.js
@@ -5,7 +5,6 @@ module-type: widget
 
 fancybox widget
 \*/
-
 (function () {
   /*jslint node: true, browser: true */
   /*global $tw: false */
@@ -29,39 +28,41 @@ fancybox widget
         'src',
         'https://source.unsplash.com/random',
       );
+
       const alt = this.getAttribute('alt', '');
       const width = this.getAttribute('width', '256');
-
       const currentTiddler = this.getVariable('currentTiddler') || 'gallary';
 
-      const aNode = this.document.createElement('a');
       const warnNode = this.document.createElement('span');
       warnNode.style.color = 'red';
       warnNode.style.fontSize = '50%';
-      const imgNode = this.document.createElement('img');
 
-      if (src === '') {
+      if (!src) {
         warnNode.textContent = 'fbox widget no src param';
         parent.insertBefore(warnNode, nextSibling);
         this.domNodes.push(warnNode);
         return;
       }
+
       if (!src.startsWith('http')) {
         warnNode.textContent = 'fbox目前仅仅支持在线图片';
         parent.insertBefore(warnNode, nextSibling);
         this.domNodes.push(warnNode);
         return;
       }
-      aNode.href = src;
-      imgNode.src = src;
-      imgNode.alt = alt;
-      imgNode.width = width;
-      imgNode.setAttribute('loading', 'lazy');
-      aNode.setAttribute('data-fancybox', currentTiddler);
-      aNode.setAttribute('data-caption', alt);
-      aNode.appendChild(imgNode);
-      parent.insertBefore(aNode, nextSibling);
-      this.domNodes.push(aNode);
+
+      const imgNode = $tw.utils.domMaker('img', {
+        class: 'cursor-pointer',
+        attributes: {
+          src,
+          alt,
+          width,
+          loading: 'lazy',
+          'data-fancybox': currentTiddler,
+        },
+      });
+      parent.insertBefore(imgNode, nextSibling);
+      this.domNodes.push(imgNode);
     }
   }
 
