f1341de932716fed1bdf181edcf2580a3caef377
oeyoews <jyao4783@gmail.com>
2023-05-27 20:39:26 +0800

    Automatic commit

--- archive/plugins/tiddlywiki-fancybox/tiddlers/widget-fancybox.js
+++ archive/plugins/tiddlywiki-fancybox/tiddlers/widget-fancybox.js
@@ -26,44 +26,23 @@ fancybox widget
       this.computeAttributes();
       this.execute();
 
-      // const currentTiddler = $tw.wiki.getTiddlerText('$:/temp/focussedTiddler');
-
-      // const local = this.getAttribute('local', 'no');
       const src = this.getAttribute('src', '');
-      // const imgTiddler = $tw.wiki.getTiddler(src)?.fields || {};
-      // const imgTiddlerBase64 = imgTiddler.text;
-
       const alt = this.getAttribute('alt', '');
-      const id = this.getAttribute('id', '');
       const width = this.getAttribute('width', '256');
 
-      const aNode = this.document.createElement('a');
-      const warnNode = this.document.createElement('span');
-      warnNode.style.color = 'red';
-      warnNode.style.fontSize = '50%';
-      const imgNode = this.document.createElement('img');
+      const imgNode = $tw.utils.domMaker('img', {
+        class: '',
+        attributes: {
+          src,
+          alt,
+          width,
+          'data-fancybox': this.getVariable('currentTiddler'),
+          'data-caption': alt,
+        },
+      });
 
-      if (src === '') {
-        warnNode.textContent = 'fbox widget no src param';
-        parent.insertBefore(warnNode, nextSibling);
-        this.domNodes.push(warnNode);
-        return;
-      }
-      if (!src.startsWith('http')) {
-        warnNode.textContent = 'fbox目前仅仅支持在线图片';
-        parent.insertBefore(warnNode, nextSibling);
-        this.domNodes.push(warnNode);
-        return;
-      }
-      aNode.href = src;
-      imgNode.src = src;
-      imgNode.alt = alt;
-      imgNode.width = width;
-      aNode.setAttribute('data-fancybox', this.getVariable('currentTiddler'));
-      aNode.setAttribute('data-caption', alt);
-      aNode.appendChild(imgNode);
-      parent.insertBefore(aNode, nextSibling);
-      this.domNodes.push(aNode);
+      parent.insertBefore(imgNode, nextSibling);
+      this.domNodes.push(imgNode);
     }
   }
 
