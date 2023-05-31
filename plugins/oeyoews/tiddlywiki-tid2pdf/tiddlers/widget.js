94fee829a2b7a0fe22d537122bc7d45f404ccbea
oeyoews <jyao4783@gmail.com>
2023-05-31 18:43:19 +0800

    Automatic commit

--- plugins/oeyoews/tiddlywiki-tid2pdf/tiddlers/widget.js
+++ plugins/oeyoews/tiddlywiki-tid2pdf/tiddlers/widget.js
@@ -28,31 +28,33 @@ tid2pdf/widget
 
       const title = this.getVariable('currentTiddler');
       const param = this.getAttribute('param', `Download ${title}.pdf`);
-
       const html2canvas = require('html2canvas.min.js');
-      const jspdf = require('jspdf.umd.min.js');
+      const jsPDF = require('jspdf.umd.min.js').jsPDF;
 
-      const buttonNode = this.document.createElement('button');
-      buttonNode.textContent = param;
-      buttonNode.className =
-        'bg-blue-200 hover:bg-blue-300 duration-200 transition rounded-sm py-1 px-2 transition duration-200 m-1';
+      const buttonNode = $tw.utils.domMaker('button', {
+        text: param,
+        class:
+          'bg-blue-200 hover:bg-blue-300 duration-200 transition rounded-sm py-1 px-2 transition duration-200 m-1',
+        attributes: {},
+      });
+
       buttonNode.addEventListener('click', function () {
-        var doc = new jspdf.jsPDF();
+        var doc = new jsPDF();
 
         const selector = `[data-tiddler-title="${title}"]`;
 
         var elements = document.querySelector(selector);
 
         html2canvas(elements, {
-          width: 1000,
+          width: 1200,
+          height: 3200,
         }).then(function (canvas) {
           var imgData = canvas.toDataURL('image/png');
           var ratio = canvas.width / canvas.height; // 计算 HTML 元素的宽高比例
           var pdfWidth = doc.internal.pageSize.getWidth();
           var pdfHeight = pdfWidth / ratio; // 根据页面大小调整 PDF 的高度
-
           doc.addImage(imgData, 'PNG', 10, 10, pdfWidth - 20, pdfHeight);
-          doc.save(`${title}.pdf`);
+          doc.save(`${title}-${pdfWidth}-${pdfHeight}.pdf`);
         });
       });
 
