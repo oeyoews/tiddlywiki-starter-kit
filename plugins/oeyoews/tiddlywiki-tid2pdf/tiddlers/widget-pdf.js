71f69654ac6629c51fa5fea934446dfa193c2f8e
oeyoews <jyao4783@gmail.com>
2023-06-05 18:25:55 +0800

    Automatic commit

--- plugins/oeyoews/tiddlywiki-tid2pdf/tiddlers/widget-pdf.js
+++ plugins/oeyoews/tiddlywiki-tid2pdf/tiddlers/widget-pdf.js
@@ -1,11 +1,12 @@
 /*\
-title: tid2pdf/widget
+title: tid2pdf/widget-pdf
 type: application/javascript
 module-type: widget
 
 tid2pdf/widget
 
 \*/
+// 如果高度过低, 显示效果可能不能达到最佳效果
 (function () {
   /*jslint node: true, browser: true */
   /*global $tw: false */
@@ -25,60 +26,50 @@ tid2pdf/widget
       this.computeAttributes();
       this.execute();
 
-      // support focused tiddler
       const title = this.getVariable('currentTiddler');
       const param = this.getAttribute('param', `Download ${title}.pdf`);
+      const html2canvas = require('html2canvas.min.js');
+      const jsPDF = require('jspdf.umd.min.js').jsPDF;
 
+      const classNames =
+        'bg-blue-200 hover:bg-blue-300 duration-200 transition rounded-sm py-1 px-2 transition duration-200 m-1';
       const buttonNode = $tw.utils.domMaker('button', {
         text: param,
-        class:
-          'bg-blue-200 hover:bg-blue-300 duration-200 transition rounded-sm py-1 px-2 transition duration-200 m-1',
-        attributes: {},
+        class: classNames,
       });
 
       const selector = `[data-tiddler-title="${title}"]`;
-      const element = document.querySelector(selector);
-
-      buttonNode.addEventListener('click', async () => {
-        await this.exportPdf(element, `${title}.pdf`);
-      });
-
-      parent.insertBefore(buttonNode, nextSibling);
-      this.domNodes.push(buttonNode);
-    }
-
-    async exportPdf(element, filename = 'untitled.pdf') {
-      confetti();
-      NProgress.start();
-      const html2canvas = require('html2canvas.min.js');
-      const { jsPDF } = require('jspdf.umd.min.js');
-
-      const container = document.createElement('div');
-      container.appendChild(element.cloneNode(true));
-      document.body.appendChild(container);
+      const elements = document.querySelector(selector);
 
       const scale = 1.5;
-      const render = function () {
-        html2canvas(container, {
+      buttonNode.addEventListener('click', function () {
+        NProgress.start();
+        html2canvas(elements, {
           allowTaint: true,
           useCORS: true,
           scale,
         }).then(function (canvas) {
           const aspectRatio = canvas.width / canvas.height;
-          const PDF_WIDTH = 210; // A4 size in mm
+          const PDF_WIDTH = 210;
           const PDF_HEIGHT = PDF_WIDTH / aspectRatio;
+
           const doc = new jsPDF({
             orientation: 'portrait', // landscape
-            unit: 'px',
+            unit: 'mm',
             format: [PDF_WIDTH, PDF_HEIGHT],
           });
-          doc.addImage(canvas, 'PNG', 0, 0, PDF_WIDTH, PDF_HEIGHT);
-          doc.save(filename);
-          container.remove();
+
+          const imgData = canvas.toDataURL('image/png');
+          doc.addImage(imgData, 'PNG', 0, 0, PDF_WIDTH, PDF_HEIGHT);
+          // doc.text(`This is a footer`, 10, PDF_HEIGHT - 10);
+          doc.save(`${title}.pdf`);
           NProgress.done();
+          confetti();
         });
-      };
-      render();
+      });
+
+      parent.insertBefore(buttonNode, nextSibling);
+      this.domNodes.push(buttonNode);
     }
   }
 
