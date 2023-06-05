dab55ef817a9b3d6700bea3867f67c52051cf68f
oeyoews <jyao4783@gmail.com>
2023-06-05 18:40:38 +0800

    Automatic commit

--- plugins/oeyoews/tiddlywiki-back-to-top/tiddlers/core/nprogress.js
+++ plugins/oeyoews/tiddlywiki-back-to-top/tiddlers/core/nprogress.js
@@ -17,9 +17,9 @@ nprogress module
   exports.synchronous = true;
   exports.startup = () => {
     window.NProgress = require('nprogress.min.js');
+    NProgress.start();
     const startTime = performance.now();
     window.onload = function () {
-      NProgress.start();
       NProgress.done();
       const endTime = performance.now();
       const loadTime = Math.floor(endTime - startTime);
