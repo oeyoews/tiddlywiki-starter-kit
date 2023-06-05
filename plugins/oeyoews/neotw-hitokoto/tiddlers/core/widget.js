d56a106f0eb25bb67301fd4f2123e7d8aee3aa30
oeyoews <jyao4783@gmail.com>
2023-06-05 19:16:45 +0800

    Automatic commit

--- plugins/oeyoews/neotw-hitokoto/tiddlers/core/widget.js
+++ plugins/oeyoews/neotw-hitokoto/tiddlers/core/widget.js
@@ -18,6 +18,7 @@ Hitokoto widget
     constructor(parseTreeNode, options) {
       super(parseTreeNode, options);
       this.executing = false;
+      this.hitokotoNode = '';
     }
 
     render(parent, nextSibling) {
@@ -25,7 +26,7 @@ Hitokoto widget
       this.computeAttributes();
       this.execute();
 
-      const hitokotoNode = $tw.utils.domMaker('div', {
+      this.hitokotoNode = $tw.utils.domMaker('div', {
         text: 'Loading ...',
         class:
           'text-transparent bg-clip-text bg-gradient-to-r from-teal-400 via-pink-500 cursor-pointer text-xs',
@@ -35,33 +36,43 @@ Hitokoto widget
           {
             name: 'click',
             handlerObject: this,
-            handlerMethod: '',
+            handlerMethod: 'handlerClick',
           },
         ],
       });
+      this.fetchHitokoto();
 
-      parent.insertBefore(hitokotoNode, nextSibling);
-      this.domNodes.push(hitokotoNode);
-
-      const fetchHitokoto = async () => {
-        const response = await fetch('https://v1.hitokoto.cn');
-        const { hitokoto: hitokotoText, from } = await response.json();
-        const hitokotoFrom = '@' + from;
-        hitokotoNode.textContent = `${hitokotoText} ${hitokotoFrom}`;
-      };
-
-      fetchHitokoto();
-
-      hitokotoNode.addEventListener('click', () => {
-        const _ = require('lodash.min.js');
-        const throttleHitokotoHandleClick = _.throttle(
-          fetchHitokoto.bind(this),
-          1000,
-        );
-
-        throttleHitokotoHandleClick();
-      });
+      parent.insertBefore(this.hitokotoNode, nextSibling);
+      this.domNodes.push(this.hitokotoNode);
     }
+
+    fetchHitokoto = async () => {
+      const response = await fetch('https://v1.hitokoto.cn');
+      const { hitokoto: hitokotoText, from } = await response.json();
+      const hitokotoFrom = '@' + from;
+      this.hitokotoNode.textContent = `${hitokotoText} ${hitokotoFrom}`;
+    };
+
+    throttle(func, delay) {
+      let timer = null;
+      let lastCallTime = 0;
+      const throttled = function () {
+        const now = new Date().getTime();
+        const timeSinceLastCall = now - lastCallTime;
+        if (!timer || timeSinceLastCall >= delay) {
+          func.apply(this, arguments);
+          lastCallTime = now;
+        } else {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+          timer = null;
+        }, delay);
+      };
+      return throttled;
+    }
+
+    handlerClick = this.throttle(this.fetchHitokoto, 1000);
   }
 
   exports.hitokoto = Hitokoto;
