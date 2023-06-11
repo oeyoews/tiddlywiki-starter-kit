082bd038da6e0c8312071894bf778e6ba8945d8f
oeyoews <jyao4783@gmail.com>
2023-06-11 12:16:34 +0800

    feat: support emoji toc task container extensions

--- plugins/oeyoews/markdown/wrapper.js
+++ plugins/oeyoews/markdown/wrapper.js
@@ -6,259 +6,371 @@ module-type: parser
 Wraps up the markdown-it parser for use as a Parser in TiddlyWiki
 
 \*/
-(function(){
+(function () {
+  /*jslint node: true, browser: true */
+  /*global $tw: false */
+  'use strict';
 
-/*jslint node: true, browser: true */
-/*global $tw: false */
-"use strict";
+  var MarkdownIt = require('./markdown-it');
 
-var MarkdownIt = require("./markdown-it");
+  function parseAsBoolean(tiddlerName) {
+    return (
+      $tw.wiki.getTiddlerText(tiddlerName, 'false').trim().toLowerCase() ===
+      'true'
+    );
+  }
 
-function parseAsBoolean(tiddlerName) {
-	return $tw.wiki.getTiddlerText(tiddlerName,"false").trim().toLowerCase() === "true";
-}
+  var pluginOpts = {
+    renderWikiText: parseAsBoolean('$:/config/markdown/renderWikiText'),
+    renderWikiTextPragma: $tw.wiki
+      .getTiddlerText('$:/config/markdown/renderWikiTextPragma')
+      .trim(),
+  };
 
-var pluginOpts = {
-	renderWikiText: parseAsBoolean("$:/config/markdown/renderWikiText"),
-	renderWikiTextPragma: $tw.wiki.getTiddlerText("$:/config/markdown/renderWikiTextPragma").trim()
-};
+  var markdownOpts = {
+    html: true,
+    xhtmlOut: true,
+    breaks: parseAsBoolean('$:/config/markdown/breaks'),
+    quotes: $tw.wiki.getTiddlerText('$:/config/markdown/quotes').trim(),
+    typographer: parseAsBoolean('$:/config/markdown/typographer'),
+    linkify: parseAsBoolean('$:/config/markdown/linkify'),
+  };
 
-var markdownOpts = {
-	html: true,
-	xhtmlOut: true,
-	breaks: parseAsBoolean("$:/config/markdown/breaks"),
-	quotes: $tw.wiki.getTiddlerText("$:/config/markdown/quotes").trim(),
-	typographer: parseAsBoolean("$:/config/markdown/typographer"),
-	linkify: parseAsBoolean("$:/config/markdown/linkify")
-};
+  // Retrieve needed TW rule classes and instantiated rules
+  function setupWikiRules(pluginOptions) {
+    var results = {};
 
-// Retrieve needed TW rule classes and instantiated rules
-function setupWikiRules(pluginOptions) {
-	var results = {};
+    function collectAllRules(classes, type) {
+      var rulesInfo = [],
+        key,
+        self = wikiParser;
+      for (key in classes) {
+        // instantiate the rule
+        var RuleClass = classes[key];
+        var rule = new RuleClass(self);
+        rule.name = key;
+        rule.class = RuleClass;
+        rule.is = {};
+        rule.is[type] = true;
+        rule.init(self);
 
-	function collectAllRules(classes,type) {
-		var rulesInfo = [], key,
-			self = wikiParser;
-		for(key in classes) {
-			// instantiate the rule
-			var RuleClass = classes[key];
-			var rule = new RuleClass(self);
-			rule.name = key;
-			rule.class = RuleClass;
-			rule.is = {};
-			rule.is[type] = true;
-			rule.init(self);
+        rulesInfo.push({
+          rule: rule,
+          matchIndex: -1,
+        });
+      }
+      return rulesInfo;
+    }
 
-			rulesInfo.push({
-				rule: rule,
-				matchIndex: -1
-			});
-		};
-		return rulesInfo;
-	}
+    var WikiParser =
+      require('$:/core/modules/parsers/wikiparser/wikiparser.js')[
+        'text/vnd.tiddlywiki'
+      ];
 
-	var WikiParser = require("$:/core/modules/parsers/wikiparser/wikiparser.js")["text/vnd.tiddlywiki"];
+    // first pass: get all rule classes
+    var wikiParser = new WikiParser(null, '', {
+      parseAsInline: true,
+      wiki: $tw.wiki,
+    });
 
-	// first pass: get all rule classes
-	var wikiParser = new WikiParser(null, '', {parseAsInline: true, wiki: $tw.wiki});
+    // restore all possible rules from each rule class
+    wikiParser.pragmaRules = collectAllRules(
+      wikiParser.pragmaRuleClasses,
+      'pragma',
+    );
+    wikiParser.blockRules = collectAllRules(
+      wikiParser.blockRuleClasses,
+      'block',
+    );
+    wikiParser.inlineRules = collectAllRules(
+      wikiParser.inlineRuleClasses,
+      'inline',
+    );
 
-	// restore all possible rules from each rule class
-	wikiParser.pragmaRules = collectAllRules(wikiParser.pragmaRuleClasses,'pragma');
-	wikiParser.blockRules = collectAllRules(wikiParser.blockRuleClasses,'block');
-	wikiParser.inlineRules = collectAllRules(wikiParser.inlineRuleClasses,'inline');
+    var pragma = pluginOptions.renderWikiText
+      ? '\\rules except latex-parser extlink\n' +
+        pluginOptions.renderWikiTextPragma
+      : '\\rules only html entity commentinline commentblock';
 
-	var pragma = pluginOptions.renderWikiText
-			? "\\rules except latex-parser extlink\n" + pluginOptions.renderWikiTextPragma
-			: "\\rules only html entity commentinline commentblock";
+    wikiParser.pos = 0;
+    wikiParser.source = pragma;
+    wikiParser.sourceLength = pragma.length;
 
-	wikiParser.pos = 0;
-	wikiParser.source = pragma;
-	wikiParser.sourceLength = pragma.length;
+    // second pass: remove uninterested rules based on \rules pragma
+    wikiParser.parsePragmas();
 
-	// second pass: remove uninterested rules based on \rules pragma
-	wikiParser.parsePragmas();
+    results.blockRules = {};
+    results.inlineRules = {};
+    results.blockRuleClasses = {};
+    results.inlineRuleClasses = {};
 
-	results.blockRules = {};
-	results.inlineRules = {}
-	results.blockRuleClasses = {};
-	results.inlineRuleClasses = {};
+    // save the rule sets for future markdown parsing
+    wikiParser.blockRules.forEach(function (ruleinfo) {
+      results.blockRules[ruleinfo.rule.name] = ruleinfo;
+      results.blockRuleClasses[ruleinfo.rule.name] = ruleinfo.rule.class;
+    });
+    wikiParser.inlineRules.forEach(function (ruleinfo) {
+      results.inlineRules[ruleinfo.rule.name] = ruleinfo;
+      results.inlineRuleClasses[ruleinfo.rule.name] = ruleinfo.rule.class;
+    });
+    return results;
+  }
 
-	// save the rule sets for future markdown parsing
-	wikiParser.blockRules.forEach(function(ruleinfo) {
-		results.blockRules[ruleinfo.rule.name] = ruleinfo;
-		results.blockRuleClasses[ruleinfo.rule.name] = ruleinfo.rule.class;
-	});
-	wikiParser.inlineRules.forEach(function(ruleinfo) {
-		results.inlineRules[ruleinfo.rule.name] = ruleinfo;
-		results.inlineRuleClasses[ruleinfo.rule.name] = ruleinfo.rule.class;
-	});
-	return results;
-}
+  // Creates markdown-it parser
+  function createMarkdownEngine(markdownItOptions, pluginOptions) {
+    const container = require('./markdown-it-container');
+    var md = new MarkdownIt(markdownItOptions)
+      .use(require('./markdown-it-sub'))
+      .use(require('./markdown-it-sup'))
+      .use(require('./markdown-it-ins'))
+      .use(require('./markdown-it-mark'))
+      .use(require('./markdown-it-footnote'))
+      .use(require('./markdown-it-emoji'))
+      .use(require('./markdown-it-task'))
+      .use(require('./markdown-it-toc'))
 
-// Creates markdown-it parser
-function createMarkdownEngine(markdownItOptions, pluginOptions) {
-	var md = new MarkdownIt(markdownItOptions)
-				.use(require("./markdown-it-sub"))
-				.use(require("./markdown-it-sup"))
-				.use(require("./markdown-it-ins"))
-				.use(require("./markdown-it-mark"))
-				.use(require("./markdown-it-footnote"))
-				.use(require("./markdown-it-deflist"));
+      .use(container, 'todo', {
+        render: function (tokens, idx) {
+          if (tokens[idx].nesting === 1) {
+            return (
+              '<div class="bg-green-100 rounded-md border-left border-l-4 border-green-500 text-green-700 px-1 my-2">\n' +
+              '<div class="font-bold">✅ 任务</div>' +
+              '<div class="content">'
+            );
+          } else {
+            return '</div>\n</div>\n';
+          }
+        },
+      })
 
-	var results = setupWikiRules(pluginOptions);
+      // 定义 warning 容器
+      .use(container, 'warning', {
+        render: function (tokens, idx) {
+          if (tokens[idx].nesting === 1) {
+            return (
+              '<div class="bg-yellow-100 rounded-md border-left border-l-4 border-yellow-500 text-yellow-700 px-2 my-2">\n' +
+              '<div class="font-bold">注意</div>' +
+              '<div class="content">'
+            );
+          } else {
+            return '</div>\n</div>\n';
+          }
+        },
+      })
 
-	MarkdownParser.prototype.blockRuleClasses = results.blockRuleClasses;
-	MarkdownParser.prototype.blockRules = results.blockRules;
+      // 定义 info 容器
+      .use(container, 'info', {
+        render: function (tokens, idx) {
+          if (tokens[idx].nesting === 1) {
+            return (
+              '<div class="bg-blue-100 rounded-md border-left border-l-4 border-blue-500 text-blue-700 px-1 my-2">\n' +
+              '<div class="font-bold">提示</div>' +
+              '<div class="content">'
+            );
+          } else {
+            return '</div>\n</div>\n';
+          }
+        },
+      })
+      .use(container, 'error', {
+        render: function (tokens, idx) {
+          if (tokens[idx].nesting === 1) {
+            return (
+              '<div class="bg-red-100 border-left rounded-md border-l-4 border-red-500 text-red-700 px-1 my-10">\n' +
+              '<div class="font-bold">警告</div>' +
+              '<div class="content">'
+            );
+          } else {
+            return '</div>\n</div>\n';
+          }
+        },
+      })
+      .use(require('./markdown-it-deflist'));
 
-	MarkdownParser.prototype.inlineRuleClasses = results.inlineRuleClasses;
-	MarkdownParser.prototype.inlineRules = results.inlineRules;
+    var results = setupWikiRules(pluginOptions);
 
-	if(pluginOptions.renderWikiText && $tw.modules.titles["$:/plugins/tiddlywiki/katex/katex.min.js"]) {
-		md.use(require("./markdown-it-katex"));
-	}
+    MarkdownParser.prototype.blockRuleClasses = results.blockRuleClasses;
+    MarkdownParser.prototype.blockRules = results.blockRules;
 
-	md.use(require("./markdown-it-tiddlywiki"),{
-			renderWikiText: pluginOptions.renderWikiText,
-			blockRules: results.blockRules,
-			inlineRules: results.inlineRules
-		});
+    MarkdownParser.prototype.inlineRuleClasses = results.inlineRuleClasses;
+    MarkdownParser.prototype.inlineRules = results.inlineRules;
 
-	$tw.utils.each(['image','prettylink','prettyextlink'], function(rule) {
-		if(MarkdownParser.prototype.inlineRules[rule]) {
-			// delegate to md; ignore the rule class in WikiParser
-			delete MarkdownParser.prototype.inlineRuleClasses[rule];
-		}
-	});
-	return md;
-}
+    if (
+      pluginOptions.renderWikiText &&
+      $tw.modules.titles['$:/plugins/tiddlywiki/katex/katex.min.js']
+    ) {
+      md.use(require('./markdown-it-katex'));
+    }
 
-/// Parse tree post processing ///
+    md.use(require('./markdown-it-tiddlywiki'), {
+      renderWikiText: pluginOptions.renderWikiText,
+      blockRules: results.blockRules,
+      inlineRules: results.inlineRules,
+    });
 
-function deactivateLinks(tree) {
-	$tw.utils.each(tree,function(node) {
-		if(node.type === "link") {
-			node.type = "text";
-			node.text = node.children[0].text;
-			delete node.attributes;
-			delete node.children;
-			delete node.attributes;
-		} else {
-			deactivateLinks(node.children);
-		}
-	});
-}
+    $tw.utils.each(['image', 'prettylink', 'prettyextlink'], function (rule) {
+      if (MarkdownParser.prototype.inlineRules[rule]) {
+        // delegate to md; ignore the rule class in WikiParser
+        delete MarkdownParser.prototype.inlineRuleClasses[rule];
+      }
+    });
+    return md;
+  }
 
-// true if the node contains "_codified_" class attribute
-function isCodified(node) {
-	return node.attributes
-		&& node.attributes.class
-		&& node.attributes.class.type === "string"
-		&& (node.attributes.class.value.split(" ").indexOf("_codified_") !== -1);
-}
+  /// Parse tree post processing ///
 
-function decodeEntities(s) {
-	return s.replace(/(&#?[a-zA-Z0-9]{2,8};)/g,$tw.utils.entityDecode);
-}
+  function deactivateLinks(tree) {
+    $tw.utils.each(tree, function (node) {
+      if (node.type === 'link') {
+        node.type = 'text';
+        node.text = node.children[0].text;
+        delete node.attributes;
+        delete node.children;
+        delete node.attributes;
+      } else {
+        deactivateLinks(node.children);
+      }
+    });
+  }
 
-// Add e"..." and e'....' syntax to enable decoding of HTML entities
-// in string literals.
-function parseStringLiteralExtended(source,pos) {
-	var node = {
-		type: "string",
-		start: pos
-	};
-	var reString = /(?:"""([\s\S]*?)"""|e?"([^"]*)")|(?:e?'([^']*)')/g;
-	reString.lastIndex = pos;
-	var match = reString.exec(source);
-	if(match && match.index === pos) {
-		node.value = match[1] !== undefined ? match[1] :
-			(match[2] !== undefined ? match[2] : match[3]);
-		node.end = pos + match[0].length;
-		if(match[0].charAt(0) === "e") {
-			node.value = decodeEntities(node.value);
-		}
-		return node;
-	} else {
-		return null;
-	}
-}
+  // true if the node contains "_codified_" class attribute
+  function isCodified(node) {
+    return (
+      node.attributes &&
+      node.attributes.class &&
+      node.attributes.class.type === 'string' &&
+      node.attributes.class.value.split(' ').indexOf('_codified_') !== -1
+    );
+  }
 
-function processWikiTree(tree,hasWikiLinkRule) {
-	var stack = [].concat(tree);
+  function decodeEntities(s) {
+    return s.replace(/(&#?[a-zA-Z0-9]{2,8};)/g, $tw.utils.entityDecode);
+  }
 
-	var mergeable  = function(node) {
-		return node.type === "element" && node.tag === "p" && (!node.attributes || Object.keys(node.attributes).length === 0);
-	};
+  // Add e"..." and e'....' syntax to enable decoding of HTML entities
+  // in string literals.
+  function parseStringLiteralExtended(source, pos) {
+    var node = {
+      type: 'string',
+      start: pos,
+    };
+    var reString = /(?:"""([\s\S]*?)"""|e?"([^"]*)")|(?:e?'([^']*)')/g;
+    reString.lastIndex = pos;
+    var match = reString.exec(source);
+    if (match && match.index === pos) {
+      node.value =
+        match[1] !== undefined
+          ? match[1]
+          : match[2] !== undefined
+          ? match[2]
+          : match[3];
+      node.end = pos + match[0].length;
+      if (match[0].charAt(0) === 'e') {
+        node.value = decodeEntities(node.value);
+      }
+      return node;
+    } else {
+      return null;
+    }
+  }
 
-	while(stack.length) {
-		var node = stack.pop();
-		if(node.type === "element" && node.tag === "p") {
-			// reduce nested <p> nodes
-			while(node.children && node.children.length === 1 && mergeable(node.children[0])) {
-				node.children = node.children[0].children;
-			}
-		} else if(hasWikiLinkRule && isCodified(node)) {
-			deactivateLinks(node.children);
-			continue;
-		}
-		if(node.children && node.children.length > 0) {
-			stack.push.apply(stack,node.children);
-		}
-	}
-}
+  function processWikiTree(tree, hasWikiLinkRule) {
+    var stack = [].concat(tree);
 
-// to extend MarkdownIt outside of this module, do:
-//
-// md = $tw.Wiki.parsers["text/markdown"].prototype.md;
-// md.use(plugin[, options]);
-MarkdownParser.prototype.md = createMarkdownEngine(markdownOpts,pluginOpts);
+    var mergeable = function (node) {
+      return (
+        node.type === 'element' &&
+        node.tag === 'p' &&
+        (!node.attributes || Object.keys(node.attributes).length === 0)
+      );
+    };
 
-function MarkdownParser(type,text,options) {
-	var env = {}
-	var md = this.md;
-	var mdTree = md.parse(text,env);
-	var textToParse = '<div class="markdown">\n' + md.renderer.render(mdTree,md.options,env) + '</div>';
+    while (stack.length) {
+      var node = stack.pop();
+      if (node.type === 'element' && node.tag === 'p') {
+        // reduce nested <p> nodes
+        while (
+          node.children &&
+          node.children.length === 1 &&
+          mergeable(node.children[0])
+        ) {
+          node.children = node.children[0].children;
+        }
+      } else if (hasWikiLinkRule && isCodified(node)) {
+        deactivateLinks(node.children);
+        continue;
+      }
+      if (node.children && node.children.length > 0) {
+        stack.push.apply(stack, node.children);
+      }
+    }
+  }
 
-	//console.log(JSON.stringify(mdTree,null,2));
-	//console.log("\n----------------\n" + textToParse);
+  // to extend MarkdownIt outside of this module, do:
+  //
+  // md = $tw.Wiki.parsers["text/markdown"].prototype.md;
+  // md.use(plugin[, options]);
+  MarkdownParser.prototype.md = createMarkdownEngine(markdownOpts, pluginOpts);
 
-	var wikiParser;
+  function MarkdownParser(type, text, options) {
+    var env = {};
+    var md = this.md;
+    var mdTree = md.parse(text, env);
+    var textToParse =
+      '<div class="markdown">\n' +
+      md.renderer.render(mdTree, md.options, env) +
+      '</div>';
 
-	var origParseStringLiteral = $tw.utils.parseStringLiteral;
-	$tw.utils.parseStringLiteral = parseStringLiteralExtended;
+    //console.log(JSON.stringify(mdTree,null,2));
+    //console.log("\n----------------\n" + textToParse);
 
-	try {
-		wikiParser = new $tw.Wiki.parsers["text/vnd.tiddlywiki"](null,textToParse,{
-			parseAsInline: true,
-			wiki: options.wiki,
-			rules: { pragma: {}, block: this.blockRuleClasses, inline: this.inlineRuleClasses }
-		});
-	}
-	catch (err) {
-		wikiParser = $tw.wiki.parseText("text/vnd.tiddlywiki",
-			"<strong>Error encountered while parsing the tiddler:</strong><p>" + err.message + "</p>",
-			{parseAsInline: false, wiki: options.wiki});
-	}
-	finally {
-		$tw.utils.parseStringLiteral = origParseStringLiteral;
-	}
-	if(wikiParser.tree.length > 0) {
-		var hasWikiLinkRule = false;
-		// see if wikilink rule has been invoked
-		$tw.utils.each(wikiParser.inlineRules,function(ruleInfo) {
-			if(ruleInfo.rule.name === "wikilink") {
-				hasWikiLinkRule = true;
-				return false;
-			}
-		});
-		processWikiTree(wikiParser.tree,hasWikiLinkRule);
-	}
-	this.tree = wikiParser.tree;
-	this.source = text;
-	this.type = type || "text/markdown";
-	this.wiki = options.wiki;
-}
+    var wikiParser;
 
-exports["text/markdown"] = MarkdownParser;
-exports["text/x-markdown"] = MarkdownParser;
+    var origParseStringLiteral = $tw.utils.parseStringLiteral;
+    $tw.utils.parseStringLiteral = parseStringLiteralExtended;
+
+    try {
+      wikiParser = new $tw.Wiki.parsers['text/vnd.tiddlywiki'](
+        null,
+        textToParse,
+        {
+          parseAsInline: true,
+          wiki: options.wiki,
+          rules: {
+            pragma: {},
+            block: this.blockRuleClasses,
+            inline: this.inlineRuleClasses,
+          },
+        },
+      );
+    } catch (err) {
+      wikiParser = $tw.wiki.parseText(
+        'text/vnd.tiddlywiki',
+        '<strong>Error encountered while parsing the tiddler:</strong><p>' +
+          err.message +
+          '</p>',
+        { parseAsInline: false, wiki: options.wiki },
+      );
+    } finally {
+      $tw.utils.parseStringLiteral = origParseStringLiteral;
+    }
+    if (wikiParser.tree.length > 0) {
+      var hasWikiLinkRule = false;
+      // see if wikilink rule has been invoked
+      $tw.utils.each(wikiParser.inlineRules, function (ruleInfo) {
+        if (ruleInfo.rule.name === 'wikilink') {
+          hasWikiLinkRule = true;
+          return false;
+        }
+      });
+      processWikiTree(wikiParser.tree, hasWikiLinkRule);
+    }
+    this.tree = wikiParser.tree;
+    this.source = text;
+    this.type = type || 'text/markdown';
+    this.wiki = options.wiki;
+  }
+
+  exports['text/markdown'] = MarkdownParser;
+  exports['text/x-markdown'] = MarkdownParser;
 })();
