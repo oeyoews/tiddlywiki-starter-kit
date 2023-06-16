204e6c7480a7e7464ec0f3eb7555e47d3739795c
oeyoews <jyao4783@gmail.com>
2023-06-16 11:31:51 +0800

    Automatic commit

--- plugins/oeyoews/neotw-unsplash/tiddlers/UnsplashAPI.js
+++ plugins/oeyoews/neotw-unsplash/tiddlers/UnsplashAPI.js
@@ -6,6 +6,8 @@ module-type: widget
 UnplashAPI widget
 
 \*/
+
+// TODO: manual
 (function () {
   /*jslint node: true, browser: true */
   /*global $tw: false */
@@ -19,32 +21,50 @@ UnplashAPI widget
     constructor(parseTreeNode, options) {
       super(parseTreeNode, options);
     }
-
     render(parent, nextSibling) {
       this.parentDomNode = parent;
       this.computeAttributes();
       this.execute();
 
-      const placeholder =
-        localStorage.getItem('unsplashApiKey') || 'Enter Unsplash API';
-      const inputNode = $tw.utils.domMaker('input', {
-        type: 'text',
-        attributes: {
-          placeholder,
-        },
-      });
-      inputNode.addEventListener('input', event => {
-        const unsplashAPI = event.target.value;
-        localStorage.setItem('unsplashApiKey', unsplashAPI);
-      });
+      // baKbPgwlhbBfF7mHcGf0DS0TmFzi8GknZ4hbUhuNkrA
+      const placeholder = localStorage.getItem('unsplashApiKey') || '';
 
-      const divNode = $tw.utils.domMaker('div', {
-        class: '',
-        attributes: {},
-        children: [inputNode],
+      Swal.fire({
+        title: 'Unsplash API',
+        input: 'text',
+        inputPlaceholder: placeholder,
+        inputValue: placeholder,
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        showLoaderOnConfirm: true,
+        preConfirm: input => {
+          if (input) {
+            localStorage.setItem('unsplashApiKey', input.trim());
+          }
+        },
+      }).then(result => {
+        if (result.isConfirmed) {
+          const inputNode = $tw.utils.domMaker('input', {
+            type: 'text',
+            attributes: {
+              value: result.value,
+              placeholder: result.value,
+            },
+          });
+          inputNode.addEventListener('input', event => {
+            const unsplashAPI = event.target.value;
+            localStorage.setItem('unsplashApiKey', unsplashAPI);
+          });
+
+          const divNode = $tw.utils.domMaker('div', {
+            class: '',
+            attributes: {},
+            children: [inputNode],
+          });
+          parent.insertBefore(divNode, nextSibling);
+          this.domNodes.push(divNode);
+        }
       });
-      parent.insertBefore(divNode, nextSibling);
-      this.domNodes.push(divNode);
     }
   }
 
