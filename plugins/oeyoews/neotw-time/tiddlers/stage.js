3f7a06183d05cc6546d83b604066bd3c34bb1c4f
oeyoews <jyao4783@gmail.com>
2023-06-16 22:27:03 +0800

    Automatic commit

---
+++ plugins/oeyoews/neotw-time/tiddlers/stage.js
@@ -0,0 +1,72 @@
+/*\
+title: $:/plugins/oeyoews/neotw-otime/stage.js
+type: application/javascript
+module-type: widget
+
+stage widget
+
+\*/
+(function () {
+  /*jslint node: true, browser: true */
+  /*global $tw: false */
+  'use strict';
+
+  if (!$tw.browser) return;
+
+  const Widget = require('$:/core/modules/widgets/widget.js').widget;
+  const { calculateTimeDiff } = require('./time');
+
+  class StageWidget extends Widget {
+    constructor(parseTreeNode, options) {
+      super(parseTreeNode, options);
+      this.interval = this.getAttribute('interval', 1000);
+    }
+
+    startTimer() {
+      // 使用箭头函数来绑定正确的 this 引用
+      this.timer = setInterval(() => {
+        // 更新 widget 的内容
+        this.update();
+      }, this.interval);
+    }
+
+    update() {
+      if (!this.domNodes || !this.domNodes[0]) {
+        clearInterval(this.timer);
+        return;
+      } else {
+        const timeStage = calculateTimeDiff('2022-02-02');
+        this.domNodes[0].textContent = timeStage;
+      }
+    }
+
+    render(parent, nextSibling) {
+      this.parentDomNode = parent;
+      this.computeAttributes();
+      this.execute();
+
+      const timeStage = calculateTimeDiff('2022-02-02');
+      const classNames = this.getAttribute('class', '').split(' ');
+
+      const buttonNode = $tw.utils.domMaker('span', {
+        text: timeStage,
+        attributes: {
+          title: timeStage,
+        },
+      });
+
+      classNames.forEach(className => {
+        if (className) {
+          buttonNode.classList.add(className);
+        }
+      });
+      parent.insertBefore(buttonNode, nextSibling);
+      this.domNodes.push(buttonNode);
+
+      this.update();
+      this.startTimer();
+    }
+  }
+
+  exports['ostage'] = StageWidget;
+})();
---
+++ plugins/oeyoews/neotw-time/tiddlers/time.js
@@ -0,0 +1,36 @@
+/*\
+title: $:/plugins/oeyoews/neotw-otime/time.js
+type: application/javascript
+module-type: library
+
+time.js
+
+\*/
+function calculateTimeDiff(pastDateString) {
+  const pastDate = new Date(pastDateString);
+  const currentDate = new Date();
+
+  // 验证过去日期是否有效
+  if (pastDate > currentDate) {
+    console.error('过去日期无效，不能超过当前时间！');
+    return;
+  }
+
+  const timeDiff = currentDate.getTime() - pastDate.getTime();
+
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+  // console.clear(); ???
+
+  const timeStage = `距离 ${pastDate.toDateString()} 已经过去了 ${days} 天 ${hours} 小时 ${minutes} 分钟 ${seconds} 秒。`;
+  return timeStage;
+}
+
+module.exports = {
+  calculateTimeDiff,
+};
