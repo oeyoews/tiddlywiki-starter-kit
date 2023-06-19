847448de7d63f084977e993cf402ca251ef837ff
oeyoews <jyao4783@gmail.com>
2023-06-19 23:28:57 +0800

    Automatic commit

--- plugins/oeyoews/neotw-hitokoto/tiddlers/core/widget.js
+++ plugins/oeyoews/neotw-hitokoto/tiddlers/core/widget.js
@@ -29,7 +29,7 @@ Hitokoto widget
       this.hitokotoNode = $tw.utils.domMaker('div', {
         text: 'Loading ...',
         class:
-          'text-transparent bg-clip-text bg-gradient-to-r from-teal-400 via-pink-500 cursor-pointer text-xs',
+          'text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-purple-400 cursor-pointer text-xs inline',
         attributes: {},
         children: [],
         eventListeners: [
@@ -65,7 +65,7 @@ Hitokoto widget
       };
     }
 
-    handlerClick = this.throttle(this.fetchHitokoto, 1000);
+    handlerClick = this.throttle(this.fetchHitokoto, 500);
   }
 
   exports.hitokoto = Hitokoto;
