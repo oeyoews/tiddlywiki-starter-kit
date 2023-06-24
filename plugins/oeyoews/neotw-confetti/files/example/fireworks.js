ce92a800ee42d4e0f63f5fd0759740eaf4cf167f
oeyoews <jyao4783@gmail.com>
2023-06-24 11:10:38 +0800

    Automatic commit

--- plugins/oeyoews/neotw-confetti/files/example/fireworks.js
+++ plugins/oeyoews/neotw-confetti/files/example/fireworks.js
@@ -1,3 +1,52 @@
+const prideCurrentTiddler = () => {
+  var myCanvas = document.createElement('canvas');
+  const neotw = document.getElementById('neotw');
+  neotw.appendChild(myCanvas);
+  var myConfetti = confetti.create(myCanvas);
+
+  var duration = 3 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = {
+    startVelocity: 25,
+    spread: 360,
+    ticks: 60,
+    scalar: 0.8,
+    // zIndex: -10000,
+    zIndex: 10000,
+    shapes: ['circle', 'circle', 'square'],
+    disableForReducedMotion: false,
+  };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  var interval = setInterval(function () {
+    var timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    var particleCount = 50 * (timeLeft / duration);
+    myConfetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      }),
+    );
+    myConfetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      }),
+    );
+  }, 250);
+  setTimeout(() => {
+    neotw.removeChild(myCanvas);
+  }, 2000);
+};
+
 const fireworks = () => {
   /* const isDev = $tw.wiki.getTiddlerText('$:/info/url/protocol');
 
@@ -204,6 +253,7 @@ const msg = () => {
 };
 
 const moduleExports = {
+  prideCurrentTiddler,
   fireworks,
   snow,
   pride,
