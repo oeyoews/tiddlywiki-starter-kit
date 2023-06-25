19cd7914679ccd9955ae72dbeba916384c79cd03
oeyoews <jyao4783@gmail.com>
2023-06-25 21:34:14 +0800

    feat: support all time

--- plugins/oeyoews/neotw-time/tiddlers/time.js
+++ plugins/oeyoews/neotw-time/tiddlers/time.js
@@ -6,17 +6,11 @@ module-type: library
 time.js
 
 \*/
-function calculateTimeDiff(pastDateString) {
-  const pastDate = new Date(pastDateString);
+function calculateTimeDiff(dateString) {
+  const targetDate = new Date(dateString);
   const currentDate = new Date();
 
-  // 验证过去日期是否有效
-  if (pastDate > currentDate) {
-    console.error('过去日期无效，不能超过当前时间！');
-    return;
-  }
-
-  const timeDiff = currentDate.getTime() - pastDate.getTime();
+  const timeDiff = Math.abs(targetDate.getTime() - currentDate.getTime());
 
   const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
@@ -25,14 +19,16 @@ function calculateTimeDiff(pastDateString) {
   const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
-  // console.clear(); ???
-
   const dayString = days > 0 ? `${days} 天 ` : '';
   const hourString = hours > 0 ? `${hours} 小时 ` : '';
   const minuteString = minutes > 0 ? `${minutes} 分钟 ` : '';
   const secondString = seconds > 0 ? `${seconds} 秒` : '';
 
-  const timeStage = `距离 ${pastDate.toLocaleDateString()} 已经过去了 ${dayString}${hourString}${minuteString}${secondString}。`;
+  const timeStage =
+    targetDate.getTime() > currentDate.getTime()
+      ? `距离 ${targetDate.toLocaleDateString()} 还有 ${dayString}${hourString}${minuteString}${secondString}。`
+      : `距离 ${targetDate.toLocaleDateString()} 已经过去了 ${dayString}${hourString}${minuteString}${secondString}。`;
+
   return timeStage;
 }
 
