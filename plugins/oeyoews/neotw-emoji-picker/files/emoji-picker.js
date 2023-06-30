0e0f3a5bfa68e28649de0c92765c00cf792868c3
oeyoews <jyao4783@gmail.com>
2023-06-30 11:03:17 +0800

    feat: add neotw-emoji-picker-zh-CN

--- plugins/oeyoews/neotw-emoji-picker/files/emoji-picker.js
+++ plugins/oeyoews/neotw-emoji-picker/files/emoji-picker.js
@@ -21,7 +21,11 @@ function emojiComponent() {
   };
   input.className =
     'px-4 py-2 bg-white border border-gray-300 rounded shadow-sm focus:outline-none focus:border-indigo-300 w-4/5';
-  input.placeholder = '🍉 Search emoji ...';
+
+  const placeholder = $tw.wiki.getTiddlerText(
+    '$:/language/emoji-picker/placeholder',
+  );
+  input.placeholder = placeholder;
 
   // Create button element
   var button = document.createElement('button');
