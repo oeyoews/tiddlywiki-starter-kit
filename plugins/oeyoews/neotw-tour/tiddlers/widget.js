f4f1b01c2cf3100ca1cb488db02e883dd7f4363e
oeyoews <jyao4783@gmail.com>
2023-07-20 22:58:52 +0800

    feat: support json file

--- plugins/oeyoews/neotw-tour/tiddlers/widget.js
+++ plugins/oeyoews/neotw-tour/tiddlers/widget.js
@@ -6,6 +6,7 @@ module-type: widget
 neotw-tour widget
 
 \*/
+// TODO postitions not work
 (function () {
   /*jslint node: true, browser: true */
   /*global $tw: false */
@@ -13,9 +14,10 @@ neotw-tour widget
 
   const Widget = require('$:/core/modules/widgets/widget.js').widget;
 
-  class DivWidget extends Widget {
+  class TourWidget extends Widget {
     constructor(parseTreeNode, options) {
       super(parseTreeNode, options);
+      this.steps = [];
     }
 
     render(parent, nextSibling) {
@@ -26,6 +28,10 @@ neotw-tour widget
       this.execute();
 
       const classNames = this.getAttribute('class', '').split(' ');
+      const targetTiddler =
+        this.getAttribute('tiddler') ||
+        '$:/plugins/oeyoews/neotw-tour/demo.json';
+      this.steps = $tw.wiki.getTiddlerData(targetTiddler);
 
       const buttonNode = $tw.utils.domMaker('button', {
         text: 'start',
@@ -52,49 +58,35 @@ neotw-tour widget
     handlerClick() {
       // 高亮元素
       const driverObj = driver({
+        // option config TODO
         showProgress: true,
         animate: true,
         showButtons: ['next', 'previous', 'close'],
-        nextBtnText: '➡️',
-        prevBtnText: '⬅️',
-        doneBtnText: '❌',
         allowClose: true,
+        /* nextBtnText: '➡️',
+        prevBtnText: '⬅️',
+        doneBtnText: '❌', */
         // overlayColor: 'red',
-        steps: [
-          {
-            element: '#h1',
-            popover: {
-              title: 'Page Heading 1',
-              description: 'This is the main heading of the page.',
-              position: 'left',
-            },
-          },
-          {
-            element: '#h2',
-            popover: {
-              title: 'Page Heading 2',
-              description: 'This is the main heading of the page.',
-              position: 'left',
-            },
-          },
-        ],
+        steps: this.steps,
       });
       driverObj.drive();
 
-      function highlight() {
+      function highlightWarning() {
         driverObj.highlight({
           // element: document.querySelector('h1'),
           popover: {
             description:
-              "<img src='https://i.imgur.com/EAQhHu5.gif' style='height: 202.5px; width: 270px;' /><span style='font-size: 15px; display: block; margin-top: 10px; text-align: center;'>Yet another highlight example.</span>",
+              "<img src='https://cdn-icons-png.flaticon.com/128/6598/6598519.png' style='height: 202.5px; width: 270px;' />",
             // title: 'Page Heading',
             // description: 'This is the main heading of the page.',
             // position: 'left',
           },
         });
       }
+
+      !this.steps && highlightWarning();
     }
   }
 
-  exports['tour'] = DivWidget;
+  exports['tour'] = TourWidget;
 })();
