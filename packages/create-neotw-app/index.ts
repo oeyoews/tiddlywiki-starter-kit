e7052f8c592c6e8b610b09679a787525dc4b43f4
oeyoews <jyao4783@gmail.com>
2023-08-30 22:56:19 +0800

    Automatic commit

--- packages/create-neotw-app/index.ts
+++ packages/create-neotw-app/index.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk';
 import createApp from './create-app';
 
-createApp().catch(e => {});
+createApp().catch(e => {
+  console.error(chalk.red.bold('程序错误'));
+});
