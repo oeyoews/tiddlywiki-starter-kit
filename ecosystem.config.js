202f4fc37aaed8f67da8591879a6e527fc326a80
oeyoews <jyao4783@gmail.com>
2023-08-30 18:30:18 +0800

    feat: add degit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,9 +16,9 @@ module.exports = {
       ignore_watch: ['./tiddlers'],
       cwd: './',
       watch_delay: 100,
-      log_file: './logs/combined.outerr.log',
-      out_file: './logs/out.log',
-      err_file: './logs/err.log',
+      log_file: './.logs/combined.outerr.log',
+      out_file: './.logs/out.log',
+      err_file: './.logs/err.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       // env: {
       //   TIDDLYWIKI_PLUGIN_PATH: './plugins',
