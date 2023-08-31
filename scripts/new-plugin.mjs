f0a1b8692caa3b13e7d83e873188bedb40d7daf2
oeyoews <jyao4783@gmail.com>
2023-08-31 16:39:19 +0800

    perf: use onstate for new-plugin

--- scripts/new-plugin.mjs
+++ scripts/new-plugin.mjs
@@ -1,34 +1,43 @@
 $.verbose = false;
 
-import prompts from 'prompts';
-import replace from 'replace';
-import titleCase from './base.mjs';
+import prompts from "prompts";
+import replace from "replace";
+
+const onPromptState = (state) => {
+  if (state.aborted) {
+    process.stdout.write("\x1B[?25h");
+    process.stdout.write("\n");
+    process.exit(0);
+  }
+};
 
 function titleCase(str) {
-  const splitStr = str.toLowerCase().split(' ');
+  const splitStr = str.toLowerCase().split(" ");
   for (let i = 0; i < splitStr.length; i++) {
     splitStr[i] =
       splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
   }
-  return splitStr.join(' ');
+  return splitStr.join(" ");
 }
 
 async function newPlugin() {
-  const template = 'templates/new-plugin';
+  const template = "templates/new-plugin";
   const questions = [
     {
-      type: 'text',
-      name: 'pluginname', // variable
-      message: 'create plugin',
-      initial: 'pluginname',
+      onState: onPromptState,
+      type: "text",
+      name: "pluginname", // variable
+      message: "create plugin",
+      initial: "pluginname",
     },
     {
-      type: 'toggle',
-      name: 'newPluginStatus',
-      message: 'Are you sure to Create this new plugin',
+      onState: onPromptState,
+      type: "toggle",
+      name: "newPluginStatus",
+      message: "Are you sure to Create this new plugin",
       initial: true,
-      inactive: 'no',
-      active: 'yes',
+      inactive: "no",
+      active: "yes",
     },
   ];
 
@@ -38,15 +47,13 @@ async function newPlugin() {
   if (newPluginStatus) {
     await $`rm -rf plugins/pluginname*`;
     // ???
-    pluginname = pluginname.trim().replace(/\s+/g, '-');
-    const upperpluginname = titleCase(pluginname.trim().replace(/-/g, ' '));
-    const target = 'plugins/oeyoews/' + pluginname;
+    pluginname = pluginname.trim().replace(/\s+/g, "-");
+    const upperpluginname = titleCase(pluginname.trim().replace(/-/g, " "));
+    const target = "plugins/oeyoews/" + pluginname;
 
     if (fs.existsSync(target)) {
       console.log(
-        chalk.red.bold(
-          `Plugin directory '${target}' already exists. Aborting.`,
-        ),
+        chalk.red.bold(`Plugin directory '${target}' already exists. Aborting.`)
       );
       return;
     }
@@ -62,7 +69,7 @@ async function newPlugin() {
 
     for (let i in regexPlace) {
       replace({
-        regex: new RegExp('\\$\\{' + i + '\\}', 'g'),
+        regex: new RegExp("\\$\\{" + i + "\\}", "g"),
         replacement: regexPlace[i],
         paths: [target],
         recursive: true,
@@ -70,7 +77,7 @@ async function newPlugin() {
       });
     }
   } else {
-    console.log(chalk.yellow('🍃 I can see the first leaf falling.'));
+    console.log(chalk.yellow("🍃 I can see the first leaf falling."));
   }
 }
 
