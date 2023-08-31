443d2130fec4344ec23bd4ddce282224d1293f05
oeyoews <jyao4783@gmail.com>
2023-08-31 22:15:33 +0800

    Automatic commit

---
+++ packages/create-tw-apps/README.md
@@ -0,0 +1,5 @@
+## README
+
+![snapshot](https://cdn.jsdelivr.net/gh/oeyoews/neotw@main/img/create-tw-app.png)
+
+* 快速创建一个 tiddlywiki-application(nodejs)
---
+++ packages/create-tw-apps/lib/vaildate-pkg.ts
@@ -0,0 +1,20 @@
+// @ts-ignore
+import validate from "validate-npm-package-name";
+
+export function validateNpmName(name: string): {
+  valid: boolean;
+  problems?: string[];
+} {
+  const nameValidation = validate(name);
+  if (nameValidation.validForNewPackages) {
+    return { valid: true };
+  }
+
+  return {
+    valid: false,
+    problems: [
+      ...(nameValidation.errors || []),
+      ...(nameValidation.warnings || []),
+    ],
+  };
+}
