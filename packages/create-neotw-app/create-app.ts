d56de8761a4c61113cf37b3ea7bde5cc2659058a
oeyoews <jyao4783@gmail.com>
2023-08-31 08:32:21 +0800

    feat: support update-check

--- packages/create-neotw-app/create-app.ts
+++ packages/create-neotw-app/create-app.ts
@@ -4,13 +4,42 @@ import ora from "ora";
 import chalk from "chalk";
 import prompts from "prompts";
 import { validateNpmName } from "./vaildate-pkg";
+import checkForUpdate from "update-check";
+import packageJson from "./package.json";
 
 // @ts-ignore
 import tiged from "tiged";
 
 const spinner = ora("Loading ...");
 
+const update = checkForUpdate(packageJson).catch(() => null);
+
+async function notifyUpdate(): Promise<void> {
+  try {
+    const res = await update;
+    if (res?.latest) {
+      console.log(
+        chalk.yellow(
+          chalk.bold("🚀 A new version of `create-next-app` is available!")
+        ) +
+          "\n" +
+          "You can update by running: " +
+          chalk.cyan("pnpm dlx create-next-app@latest") +
+          "\n"
+      );
+    } else {
+      console.log(
+        chalk.green(
+          `🎉 ${packageJson.name} is up to date ${packageJson.version}\n`
+        )
+      );
+    }
+    // process.exit(0);
+  } catch {}
+}
+
 export default async function createApp() {
+  await notifyUpdate();
   const user = "oeyoews";
   const repo = "tiddlywiki-starter-kit";
   const initial = `${user}/${repo}`;
