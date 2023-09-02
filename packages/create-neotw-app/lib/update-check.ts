ebedaa5268ac95b85bd1cfc12b5420fe6ea4f071
oeyoews <jyao4783@gmail.com>
2023-09-02 18:04:31 +0800

    Automatic commit

--- packages/create-neotw-app/lib/update-check.ts
+++ packages/create-neotw-app/lib/update-check.ts
@@ -6,23 +6,13 @@ const update = checkForUpdate(packageJson).catch(() => null);
 export async function notifyUpdate(): Promise<void> {
   try {
     const res = await update;
-    if (res?.latest) {
-      console.log(
-        chalk.yellow(
-          chalk.bold(`🚀 A new version of ${packageJson.name}  is available!`)
-        ) +
-          "\n" +
-          "You can update by running: " +
-          chalk.cyan(`pnpm dlx ${packageJson.name}@latest`) +
-          "\n"
-      );
-    } else {
-      console.log(
-        chalk.green(
-          `🎉 ${packageJson.name} is up to date ${packageJson.version}\n`
-        )
-      );
-    }
+    console.log(
+      chalk.green(
+        res?.latest
+          ? `📦 A new version of ${packageJson.name}  is available! ${packageJson.version} → ${res?.latest}`
+          : `🎉 ${packageJson.name} is up to date ${packageJson.version}\n`
+      )
+    );
     // process.exit(0);
   } catch {}
 }
