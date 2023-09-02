64802e1f8c64f5cf7d4e436c513451d84fa58539
oeyoews <jyao4783@gmail.com>
2023-09-02 15:47:22 +0800

    feat: remove json5 and split config folder

--- tiddlywiki.config.js
+++ tiddlywiki.config.js
@@ -1,13 +1,27 @@
 const fs = require("fs");
 const path = require("path");
-const JSON5 = require("json5");
 
-const configFilePath = path.join(__dirname, "tiddlywiki.json5");
+const plugins = require("./config/plugins");
+const build = require("./config/build");
 const tiddlywikiInfoPath = path.join(__dirname, "tiddlywiki.info");
+const configData = {
+  description:
+    "A modern style and elegant notebook built with Tiddlywiki5 and Tailwindcss",
+  themes: [
+    // vanilla 主题, 必选
+    "tiddlywiki/vanilla",
+    "nico/notebook",
+  ],
+  plugins,
+  build,
+  // 命令
+  config: {
+    "retain-original-tiddler-path": false,
+    // 不建议修改, 如果修改了filesystem
+    "default-tiddler-location": "tiddlers",
+  },
+};
 
-const cofnigFile = fs.readFileSync(configFilePath, "utf-8");
-const configData = JSON5.parse(cofnigFile);
-
-fs.writeFileSync(tiddlywikiInfoPath, JSON.stringify(configData, null, 2));
+fs.writeFileSync(tiddlywikiInfoPath, JSON.stringify(configData));
 
 console.log("新的tiddlywiki.info配置文件已生成！");
