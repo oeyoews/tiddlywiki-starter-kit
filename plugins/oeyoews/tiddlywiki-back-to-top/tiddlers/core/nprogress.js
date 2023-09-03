9ecba41498467ecd0a23446b5b643e050dc0b94b
oeyoews <jyao4783@gmail.com>
2023-09-03 22:58:12 +0800

    Automatic commit

--- plugins/oeyoews/tiddlywiki-back-to-top/tiddlers/core/nprogress.js
+++ plugins/oeyoews/tiddlywiki-back-to-top/tiddlers/core/nprogress.js
@@ -9,21 +9,23 @@ nprogress module
 (function () {
   /*jslint node: true, browser: true */
   /*global $tw: false */
-  'use strict';
+  "use strict";
 
-  exports.name = 'nprogress-startup-hook';
-  exports.platforms = ['browser'];
-  exports.after = ['load-modules'];
+  exports.name = "nprogress-startup-hook";
+  exports.platforms = ["browser"];
+  exports.after = ["load-modules"];
   exports.synchronous = true;
   exports.startup = () => {
-    window.NProgress = require('nprogress.min.js');
-    NProgress.start();
-    const startTime = performance.now();
-    window.onload = function () {
-      NProgress.done();
-      const endTime = performance.now();
-      const loadTime = Math.floor(endTime - startTime);
-      console.log(`🎉 Page loaded in ${loadTime}ms`);
-    };
+    try {
+      window.NProgress = require("nprogress.min.js");
+      NProgress.start();
+      const startTime = performance.now();
+      window.onload = function () {
+        NProgress.done();
+        const endTime = performance.now();
+        const loadTime = Math.floor(endTime - startTime);
+        console.log(`🎉 Page loaded in ${loadTime}ms`);
+      };
+    } catch (e) {}
   };
 })();
