c882e1ffd9d328d4c98ff8cabed8aa52e251a7f3
oeyoews <jyao4783@gmail.com>
2023-09-07 16:51:39 +0800

    Automatic commit

--- plugins/oeyoews/neotw-pwa/tiddlers/get-manifest.js
+++ plugins/oeyoews/neotw-pwa/tiddlers/get-manifest.js
@@ -5,16 +5,6 @@ module-type: route
 
 GET /manifest.json
 
-// 也许我们可以尝试模仿favicon.ico, 支持manifest, 构建的时候也可以自动生成在根目录下, 唯一的区别就是manifest.json只在启动的时候寻找, 之后不会自动刷新, 暂时放在file文件夹内, server和online共用一个文件(需要修改link /manifest.json -> /files/manifest.json)
-
-method 01: 直接链接 files/manifest.json, 不需要写任何代码; manifest.json 没有放在根目录不太规范
-
-method 02: route 代码 让/manifest.json, 可访问, 从而link /manifest.json, 构建可以共享$:/manifest.json, 模仿favicon.ico
-          但是, 只能是成为shadow才能起作用, 只在启动的时候寻找
-
-NOTE: 不能提前访问xxx.js文件从buffer
-NOTE: 无论是从files 文件夹, 还是tw里面读取文件, js文件都必须是shadow(favicon.ico为什么可以)
-
 \*/
 (function () {
   /*jslint node: true, browser: true */
