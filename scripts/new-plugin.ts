b3edbb2c5422fdfc75c59be47ec84c2687584cc4
oeyoews <jyao4783@gmail.com>
2023-09-09 17:29:32 +0800

    feat: update new-plugin

--- scripts/new-plugin.ts
+++ scripts/new-plugin.ts
@@ -1,10 +1,9 @@
 import prompts from "prompts";
-import replace from "replace";
 import fs from "fs";
-import os from "os";
-import { exec } from "child_process"; // 导入 child_process 模块的 exec 函数
+import { exec } from "child_process";
+import path from "path";
 
-const onPromptState = (state) => {
+const onPromptState = (state: { aborted: any }) => {
   if (state.aborted) {
     process.stdout.write("\x1B[?25h");
     process.stdout.write("\n");
@@ -12,85 +11,53 @@ const onPromptState = (state) => {
   }
 };
 
-function titleCase(str) {
-  const splitStr = str.toLowerCase().split(" ");
-  for (let i = 0; i < splitStr.length; i++) {
-    splitStr[i] =
-      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+async function processFilesRecursively(directory: string, pluginname: string) {
+  const files = fs.readdirSync(directory);
+
+  for (const file of files) {
+    const filePath = path.join(directory, file);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isDirectory()) {
+      await processFilesRecursively(filePath, pluginname);
+    } else {
+      let content = fs.readFileSync(filePath, "utf8");
+      content = content.replace(/\$\{pluginname\}/g, pluginname);
+      fs.writeFileSync(filePath, content);
+    }
   }
-  return splitStr.join(" ");
 }
 
 async function newPlugin() {
   const template = "templates/new-plugin";
-  const questions = [
-    {
-      onState: onPromptState,
-      type: "text",
-      name: "pluginname", // 变量
-      message: "create plugin",
-      initial: "pluginname",
-    },
-    {
-      onState: onPromptState,
-      type: "toggle",
-      name: "newPluginStatus",
-      message: "Are you sure to Create this new plugin",
-      initial: true,
-      inactive: "no",
-      active: "yes",
-    },
-  ];
 
-  // 获取回答
-  let { newPluginStatus, pluginname } = await prompts(questions);
-
-  if (newPluginStatus) {
-    exec(`rm -rf plugins/${pluginname}*`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error removing existing plugins: ${error.message}`);
-        return;
+  const { pluginname } = await prompts({
+    onState: onPromptState,
+    type: "text",
+    name: "pluginname",
+    message: "create plugin",
+    initial: "pluginname",
+    validate: (value) => {
+      if (fs.existsSync(`plugins/oeyoews/${value}`)) {
+        return `${value} folder already exists`;
       }
-      // 继续执行您的代码
-      pluginname = pluginname.trim().replace(/\s+/g, "-");
-      const upperpluginname = titleCase(pluginname.trim().replace(/-/g, " "));
-      const target = `plugins/oeyoews/${pluginname}`;
+      return true;
+    },
+  });
 
-      if (fs.existsSync(target)) {
-        console.log(`Plugin directory '${target}' already exists. Aborting.`);
-        return;
-      }
+  const target = path.join("plugins/oeyoews", pluginname);
 
-      exec(
-        `mkdir ${target} && cp -r ${template}/* ${target}`,
-        (error, stdout, stderr) => {
-          if (error) {
-            console.error(`Error creating plugin directory: ${error.message}`);
-            return;
-          }
-          // 继续执行您的代码
-          const { username } = os.userInfo();
-          const regexPlace = {
-            upperpluginname,
-            pluginname,
-            username,
-          };
+  fs.mkdirSync(target, { recursive: true });
+  exec(`cp -r ${template}/* ${target}`, () => {
+    const targetFiles = fs.readdirSync(target);
 
-          for (let i in regexPlace) {
-            replace({
-              regex: new RegExp("\\$\\{" + i + "\\}", "g"),
-              replacement: regexPlace[i],
-              paths: [target],
-              recursive: true,
-              silent: true,
-            });
-          }
-        }
-      );
-    });
-  } else {
-    console.log("🍃 I can see the first leaf falling.");
-  }
+    try {
+      processFilesRecursively(target, pluginname);
+    } catch (error) {
+      fs.rmSync(target, { recursive: true });
+      console.log(error);
+    };
+  })
 }
 
 newPlugin();
