3de77f96e7077e5a79ccaac19d038d55798695fa
oeyoews <jyao4783@gmail.com>
2023-09-11 09:24:54 +0800

    feat: use ci-info

--- lib/build.ts
+++ lib/build.ts
@@ -1,13 +1,26 @@
 import ora from 'ora';
 import generateTiddlyWikiInfo from '../tiddlywiki.config.mjs';
+import ci from 'ci-info';
+// @ts-ignore
+import tiged from 'tiged';
+
+const emitter = tiged('oeyoews/tiddlywiki-starter-kit', {
+  disableCache: true,
+  force: true,
+  verbose: false,
+});
+
+if (ci.name === 'VERCEL' || ci.name === 'GITHUB') {
+  emitter.clone('tiddlers').then(() => {
+    log.succeed('tiddlers 文件夹复制完成');
+  });
+}
 
 const buildDir = process.env.OUTPURDIR || '.tiddlywiki';
 const log = ora('Building ...');
 
 function copyFiles() {
-  // @ts-ignore
   Bun.spawn(['cp', '-r', 'files', 'vercel.json', buildDir], {
-    // @ts-ignore
     onExit: (proc, exitCode, signalCode, error) => {
       if (exitCode === 0) {
         log.succeed('复制文件完成');
