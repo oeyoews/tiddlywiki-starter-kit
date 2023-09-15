d4a1711d2ef7c0b99bb8ac28157db28b1c1f2cf8
oeyoews <jyao4783@gmail.com>
2023-09-15 09:07:25 +0800

    fix: copy files dir to support sounds

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ set -e
 # /app/wiki -> ${PWD}/wiki
 if [ ! -f ./wiki/tiddlywiki.info ]; then
     cp ./tiddlywiki.info ./wiki/tiddlywiki.info
-    cp ./files ./wiki/files
+    cp -r ./files ./wiki/files
     echo '更新 tiddlywiki.info 文件成功'
 fi
 
