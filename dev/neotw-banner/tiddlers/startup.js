05629195836009b946b19c07766031979210509a
oeyoews <jyao4783@gmail.com>
2023-09-30 16:50:29 +0800

    Automatic commit

---
+++ dev/neotw-banner/tiddlers/startup.js
@@ -0,0 +1,25 @@
+/*\
+title: test/startup.js
+type: application/javascript
+module-type: startup
+
+test module
+
+\*/
+(function () {
+  /*jslint node: true, browser: true */
+  /*global $tw: false */
+  'use strict';
+
+  exports.name = 'test-startup';
+  exports.platforms = ['browser'];
+  exports.after = ['story'];
+  exports.synchronous = true;
+  exports.startup = () => {
+    const Node = document.createElement('div');
+    Node.className =
+      'h-screen bg-white flex justify-center items-center text-lg font-bld';
+    Node.textContent = '待完工...';
+    document.body.insertBefore(Node, document.body.firstChild);
+  };
+})();
