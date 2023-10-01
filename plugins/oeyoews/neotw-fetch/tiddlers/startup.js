568fc45301baf00bc4fd84f2d85beef86b467d1e
oeyoews <jyao4783@gmail.com>
2023-10-01 19:56:20 +0800

    Automatic commit

--- plugins/oeyoews/neotw-fetch/tiddlers/startup.js
+++ plugins/oeyoews/neotw-fetch/tiddlers/startup.js
@@ -15,7 +15,7 @@ fetch-readme module
   exports.name = 'fetch-readme-startup-hook';
   exports.platforms = ['browser'];
   exports.after = ['startup'];
-  exports.synchronous = true;
+  exports.synchronous = false;
   exports.startup = async () => {
     $tw.rootWidget.addEventListener('om-fetch-mdfile', async (event) => {
       const paramObject = event.paramObject || {};
