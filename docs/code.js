45bcfbdd14b036c24f6878c77138a843960e6a0f
oeyoews <jyao4783@gmail.com>
2023-10-08 11:33:02 +0800

    更新 code.js

--- docs/code.js
+++ docs/code.js
@@ -32,3 +32,42 @@ $tw.modal.display(title); // show a dialog
 
 // widget
 this.getVariable('currentTiddler'); 获取当前条目名称
+
+/** class 
+  * 
+  */
+
+// 如果目标元素没有class可以使用，仅支持单个class
+// $tw.utils.addClass(commode, 'font-bold')
+// tiddler的field的class支持设置样式就是来源于此，除了这一点，在写第三方插件的过程中感觉这种这种写法并没有比className有什么优势， 更推荐使用classList方法
+exports.addClass = function(el,className) {
+	var c = (el.getAttribute("class") || "").split(" ");
+	if(c.indexOf(className) === -1) {
+		c.push(className);
+		el.setAttribute("class",c.join(" "));
+	}
+};
+
+// 移除class
+exports.removeClass = function(el,className) {
+	var c = (el.getAttribute("class") || "").split(" "),
+		p = c.indexOf(className);
+	if(p !== -1) {
+		c.splice(p,1);
+		el.setAttribute("class",c.join(" "));
+	}
+};
+
+// 切换class
+exports.toggleClass = function(el,className,status) {
+	if(status === undefined) {
+		status = !exports.hasClass(el,className);
+	}
+	if(status) {
+		exports.addClass(el,className);
+	} else {
+		exports.removeClass(el,className);
+	}
+};
+
+
