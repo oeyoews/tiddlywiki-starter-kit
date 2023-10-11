2affaa7b563d6096703f0e5eece0855218cc8a90
oeyoews <jyao4783@gmail.com>
2023-10-11 20:48:33 +0800

    Update startup.js

--- plugins/oeyoews/neotw-fetch/tiddlers/startup.js
+++ plugins/oeyoews/neotw-fetch/tiddlers/startup.js
@@ -16,14 +16,17 @@ fetch-readme module
   exports.platforms = ['browser'];
   exports.after = ['startup'];
   exports.synchronous = true;
+
+  // jsdelivr.com 有缓存, 所以有时候readme 不是最新的, 使用@latest也会限制, 如果直接使用 raw GitHub readme, 会有请求速率限制
   exports.startup = async () => {
+    const readmeURL = 'https://raw.githubusercontent.com/oeyoews/neotw/main/README.md'
     $tw.rootWidget.addEventListener('om-fetch-mdfile', async (event) => {
       const paramObject = event.paramObject || {};
       const time = new Date().getTime();
       const fileName = paramObject.fileName || `fetch-${time}`;
       const url =
         paramObject.url ||
-        'https://raw.githubusercontent.com/oeyoews/neotw/main/README.md';
+        readmeURL;
       const response = await fetch(url);
       const text = await response.text();
       $tw.wiki.setText(fileName, 'text', null, text);
@@ -31,9 +34,7 @@ fetch-readme module
     });
 
     // if (window.location.protocol === 'http:') return;
-    const url =
-      'https://cdn.jsdelivr.net/gh/oeyoews/tiddlywiki-starter-kit@main/README.md';
-    const response = await fetch(url);
+    const response = await fetch(readmeURL);
     const text = await response.text();
     const fileName = '$:/plugins/oeyoews/neotw/README';
     $tw.wiki.setText(fileName, 'text', null, text);
