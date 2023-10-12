94c3f424eb02573362b4033b74b6754251b463a1
oeyoews <jyao4783@gmail.com>
2023-10-12 21:34:54 +0800

    feat: add two echarts addons

---
+++ plugins/oeyoews/neotw/tiddlers/modules/echarts/addons/sevenday.js
@@ -0,0 +1,207 @@
+/*\
+title: addon/sevenday.js
+module-type: echarts-component
+type: application/javascript
+description: seven
+\*/
+
+// TODO: 简化时间的处理
+// TODO: 指定线的颜色, 区域的颜色
+// @description: echarts 几乎支持每一处的样式设置, 这里仅根据需要设置必要的样式
+const getData = (date, type = 'created') =>
+  $tw.wiki.filterTiddlers(`[sameday:${type}[${date}]!is[system]!has[draft.of]]`)
+    .length;
+
+function parsesixDate(dateString) {
+  const year = parseInt(dateString.substr(0, 4));
+  const month = parseInt(dateString.substr(4, 2)) - 1; // 月份从0开始，需要减1
+  const day = parseInt(dateString.substr(6, 2));
+  const realDate = new Date(year, month, day);
+  //.toLocaleDateString();
+  return realDate;
+}
+
+function getSevenDaysBefore(dateString, daysLength = 7) {
+  const currentDate = dateString ? parsesixDate(dateString) : new Date();
+
+  const sevenDays = [];
+
+  for (let i = 0; i < daysLength; i++) {
+    // 获取当前日期的年、月、日
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1; // 月份从0开始，需要加1
+    const day = currentDate.getDate();
+
+    // 将年、月、日格式化成字符串，并添加到数组
+    const dateString = `${year}${month < 10 ? '0' : ''}${month}${
+      day < 10 ? '0' : ''
+    }${day}`;
+    sevenDays.unshift(dateString); // 使用unshift方法将日期添加到数组头部
+
+    // 将当前日期减一天，以便生成前一天的日期
+    currentDate.setDate(currentDate.getDate() - 1);
+  }
+
+  return sevenDays;
+}
+
+function onUpdate(myChart, _state, addonAttributes) {
+  const {
+    days,
+    date,
+    title: text = '最近文章动态',
+    subtitle: subtext = '',
+    disableClick = 'no',
+    smooth = 'true',
+  } = addonAttributes;
+
+  const sevendays = getSevenDaysBefore(date, days);
+
+  // TODO: 封装成函数 https://echarts.apache.org/examples/en/editor.html?c=bump-chart
+  const createdData = [];
+  const modifiedData = [];
+
+  sevendays.forEach((date) => createdData.push(getData(date)));
+  sevendays.forEach((date) => modifiedData.push(getData(date, 'modified')));
+
+  const option = {
+    title: {
+      text,
+      subtext,
+      left: 'center',
+      top: 'bottom',
+    },
+    legend: {
+      data: ['created', 'modified'],
+    },
+    toolbox: {
+      feature: {
+        restore: {},
+      },
+    },
+    tooltip: {
+      trigger: 'item', // item
+      formatter: function (params) {
+        const { name: date, value: count, seriesName } = params;
+        const realDate = parsesixDate(date).toLocaleDateString();
+        if (seriesName === 'created') {
+          return count
+            ? `${realDate} 写了 ${count} 篇文章`
+            : `${realDate} 没有写新的文章`;
+        } else {
+          return count
+            ? `${realDate} 更新了 ${count} 篇文章`
+            : `${realDate} 没有文章更新`;
+        }
+      },
+    },
+    // color: [''],
+    xAxis: {
+      boundaryGap: false,
+      type: 'category',
+      data: sevendays,
+      name: '日期',
+    },
+    yAxis: {
+      type: 'value',
+      name: '文章数量',
+    },
+    animationDuration: 2000,
+    series: [
+      {
+        name: 'created',
+        data: createdData,
+        showSymbol: true,
+        type: 'line',
+        symbolSize: 10,
+        endLabel: {
+          show: true,
+          formatter: '{a}',
+          distance: 20,
+        },
+        // areaStyle: {
+        // normal: {
+        //   color: "green", //改变区域颜色
+        //   lineStyle: {
+        //     color: "green", //改变折线颜色
+        //   },
+        // },
+        // },
+        lineStyle: {
+          width: 4,
+          // color: 'purple'
+        },
+        emphasis: {
+          focus: 'series',
+          itemStyle: {
+            scale: 1.5,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+          },
+        },
+        smooth: true,
+      },
+      {
+        name: 'modified',
+        data: modifiedData,
+        lineStyle: {
+          width: 4,
+        },
+        symbolSize: 10,
+        type: 'line',
+        showSymbol: true,
+        endLabel: {
+          show: true,
+          formatter: '{a}',
+          distance: 20,
+        },
+        // areaStyle: {},
+        emphasis: {
+          focus: 'series',
+          itemStyle: {
+            // color: '',
+            scale: 1.25,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)',
+          },
+        },
+        smooth: true,
+      },
+    ],
+  };
+
+  myChart.setOption(option);
+  myChart.on('click', 'series', function (params) {
+    const { name: date, value: count, seriesName } = params;
+    const goto = new $tw.Story();
+    const filter = `[sameday:${seriesName}[${date}]!is[system]!has[draft.of]]`;
+
+    if (!count) return;
+    $tw.rootWidget.invokeActionString(
+      '<$action-setfield $tiddler="$:/temp/advancedsearch" text="""' +
+        filter +
+        '"""/><$action-setfield $tiddler="$:/temp/advancedsearch/input" text="""' +
+        filter +
+        '"""/><$action-setfield $tiddler="$:/temp/advancedsearch/refresh" text="yes"/><$action-setfield $tiddler="$:/state/tab--1498284803" text="$:/core/ui/AdvancedSearch/Filter"/>',
+    );
+    goto.navigateTiddler('$:/AdvancedSearch');
+  });
+}
+
+function shouldUpdate(_state, changedTiddlers, _changedAttributes) {
+  return false;
+}
+
+// function onMount() {}
+// function onUnmount(state) { }
+
+/**
+ *	@param: days 指定天数
+ *  date: 指定日期
+ *  title: 标题
+ *  subtitle: 副标题
+ */
+module.exports = {
+  onUpdate,
+  shouldUpdate,
+};
---
+++ plugins/oeyoews/neotw/tiddlers/modules/echarts/tagpie.js
@@ -0,0 +1,148 @@
+/*\
+title: addon/tagpie.js
+module-type: echarts-component
+type: application/javascript
+description: tag pie on tiddlywiki
+
+\*/
+
+function getData(tag) {
+  const count = $tw.wiki.filterTiddlers(`[tag[${tag}]!has[draft.of]]`).length;
+  return {
+    name: tag,
+    value: count,
+  };
+}
+
+const goto = new $tw.Story();
+
+const gotoTagTiddler = (params) => {
+  const title = params.name;
+  const existTiddler = $tw.wiki.tiddlerExists(title);
+  if (!existTiddler) {
+    console.log(title + ' not found');
+    return;
+  }
+  // parentWidget.dispatchEvent( { type: 'tm-navigate', navigateTo: title } )
+  goto.navigateTiddler(title);
+};
+
+function onUpdate(myChart, _, addonAttributes) {
+  // 暴露参数给用户
+  const {
+    title: text,
+    filter = '[tags[]!prefix[$:/]]',
+    sort = 'descend',
+    width = 2,
+    radius = 10,
+    toolbox = 'hide',
+    doughnut,
+    focusSelf,
+    legend,
+  } = addonAttributes;
+  // data必须在执行onUpdate函数的时候获取到最新数据,不要写在函数外面
+  const data = [];
+
+  // alpha sort default
+  const tags = $tw.wiki.filterTiddlers(filter).sort();
+  tags.forEach((tag) => data.push(getData(tag)));
+
+  const borderWidth = data.length > 10 ? 0 : width;
+  const borderRadius = data.length > 10 ? 5 : radius;
+
+  // 如果类型过多, width 自动设置为0, 此时无视用户的width配置
+  // 配置具体参考echarts官方文档
+  const option = {
+    title: {
+      text,
+      subtext: '',
+      left: 'left',
+      top: 'top',
+    },
+    toolbox: {
+      show: toolbox === 'show' ? true : false,
+      left: 0,
+      bottom: 0,
+      feature: {
+        dataView: { show: true, readOnly: false },
+        restore: {},
+        saveAsImage: {},
+      },
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: function (params) {
+        const { name, value, percent } = params;
+        if (value) {
+          return `${name} 标签 有 ${value}个条目, ${percent}%`;
+        } else {
+          return name;
+        }
+      },
+    },
+    legend: {
+      show: legend === 'yes' ? true : false,
+      orient: 'vertical',
+      right: 10,
+      top: 20,
+      bottom: 20,
+      type: 'scroll',
+    },
+    series: [
+      {
+        name: 'Tag',
+        type: 'pie',
+        radius: doughnut === 'yes' ? ['40%', '70%'] : '50%',
+        center: legend === 'yes' ? ['40%', '50%'] : '50%',
+        data,
+        itemStyle: {
+          borderRadius,
+          borderWidth,
+          borderColor: '#fff',
+        },
+        emphasis: {
+          focus: focusSelf === 'yes' ? 'self' : '',
+          itemStyle: {},
+        },
+      },
+    ],
+  };
+
+  // descend or ascend sort
+  data.sort(function (a, b) {
+    return sort === 'descend' ? b.value - a.value : a.value - b.value;
+  });
+
+  myChart.setOption(option);
+  myChart.on('click', 'series', gotoTagTiddler);
+}
+
+// TODO: need refresh manually here
+function shouldUpdate(_, changedTiddlers) {
+  // changeTiddlers 会包含一些系统tiddler的状态变换tiddler, 应该去掉
+  const changedTiddlersLength = Object.keys(changedTiddlers).filter(
+    (tiddler) =>
+      !(tiddler.startsWith('$:/') || tiddler.startsWith('Draft of')).length,
+  );
+  return changedTiddlersLength ? true : false;
+}
+
+// function onMount() { }
+// function onUnmount() {}
+
+module.exports = {
+  onUpdate,
+  shouldUpdate,
+};
+
+/*
+ * @description: 理论上所有的option配置都可以暴露出来, 这里仅仅暴露一些常用的配置, echarts将这些工作交给了addon, 大概是不同类型的addon处理起来比较复杂,但是与此同时,addon就更具有扩展性
+ * @param: filter 默认是用户的所有tiddler, 但是你也可以使用 filter='[tag[Journal]]' 列出所有的 Journal tiddler
+ * @param: title
+ * @param: radius
+ * @param: sort {descend|ascend}
+ * @param: doughnut {'yes'}
+ * @param: width {0}
+ * @param: legend {'yes'}
+ * @param: toolbox {'show'|'hide'}
+ */
