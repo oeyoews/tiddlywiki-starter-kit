8fafc4c908e2c1808fdd1fffbe65fbff730cba17
oeyoews <jyao4783@gmail.com>
2023-10-18 16:08:01 +0800

    feat: add url params

--- plugins/oeyoews/tiddlywiki-videos/tiddlers/widget.js
+++ plugins/oeyoews/tiddlywiki-videos/tiddlers/widget.js
@@ -1,67 +1,65 @@
 /*\
-title: $:/plugins/oeyoews/tiddlywiki-videos/widget
+title: $:/plugins/oeyoews/tiddlywiki-videos/videos-widget.js
 type: application/javascript
 module-type: widget
 
 youtube widget
 \*/
 
-(function () {
-  const { widget: Widget } = require('$:/core/modules/widgets/widget.js');
+const { widget: Widget } = require('$:/core/modules/widgets/widget.js');
 
-  class YoutubeWidget extends Widget {
-    constructor(parseTreeNode, options) {
-      super(parseTreeNode, options);
-    }
-
-    render(parent, nextSibling) {
-      if (!$tw.browser) return;
-
-      this.parentDomNode = parent;
-      this.computeAttributes();
-      this.execute();
-
-      const createElement = $tw.utils.domMaker;
-
-      const { youtubeId = 'o7GyFa90klg', playlist } = this.attributes;
-
-      const prefix = 'https://www.youtube.com/embed/';
-
-      /* const iframeSrc = playlist
-        ? `${prefix}videoseries?list=${youtubeId}`
-        : `${prefix}${youtubeId}`; */
-      const midleUrl = playlist ? 'videoseries?list=' : '';
-      const src = prefix + midleUrl + youtubeId;
-
-      // Create an object to represent the iframe attributes
-      const iframeAttributes = {
-        src,
-        width: 800,
-        height: 450,
-        frameborder: 0,
-        allowfullscreen: '',
-        class: 'border-none shadow-lg rounded-lg', // w-full
-        allowsInlineMediaPlayback: 'true',
-        playsinline: '1',
-        title: 'youtube video', // example attribute
-      };
-
-      const iframeNode = this.document.createElement('iframe');
-
-      const domNode = createElement('div', {
-        class: 'flex justify-center item-center my-4',
-        children: [iframeNode],
-      });
-
-      Object.entries(iframeAttributes).forEach(([key, value]) => {
-        iframeNode.setAttribute(key, value);
-      });
-
-      parent.insertBefore(domNode, nextSibling);
-      this.domNodes.push(domNode);
-    }
+class YoutubeWidget extends Widget {
+  constructor(parseTreeNode, options) {
+    super(parseTreeNode, options);
   }
 
-  exports.youtube = YoutubeWidget;
-  exports.yt = YoutubeWidget;
-})();
+  render(parent, nextSibling) {
+    if (!$tw.browser) return;
+
+    this.parentDomNode = parent;
+    this.computeAttributes();
+    this.execute();
+
+    const createElement = $tw.utils.domMaker;
+
+    const { youtubeId = 'o7GyFa90klg', playlist, url } = this.attributes;
+
+    const prefix = 'https://www.youtube.com/embed/';
+
+    /* const iframeSrc = playlist
+        ? `${prefix}videoseries?list=${youtubeId}`
+        : `${prefix}${youtubeId}`; */
+    const midleUrl = playlist ? 'videoseries?list=' : '';
+    const src = url || prefix + midleUrl + youtubeId;
+
+    // Create an object to represent the iframe attributes
+    const iframeAttributes = {
+      src,
+      width: 800,
+      height: 450,
+      frameborder: 0,
+      allowfullscreen: '',
+      class: 'border-none shadow-lg rounded-lg', // w-full
+      allowsInlineMediaPlayback: 'true',
+      playsinline: '1',
+      title: 'youtube video', // example attribute
+    };
+
+    const iframeNode = this.document.createElement('iframe');
+
+    const domNode = createElement('div', {
+      class: 'flex justify-center item-center my-4',
+      children: [iframeNode],
+    });
+
+    Object.entries(iframeAttributes).forEach(([key, value]) => {
+      iframeNode.setAttribute(key, value);
+    });
+
+    parent.insertBefore(domNode, nextSibling);
+    this.domNodes.push(domNode);
+  }
+}
+
+exports.youtube = YoutubeWidget;
+exports.yt = YoutubeWidget;
--- templates/new-plugin/tiddlers/widget.js
+++ templates/new-plugin/tiddlers/widget.js
@@ -6,44 +6,38 @@ module-type: widget
 ${pluginname} widget
 
 \*/
-(function () {
-  /*jslint node: true, browser: true */
-  /*global $tw: false */
-  'use strict';
+const { widget: Widget } = require('$:/core/modules/widgets/widget.js');
 
-  const { widget: Widget } = require('$:/core/modules/widgets/widget.js');
-
-  class ExampleWidget extends Widget {
-    constructor(parseTreeNode, options) {
-      super(parseTreeNode, options);
-    }
-
-    render(parent, nextSibling) {
-      this.parentDomNode = parent;
-      this.computeAttributes();
-      this.execute();
-
-      const createElement = $tw.utils.domMaker;
-
-      const btn = createElement('button', {
-        text: 'click me',
-        class: 'rounded p-1',
-      });
-
-      const domNode = createElement('div', {
-        text: 'example',
-        class: 'underline font-bold',
-        children: [btn],
-      });
-
-      parent.insertBefore(domNode, nextSibling);
-      this.domNodes.push(domNode);
-    }
+class ExampleWidget extends Widget {
+  constructor(parseTreeNode, options) {
+    super(parseTreeNode, options);
   }
 
-  /**
-   * @description ${pluginname} widget
-   * @param xxx
-   */
-  exports.test = ExampleWidget;
-})();
+  render(parent, nextSibling) {
+    this.parentDomNode = parent;
+    this.computeAttributes();
+    this.execute();
+
+    const createElement = $tw.utils.domMaker;
+
+    const btn = createElement('button', {
+      text: 'click me',
+      class: 'rounded p-1',
+    });
+
+    const domNode = createElement('div', {
+      text: 'example',
+      class: 'underline font-bold',
+      children: [btn],
+    });
+
+    parent.insertBefore(domNode, nextSibling);
+    this.domNodes.push(domNode);
+  }
+}
+
+/**
+ * @description ${pluginname} widget
+ * @param xxx
+ */
+exports.test = ExampleWidget;
