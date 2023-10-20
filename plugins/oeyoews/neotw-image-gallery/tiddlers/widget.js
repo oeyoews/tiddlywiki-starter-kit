b65803c5ac1fb3a765a3603213f66c0356740b3b
oeyoews <jyao4783@gmail.com>
2023-10-20 12:29:17 +0800

    feat: add fs-extra to support copy

---
+++ plugins/oeyoews/neotw-image-gallery/tiddlers/widget.js
@@ -0,0 +1,43 @@
+/*\
+title: $:/plugins/oeyoews/neotw-image-gallery/widget.js
+type: application/javascript
+module-type: widget
+
+neotw-image-gallery widget
+
+\*/
+const { widget: Widget } = require('$:/core/modules/widgets/widget.js');
+
+class ExampleWidget extends Widget {
+  constructor(parseTreeNode, options) {
+    super(parseTreeNode, options);
+  }
+
+  render(parent, nextSibling) {
+    this.parentDomNode = parent;
+    this.computeAttributes();
+    this.execute();
+
+    const createElement = $tw.utils.domMaker;
+
+    const btn = createElement('button', {
+      text: 'click me',
+      class: 'rounded p-1',
+    });
+
+    const domNode = createElement('div', {
+      text: 'example',
+      class: 'underline font-bold',
+      children: [btn],
+    });
+
+    parent.insertBefore(domNode, nextSibling);
+    this.domNodes.push(domNode);
+  }
+}
+
+/**
+ * @description neotw-image-gallery widget
+ * @param xxx
+ */
+exports.test = ExampleWidget;
