7d457f36cc7c3f6dfe99095fd5311f4e3b221e04
oeyoews <jyao4783@gmail.com>
2023-10-27 15:58:43 +0800

    update rendertiddler

--- plugins/oeyoews/hitokoto/tiddlers/widget.js
+++ plugins/oeyoews/hitokoto/tiddlers/widget.js
@@ -7,7 +7,6 @@ hitokoto widget
 
 \*/
 const { widget: Widget } = require('$:/core/modules/widgets/widget.js');
-const MarkdownIt = require('$:/plugins/tiddlywiki/markdown/markdown-it');
 const getRandomColor = require('./getRandomColor');
 
 class HitokotoWidget extends Widget {
@@ -27,46 +26,19 @@ class HitokotoWidget extends Widget {
     );
 
     const children = [];
-    function parseAsBoolean(tiddlerName) {
-      return (
-        $tw.wiki.getTiddlerText(tiddlerName, 'false').trim().toLowerCase() ===
-        'true'
-      );
-    }
-
-    const md = new MarkdownIt({
-      html: true,
-      xhtmlOut: true,
-      breaks: parseAsBoolean('$:/config/markdown/breaks'),
-      quotes: $tw.wiki.getTiddlerText('$:/config/markdown/quotes').trim(),
-      typographer: parseAsBoolean('$:/config/markdown/typographer'),
-      linkify: parseAsBoolean('$:/config/markdown/linkify'),
-    });
-
-    // bug
-    // .use(require('$:/plugins/tiddlywiki/markdown/markdown-it-tiddlywiki.js'))
-    const admonition = '$:/plugins/cdr/markdown-more/markdown-it-admonition.js';
-    const checklist = '$:/plugins/cdr/markdown-more/markdown-it-checklist.js';
-    if ($tw.wiki.tiddlerExists('$:/plugins/cdr/markdown-more')) {
-      md.use(require(admonition)).use(require(checklist));
-    }
 
     journalTiddlers.map((tiddler) => {
       const color = getRandomColor();
-      const { text, created, creator, type, title } =
-        $tw.wiki.getTiddler(tiddler).fields;
+      console.log(color, 'xxx');
+      const { created, creator, title } = $tw.wiki.getTiddler(tiddler).fields;
       let content;
-      if (type == 'text/markdown') {
-        content = md.render(text);
-      } else {
-        // https://github.com/Jermolene/TiddlyWiki5/pull/7413
-        content = $tw.wiki.renderText('text/html', 'text/vnd.tiddlywiki', text);
-      }
+      // https://github.com/Jermolene/TiddlyWiki5/pull/7413
+      content = $tw.wiki.renderTiddler('text/html', title);
       const link = `[[${title}]]`;
       // TODO: use eventlisteners
       // content += $tw.wiki.renderText('text/html', 'text/vnd.tiddlywiki', link);
       const htNode = this.document.createElement('blockquote');
-      htNode.className = `my-4 bg-${color}-200 p-2 rounded border-l-[3px] border-l-${color}-300 mx-0`;
+      htNode.className = `my-4 bg-${color}-100/50 px-2 rounded border-l-[3px] border-l-${color}-300 mx-0`;
       htNode.innerHTML = content;
       children.push(htNode);
     });
