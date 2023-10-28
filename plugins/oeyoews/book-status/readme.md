1b708745fd4b9657dea4e738de348654860d37d4
oeyoews <jyao4783@gmail.com>
2023-10-28 12:08:44 +0800

    feat: add tocstatus

--- plugins/oeyoews/book-status/readme.md
+++ plugins/oeyoews/book-status/readme.md
@@ -4,11 +4,13 @@
 
 ## TODO
 
-* [x] getstorytiddler
-* [x] click event
-* [x] status json
-* [x] 如何获取每个笔记的书名, 通过sourceTiddler, 或者获取tag, 每本书只有一个tag
-* [ ] viewtemplate(filter: $:/plugins/books/) 也许可以做成viewtoolbar, 但是不够醒目
-* [ ] 也许可以借助localstorage当作临时状态(), 最后结束是, 询问是否要保存此次阅读记录, 通知阅读时间
-* [ ] 支持待看状态
-* [ ] 每本书一个配置文件, 目前是一个文件
+- [ ] 在每个目录旁边显示是否阅读过, bingo/x
+- [ ] 获取目录json数据
+- [x] getstorytiddler
+- [x] click event
+- [x] status json
+- [x] 如何获取每个笔记的书名, 通过sourceTiddler, 或者获取tag, 每本书只有一个tag
+- [ ] viewtemplate(filter: $:/plugins/books/) 也许可以做成viewtoolbar, 但是不够醒目
+- [ ] 也许可以借助localstorage当作临时状态(), 最后结束是, 询问是否要保存此次阅读记录, 通知阅读时间
+- [ ] 支持待看状态
+- [ ] 每本书一个配置文件, 目前是一个文件
---
+++ plugins/oeyoews/book-status/tiddlers/tocstatus.js
@@ -0,0 +1,70 @@
+/*\
+title: $:/plugins/oeyoews/book-status/tocstatus.js
+type: application/javascript
+module-type: widget
+
+使用wikitext 应该也能做, 但是数据在json里面, 比较麻烦
+\*/
+const { widget: Widget } = require('$:/core/modules/widgets/widget.js');
+
+class BookTocStatusWidget extends Widget {
+  constructor(parseTreeNode, options) {
+    super(parseTreeNode, options);
+  }
+
+  render(parent, nextSibling) {
+    if (!$tw.browser) return;
+    this.parentDomNode = parent;
+    this.computeAttributes();
+    this.execute();
+
+    const { bookname = '劫持' } = this.attributes;
+    const wiki = $tw.wiki;
+    const createElement = $tw.utils.domMaker;
+    const statusfilename = 'bookstatus.json';
+    const statuses = wiki.getTiddlerData(statusfilename);
+    const pluginname = '$:/plugins/books/' + bookname;
+    if (!wiki.tiddlerExists(pluginname)) return;
+    const { tiddlers } = wiki.getPluginInfo(pluginname);
+    const toc = Object.keys(tiddlers);
+    const status = statuses[bookname];
+
+    const tocStatusData = toc.map((title) => [title, '未读']);
+    const mergedStatus = Object.assign(tocStatusData, Object.entries(status));
+    const children = [];
+
+    // update status
+    const createLi = (title, status) => {
+      const li = this.document.createElement('li');
+      const color = status === '已读' ? 'green' : 'red';
+      const content = $tw.wiki.renderText(
+        'text/html',
+        'text/vnd.tiddlywiki',
+        `[[${title}]] <sup>@@color:${color};${status}@@</sup>`,
+      );
+      li.innerHTML = content;
+      children.push(li);
+      li.addEventListener('click', (e) => {
+        e.preventDefault();
+        new $tw.Story().navigateTiddler(title);
+      });
+    };
+
+    mergedStatus.forEach(([title, status]) => {
+      createLi(title, status);
+    });
+
+    const domNode = createElement('ol', {
+      children,
+    });
+
+    parent.insertBefore(domNode, nextSibling);
+    this.domNodes.push(domNode);
+  }
+}
+
+/**
+ * @description book-status widget
+ * @param bookname
+ */
+exports.tocstatus = BookTocStatusWidget;
