c198b7605d97c1a26f3c1a17da52b77568a2dc7c
oeyoews <jyao4783@gmail.com>
2023-10-28 19:24:55 +0800

    Update prevent-js.js

--- plugins/oeyoews/neotw/tiddlers/startup/prevent-js.js
+++ plugins/oeyoews/neotw/tiddlers/startup/prevent-js.js
@@ -3,7 +3,8 @@ title: $:/plugins/oeyoews/neotw/prevent-edit.js
 type: application/javascript
 module-type: startup
 
-退出网页不提示??.
+退出网页不提示
+https://talk.tiddlywiki.org/t/prevent-prevent-leave-you-have-unsaved-changes/8192
 \*/
 
 exports.startup = function () {
