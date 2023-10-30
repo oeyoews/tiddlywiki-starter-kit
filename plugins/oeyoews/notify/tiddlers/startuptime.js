acca11f1bded2f8633d5b7a7cc2cdb8568b1cbca
oeyoews <jyao4783@gmail.com>
2023-10-30 19:48:20 +0800

    Update startuptime.js

--- plugins/oeyoews/notify/tiddlers/startuptime.js
+++ plugins/oeyoews/notify/tiddlers/startuptime.js
@@ -11,11 +11,19 @@ exports.after = ['load-modules'];
 exports.synchronous = true;
 exports.startup = () => {
   try {
+    const configfilename = '$:/config/tiddlywiki-starter-kit/setting.json';
+    if (!$tw.wiki.tiddlerExists(configfilename)) {
+      // TODO: Create config file
+      console.warn(`${configfilename} 文件不存在`);
+    }
+
+    const settings = $tw.wiki.getTiddlerData(configfilename) || {};
     const notifier = new $tw.Notify();
     const startTime = performance.now();
 
     window.addEventListener('load', () => {
       const loadTime = Math.floor(performance.now() - startTime);
+      if (settings?.['notify#startuptime'] === 'no') return;
       notifier.display({
         title: '启动时间',
         text: `本次启动耗时约 ${loadTime}ms`,
