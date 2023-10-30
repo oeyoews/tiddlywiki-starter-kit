5ae163523cef51a5a9c94c5b716f6e55c59fa30c
oeyoews <jyao4783@gmail.com>
2023-10-30 09:05:10 +0800

    refactor(qrcode): render to image

--- plugins/oeyoews/qrcode/tiddlers/widget.js
+++ plugins/oeyoews/qrcode/tiddlers/widget.js
@@ -25,38 +25,15 @@ class QRCodeWidget extends Widget {
     this.computeAttributes();
     this.execute();
 
-    // canvas 的清晰度不如svg
-    // const canvas = this.document.createElement('canvas');
-    // QRCode.toCanvas(canvas, 'this is a qrcode test.', {
-    //   width: 512,
-    // });
-
-    const parser = new DOMParser();
     const storyTiddler = this.getVariable('storyTiddler');
     const href = location.href;
     const hashurl = encodeURIComponent(storyTiddler);
     const currentTiddlerUrl = `${href}#${hashurl}`;
     const {
       renderType,
-      text = renderType === 'url' && storyTiddler
-        ? currentTiddlerUrl
-        : $tw.wiki.getTiddlerText('$:/info/url/full'),
+      text = renderType === 'url' && storyTiddler ? currentTiddlerUrl : href,
       width = 256,
-      title = 'info-qrcode.svg',
-      save = false,
-      // modal = false,
-      //   timestamp = false
     } = this.attributes;
-    let domNode;
-
-    const tmpTiddlerTitle = '$:/temp/svgTiddler';
-    const generateSvgtiddler = (title, text = this.svgString) => {
-      $tw.wiki.addTiddler({
-        type: this.type,
-        title,
-        text,
-      });
-    };
 
     QRCode.toString(
       text,
@@ -69,22 +46,10 @@ class QRCodeWidget extends Widget {
       },
     );
 
-    save === 'true' && generateSvgtiddler(title);
-    const svgDoc = parser.parseFromString(this.svgString, this.type);
-
-    domNode = svgDoc.documentElement;
-
-    // domNode.addEventListener('click', () => {
-    //   generateSvgtiddler(tmpTiddlerTitle);
-    //   this.parentWidget.dispatchEvent({
-    //     type: 'tm-modal',
-    //     param: tmpTiddlerTitle,
-    //   });
-    // });
-
-    // if (!modal) {
-    //   domNode.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M8 21H4a1 1 0 0 1-1-1v-4a1 1 0 0 0-2 0v4a3 3 0 0 0 3 3h4a1 1 0 0 0 0-2Zm14-6a1 1 0 0 0-1 1v4a1 1 0 0 1-1 1h-4a1 1 0 0 0 0 2h4a3 3 0 0 0 3-3v-4a1 1 0 0 0-1-1ZM20 1h-4a1 1 0 0 0 0 2h4a1 1 0 0 1 1 1v4a1 1 0 0 0 2 0V4a3 3 0 0 0-3-3ZM2 9a1 1 0 0 0 1-1V4a1 1 0 0 1 1-1h4a1 1 0 0 0 0-2H4a3 3 0 0 0-3 3v4a1 1 0 0 0 1 1Zm8-4H6a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1ZM9 9H7V7h2Zm5 2h4a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1Zm1-4h2v2h-2Zm-5 6H6a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1Zm-1 4H7v-2h2Zm5-1a1 1 0 0 0 1-1a1 1 0 0 0 0-2h-1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1Zm4-3a1 1 0 0 0-1 1v3a1 1 0 0 0 0 2h1a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1Zm-4 4a1 1 0 1 0 1 1a1 1 0 0 0-1-1Z"/></svg>`;
-    // }
+    const domNode = this.document.createElement('img');
+    const svghashtext = encodeURIComponent(this.svgString);
+    domNode.src = `data:${this.type},${svghashtext}`;
+    domNode.className = 'spotlight';
 
     parent.insertBefore(domNode, nextSibling);
     this.domNodes.push(domNode);
