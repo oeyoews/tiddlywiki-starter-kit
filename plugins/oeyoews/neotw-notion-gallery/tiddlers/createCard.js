02b3d2a342958264ff58078c4adeba48764fe587
oeyoews <jyao4783@gmail.com>
2023-11-02 10:44:11 +0800

    update

--- plugins/oeyoews/neotw-notion-gallery/tiddlers/createCard.js
+++ plugins/oeyoews/neotw-notion-gallery/tiddlers/createCard.js
@@ -4,6 +4,8 @@ type: application/javascript
 module-type: library
 
 \*/
+
+// @see-also: https://www.bilibili.com/video/BV1FU4y157Li/?spm_id_from=333.788&vd_source=d6afd7eedd9f9c940321c63f0a1539e3
 /** create card */
 module.exports = function createCard(
   title,
@@ -20,21 +22,25 @@ module.exports = function createCard(
 
   const createElement = $tw.utils.domMaker;
 
-  function callback(entries, observer) {
+  const callback = (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         const image = entry.target;
+        // 开始加载图片
         image.src = cover;
+        // 加载动效
         image.onload = () => {
           img.alt = title;
           image.classList.remove(...dynamicClassNames.split(' '));
           img.classList.add('cursor-pointer');
         };
-        observer.unobserve(image); // 加载后取消监测
+        // 加载图片后取消监测
+        observer.unobserve(image);
       }
     });
-  }
+  };
 
+  // 添加观察者
   const observer = new IntersectionObserver(callback, options);
 
   const dynamicClassNames = 'scale-105 blur-md bg-black/10 cursor-wait';
@@ -79,8 +85,10 @@ module.exports = function createCard(
     children: [img, contentNode],
   });
 
+  // 观察图片
   observer.observe(img);
 
+  // 禁止右键功能
   item.addEventListener('contextmenu', (e) => {
     e.preventDefault();
   });
