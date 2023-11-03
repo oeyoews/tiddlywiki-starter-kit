522be6d4593fdf983654514acc98cb44e782e05c
oeyoews <jyao4783@gmail.com>
2023-11-03 11:40:39 +0800

    Update startup.js

--- plugins/oeyoews/neotw-fetch/tiddlers/startup.js
+++ plugins/oeyoews/neotw-fetch/tiddlers/startup.js
@@ -12,14 +12,13 @@ exports.synchronous = true;
 
 exports.startup = () => {
   const { addfile } = require('./addfile');
-
-  const readmeURL =
-    'https://raw.githubusercontent.com/oeyoews/neotw/main/README.md';
-
   $tw.rootWidget.addEventListener('om-fetch', (event) => {
     const time = new Date().getTime();
     const {
-      paramObject: { url = readmeURL, fileName = `fetch-${time}` } = {},
+      paramObject: {
+        url = 'https://raw.githubusercontent.com/oeyoews/neotw/main/README.md',
+        fileName = `fetch-${time}`,
+      } = {},
     } = event;
     addfile(url, fileName);
   });
