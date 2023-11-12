0329fbdaa4eadb21da12c1891995f6c24d897525
oeyoews <jyao4783@gmail.com>
2023-11-12 16:37:49 +0800

    update

--- plugins/oeyoews/neotw-icons/tiddlers/widget.js
+++ plugins/oeyoews/neotw-icons/tiddlers/widget.js
@@ -5,39 +5,39 @@ module-type: widget
 
 neotw-icons widget
 \*/
-  const Widget = require('$:/core/modules/widgets/widget.js').widget;
+const Widget = require('$:/core/modules/widgets/widget.js').widget;
 
-  class IconsWidget extends Widget {
-    constructor(parseTreeNode, options) {
-      super(parseTreeNode, options);
-    }
-
-    render(parent, nextSibling) {
-      if (!$tw.browser) return;
-      this.parentDomNode = parent;
-      this.computeAttributes();
-      this.execute();
-
-      const {
-        title = '',
-        style = '',
-        icon = 'simple-icons:tiddlywiki',
-        class: classNames = '',
-      } = this.attributes;
-
-      // NOTE: dont use dommaker
-      const iconNode = this.document.createElement('iconify-icon');
-
-      iconNode.setAttribute('icon', icon);
-      iconNode.title = title;
-      iconNode.style = style;
-      iconNode.className = classNames;
-      iconNode.style.verticalAlign = 'middle';
-
-      parent.insertBefore(iconNode, nextSibling);
-      this.domNodes.push(iconNode);
-    }
+class IconsWidget extends Widget {
+  constructor(parseTreeNode, options) {
+    super(parseTreeNode, options);
   }
 
-  exports['iconify'] = IconsWidget;
-  exports['iconify-icon'] = IconsWidget;
+  render(parent, nextSibling) {
+    if (!$tw.browser) return;
+    this.parentDomNode = parent;
+    this.computeAttributes();
+    this.execute();
+
+    const {
+      title = '',
+      style = '',
+      icon = 'simple-icons:tiddlywiki',
+      class: classNames = '',
+    } = this.attributes;
+
+    // NOTE: dont use dommaker
+    const iconNode = this.document.createElement('iconify-icon');
+
+    iconNode.setAttribute('icon', icon);
+    iconNode.title = title;
+    iconNode.style = style;
+    iconNode.className = classNames;
+    iconNode.style.verticalAlign = 'middle';
+
+    parent.insertBefore(iconNode, nextSibling);
+    this.domNodes.push(iconNode);
+  }
+}
+
+exports['iconify'] = IconsWidget;
+exports['iconify-icon'] = IconsWidget;
