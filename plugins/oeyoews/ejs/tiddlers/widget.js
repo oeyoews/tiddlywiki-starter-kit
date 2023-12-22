38ddfa8623ae0ed3f3d1121bef418a4630236a70
oeyoews <jyao4783@gmail.com>
2023-12-22 17:11:33 +0800

    update

--- plugins/oeyoews/ejs/tiddlers/widget.js
+++ plugins/oeyoews/ejs/tiddlers/widget.js
@@ -19,10 +19,22 @@ class EJSWidget extends Widget {
     this.computeAttributes();
     this.execute();
 
+    const templateFile = '$:/plugins/oeyoews/ejs/templates/test.ejs';
+    // NOTE: 不要用$tw.wiki.tiddlerExists 判断插件的某一个tiddler是否存在
+    // if ($tw.wiki.tiddlerExists(templateFile)) {
+    //   console.warn(templateFile, '文件不存在');
+    //   return;
+    // }
+    const template = $tw.wiki.getTiddlerText(templateFile);
+
+    if (!template) {
+      return;
+    }
+
     const ejs = require('./ejs.min.js');
     const createElement = $tw.utils.domMaker;
     const people = ['geddy', 'neil', 'alex'];
-    const html = ejs.render('<%= people.join(", "); %>', { people: people });
+    const html = ejs.render(template, { people });
 
     const domNode = createElement('div', {});
     domNode.innerHTML = html;
