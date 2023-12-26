b2f8285863c353318b3e0e063195152c5cc7d1f3
oeyoews <jyao4783@gmail.com>
2023-12-26 17:44:35 +0800

    update

--- plugins/oeyoews/translate/readme.md
+++ plugins/oeyoews/translate/readme.md
@@ -2,6 +2,12 @@
 
 > 默认翻译引擎为 Google Translate
 
+## Recipes
+
+- 建议翻译纯文本, 不建议翻译带有 wikitext, 因为 wikitext 不支持全角字符 ! 等字符, 所以一些标题不会被识别
+
+> 其实也可以将翻译的内容提前渲染成 html, 然后翻译(还没试过)
+
 ## Motivation
 
 <!-- your plugin motivation, or why you write this plugin -->
--- plugins/oeyoews/translate/tiddlers/startup.js
+++ plugins/oeyoews/translate/tiddlers/startup.js
@@ -13,20 +13,47 @@ exports.after = ['startup'];
 exports.synchronous = true;
 exports.startup = () => {
   const translate = require('./translate.min.js');
-  // TODO: support cn <--> zh
+  const story = new $tw.Story();
+  // TODO: support cn <--> zh on viewtoolbar
   $tw.rootWidget.addEventListener('om-translate-tiddler', async (event) => {
     const title = event.tiddlerTitle;
-    const newTitle = title + '-cn';
+    // TODO: support translate english title
+    const type = $tw.wiki.getTiddler(title).fields.type;
     const content = $tw.wiki.getTiddlerText(title);
-    const text = await translate(content, 'zh');
+    const nprogress = new $tw.NProgress();
+
+    nprogress.start();
+
+    const translatedTitle = await translate(title, 'zh');
+    const newTitle =
+      String(translatedTitle).toLocaleLowerCase() === title.toLocaleLowerCase()
+        ? title + '-cn'
+        : translatedTitle;
     // NOTE: 似乎每次的翻译内容不会100%保持一致, 所以无法防止多次重复翻译
+    // 需要检查 english title to cn title, 的tiddler 是否存在过
+    if ($tw.wiki.tiddlerExists(newTitle)) {
+      console.warn(newTitle, ' 条目已存在');
+      story.navigateTiddler(newTitle);
+      nprogress.done();
+      return false;
+    }
+    const text = await translate(content, 'zh');
     $tw.wiki.addTiddler({
       title: newTitle,
       text,
+      type,
       translate: 'yes',
     });
-    const story = new $tw.Story();
+
+    // 添加字段, 为模板数据做准备
+    $tw.wiki.setText(title, 'cn', null, newTitle, {
+      suppressTimestamp: true,
+    });
+    $tw.wiki.setText(newTitle, 'en', null, title, {
+      suppressTimestamp: true,
+    });
     story.navigateTiddler(newTitle);
     // TODO: add notify
+    nprogress.done();
   });
 };
