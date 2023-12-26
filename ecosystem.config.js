9e035724207fd2f91a323f5fab8496b86e12888d
oeyoews <jyao4783@gmail.com>
2023-12-26 23:25:07 +0800

    update version

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       name: tiddlywikiName,
       script: './lib/startup.mjs',
       watch: ['./plugins', './themes'],
-      ignore_watch: ['./tiddlers', '**/tiddlers'],
+      ignore_watch: ['./tiddlers', '**/tiddlers', 'package/*'],
       max_memory_restart: '1024M', // 更多的情况是，tw 的浏览器实例卡死，而不是 server 端内存泄露，所以内存限制基本没用
       max_restarts: 3, // 似乎不生效
       exec_mode: 'cluster',
