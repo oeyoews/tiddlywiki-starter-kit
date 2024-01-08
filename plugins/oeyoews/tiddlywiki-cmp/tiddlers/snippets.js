570950ca9a6d551f837a8f725ce7d0b2c9cf9ce1
oeyoews <jyao4783@gmail.com>
2024-01-08 16:11:02 +0800

    feat: support emoji list

--- plugins/oeyoews/tiddlywiki-cmp/tiddlers/snippets.js
+++ plugins/oeyoews/tiddlywiki-cmp/tiddlers/snippets.js
@@ -6,6 +6,12 @@ module-type: snippets
 tiddlywiki-cmp module
 
 \*/
+const emojis = require('./emojilist');
+
+const emojisnippets = Object.entries(emojis).map((emoji) => ({
+  title: emoji[0],
+  text: emoji[1]
+}));
 
 module.exports = [
   {
@@ -19,5 +25,6 @@ module.exports = [
   {
     title: 'unsplash image',
     text: 'https://source.unsplash.com/random/1920x1080?fm=blurhash&${sea}'
-  }
+  },
+  ...emojisnippets
 ];
