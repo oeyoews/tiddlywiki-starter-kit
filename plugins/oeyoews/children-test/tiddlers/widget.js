e1fb7ac246d562f6b8278691e2172efe85fa564b
oeyoews <jyao4783@gmail.com>
2024-01-11 15:38:36 +0800

    to class

--- plugins/oeyoews/children-test/tiddlers/widget.js
+++ plugins/oeyoews/children-test/tiddlers/widget.js
@@ -6,75 +6,76 @@ module-type: widget
 children-test widget
 
 \*/
-var Widget = require('$:/core/modules/widgets/widget.js').widget;
 
-var TWSRRuby = function (parseTreeNode, options) {
-  this.initialise(parseTreeNode, options);
-};
+// Import the Widget class from the widget.js file
+const Widget = require('$:/core/modules/widgets/widget.js').widget;
 
-TWSRRuby.prototype = new Widget(); //Inherit from the basey widget class
-
-TWSRRuby.prototype.execute = function () {
-  Widget.prototype.execute.call(this);
-};
-
-//Render this widget into the DOM
-_RubyRender = function (_this, parent, nextSibling, useAnswer) {
-  var oc = _this.parseTreeNode.children;
-  if (true) {
-    _this.parentDomNode = parent;
-    _this.computeAttributes();
-    var rubyPosition = 'over';
-    var ruby = _this.getAttribute('u');
-    if (!ruby || ruby == '') {
-      var ruby = _this.getAttribute('l');
-      if (!ruby || ruby == '') {
-      } else {
-        rubyPosition = 'under';
-      }
-    }
-
-    var p = _this;
-    var tags = [];
-    while (p) {
-      var ct = p.getVariable('currentTiddler');
-      var tid = $tw.wiki.getTiddler(ct);
-      if (tid) {
-        tags = tags.concat(tid.getFieldList('tags'));
-        tags = tags.filter(function (item, pos) {
-          return tags.indexOf(item) == pos;
-        });
-      }
-      p = p.parentWidget;
-    }
-
-    var container = _this.document.createElement('span');
-    Widget.prototype.render.call(_this, container, null);
-    var rtTxt =
-      '<rt>' +
-      (useAnswer ? '<$a s>' : '') +
-      ruby +
-      (useAnswer ? '</$a>' : '') +
-      '</rt>';
-    var wikiParser = $tw.wiki.parseText(
-      'text/vnd.tiddlywiki',
-      '<ruby style="ruby-position:' +
-        rubyPosition +
-        '">' +
-        container.innerHTML +
-        rtTxt +
-        '</ruby>',
-      { parseAsInline: true }
-    );
-    console.log(wikiParser.tree);
-    _this.parseTreeNode.children = wikiParser.tree;
+// Define the TWSRRuby class
+class TWSRRuby extends Widget {
+  constructor(parseTreeNode, options) {
+    super(parseTreeNode, options);
   }
-  Widget.prototype.render.call(_this, parent, nextSibling);
-  _this.parseTreeNode.children = oc;
-};
 
-TWSRRuby.prototype.render = function (parent, nextSibling) {
-  _RubyRender(this, parent, nextSibling, true);
-};
+  // Execute method
+  execute() {
+    super.execute();
+  }
 
+  // Render method
+  render(parent, nextSibling) {
+    this._RubyRender(this, parent, nextSibling, true);
+  }
+
+  // Custom render method
+  _RubyRender(_this, parent, nextSibling, useAnswer) {
+    const oc = _this.parseTreeNode.children;
+    if (true) {
+      _this.parentDomNode = parent;
+      _this.computeAttributes();
+      let rubyPosition = 'over';
+      let ruby = _this.getAttribute('u');
+      if (!ruby || ruby === '') {
+        ruby = _this.getAttribute('l');
+        if (!ruby || ruby === '') {
+        } else {
+          rubyPosition = 'under';
+        }
+      }
+
+      let p = _this;
+      let tags = [];
+      while (p) {
+        const ct = p.getVariable('currentTiddler');
+        const tid = $tw.wiki.getTiddler(ct);
+        if (tid) {
+          tags = tags.concat(tid.getFieldList('tags'));
+          tags = tags.filter((item, pos) => {
+            return tags.indexOf(item) === pos;
+          });
+        }
+        p = p.parentWidget;
+      }
+
+      const container = _this.document.createElement('span');
+      super.render(container, null);
+      const rtTxt =
+        '<rt>' +
+        (useAnswer ? '<$a s>' : '') +
+        ruby +
+        (useAnswer ? '</$a>' : '') +
+        '</rt>';
+      const wikiParser = $tw.wiki.parseText(
+        'text/vnd.tiddlywiki',
+        `<ruby style="ruby-position:${rubyPosition}">${container.innerHTML}${rtTxt}</ruby>`,
+        { parseAsInline: true }
+      );
+      console.log(wikiParser.tree);
+      _this.parseTreeNode.children = wikiParser.tree;
+    }
+    super.render(parent, nextSibling);
+    _this.parseTreeNode.children = oc;
+  }
+}
+
+// Export the TWSRRuby class
 exports.r = TWSRRuby;
