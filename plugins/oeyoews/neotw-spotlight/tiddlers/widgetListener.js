b94c687d3c32fbc1b7e1e9c1bd52bcfe30320e60
oeyoews <jyao4783@gmail.com>
2024-01-26 13:40:20 +0800

    update

--- plugins/oeyoews/neotw-spotlight/tiddlers/widgetListener.js
+++ plugins/oeyoews/neotw-spotlight/tiddlers/widgetListener.js
@@ -36,7 +36,6 @@ class SpotlightWidget extends Widget {
     };
     // 使用事件委托，将点击事件委托给父元素 tiddlerNode
     tiddlerNode.addEventListener('click', (event) => {
-      console.log('update');
       const target = event.target;
       if (target.tagName.toLowerCase() === 'img') {
         const src = target.getAttribute('src');
