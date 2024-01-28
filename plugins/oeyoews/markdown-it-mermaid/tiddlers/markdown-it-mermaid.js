78abc5197e8c093dee4def35293ba6c5ffb39bb3
oeyoews <jyao4783@gmail.com>
2024-01-28 18:02:05 +0800

    update

--- plugins/oeyoews/markdown-it-mermaid/tiddlers/markdown-it-mermaid.js
+++ plugins/oeyoews/markdown-it-mermaid/tiddlers/markdown-it-mermaid.js
@@ -6,33 +6,50 @@ const MermaidPlugin = (md) => {
   // extends md api: add mermaid api
   md.mermaid = mermaid;
 
-  const temp = md.renderer.rules.fence.bind(md.renderer.rules);
+  const defaultFenceRender = md.renderer.rules.fence;
 
   /*   mermaid.parseError = function (err, hash) {
     console.log(err);
   };
  */
-  md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
+  const renderNode = (text, idx) => {
+    const innerHTML = mermaid.render('mermaid_' + idx, text);
+    return `<div>${innerHTML}</div>`;
+  };
+  const customMermaidFenceRender = (tokens, idx, options, env, slf) => {
     const token = tokens[idx];
     const code = token.content.trim();
     // TODO: support render title
     // TODO: use https://github.com/agoose77/markdown-it-mermaid/blob/main/src/index.ts to render as img to encodeurl
     const [type, theme, ...title] = token.info.split(' ');
-    if (type === 'mermaid') {
+    if (type.trim() !== 'mermaid') {
+      return defaultFenceRender(tokens, idx, (options = {}), env, slf);
+    } else if (type.trim() === 'mermaid') {
       try {
+        // @see-also: https://mermaid.js.org/config/schema-docs/config.html
+        /* const palette = $tw.wiki.getTiddlerText('$:/palette');
+        const darkMode =
+          $tw.wiki.getTiddler(palette)?.fields['color-scheme'] === 'dark'
+            ? true
+            : false; */
         const config = {
           securityLevel: 'loose',
-          theme: theme || 'default',
-          startOnLoad: false,
-          htmlLabels: true,
-          ...options
+          theme: theme || 'default', //  "default" | "forest" | "dark" | "neutral"
+          // startOnLoad: false, // ???
+          htmlLabels: true
+          // TODO: NOT work
+          // darkMode
+          // ...options // 这里会导致渲染问题
         };
         mermaid.initialize(config);
-        return mermaid.render('mermaid' + idx, code);
-      } catch (e) {}
+        return renderNode(code, idx);
+      } catch (e) {
+        return `<pre>${code}</pre>`;
+      }
     }
-    return temp(tokens, idx, options, env, slf);
   };
+
+  md.renderer.rules.fence = customMermaidFenceRender;
 };
 
 module.exports = MermaidPlugin;
