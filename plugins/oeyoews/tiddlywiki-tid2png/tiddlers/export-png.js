8c39c6e0213da51dcba59236fc283e2a8a250e06
oeyoews <jyao4783@gmail.com>
2024-01-30 22:46:06 +0800

    update

--- plugins/oeyoews/tiddlywiki-tid2png/tiddlers/export-png.js
+++ plugins/oeyoews/tiddlywiki-tid2png/tiddlers/export-png.js
@@ -7,7 +7,34 @@ export
 \*/
 const html2canvas = require('html2canvas.min.js');
 
+const downloadSvg = $tw.wiki.getTiddlerText(
+  '$:/plugins/oeyoews/tiddlywiki-tid2png/download.svg'
+);
+const progress = $tw.NProgress;
+
+const hideElements = [
+  // '.gk0wk-notionpagebg',
+  // '.tc-tiddler-controls' // 必须要使用 style: display: !important
+  // '.tc-tags-wrapper'
+  // '.tc-subtitle',
+];
+
+function hideElementsWithSelectors(selectors, show) {
+  selectors.forEach((selector) => {
+    const elements = targetEl.querySelectorAll(selector);
+    elements.forEach((el) => {
+      // el.hidden = show;
+      el.style.setProperty('display', show, 'important');
+    });
+  });
+}
+
 module.exports = async function exportPng(title, customSelector) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/showOpenFilePicker
+  // NOTE: 这仍然无法获取用户是否取消了操作.
+  let isExporting = localStorage.getItem('tid2png') || 'no';
+  if (isExporting === 'yes') alert('正在导出 ...');
+  localStorage.setItem('tid2png', 'yes');
   if (customSelector && !this.document.querySelector(customSelector)) {
     $tw.wiki.addTiddler({
       title: '$:/temp/export-png',
@@ -17,43 +44,19 @@ module.exports = async function exportPng(title, customSelector) {
     return;
   }
 
-  const downloadSvg = $tw.wiki.getTiddlerText(
-    '$:/plugins/oeyoews/tiddlywiki-tid2png/download.svg'
-  );
-  const progress = $tw.NProgress;
-
   progress.start();
   const selector = customSelector || `[data-tiddler-title="${title}"]`;
   // html2canvas 不支持 cloneNode, 在 widget 中可以直接移除 popup，因为 widget 会重新渲染，popup 会自动恢复？但是这是一个 listener, 不建议直接修改 dom;
   // 下面使用了 hidden 隐藏 titlebar 元素，实际页面不会被用户感知到有所抖动 (由于 html2canvas 是异步)
   const targetEl = document.querySelector(selector);
 
-  const hideElements = [
-    // '.gk0wk-notionpagebg', '.tc-tiddler-controls'
-    // '.tc-tags-wrapper'
-    // '.tc-subtitle',
-  ];
-
-  async function hideElementsWithSelectors(selectors, show) {
-    await selectors.forEach((selector) => {
-      const elements = targetEl.querySelectorAll(selector);
-      elements.forEach((el) => {
-        // NOTE: 有的元素无法隐藏掉，maybe use css text and important
-        el.hidden = show;
-      });
-    });
-  }
-
-  hideElementsWithSelectors(hideElements, true);
-
-  await html2canvas(targetEl, {
+  html2canvas(targetEl, {
     useCORS: true
   })
     .then((canvas) => {
       canvas.toBlob((blob) => {
         const sizeInMB = (blob.size / (1024 * 1024)).toFixed(2);
         const imgData = canvas.toDataURL('image/png', 0.8); // 转换 canvas 为 PNG 格式的数据 URL
-
         // 这个图片是用来预览的
         const imgNode = $tw.utils.domMaker('img', {
           class: 'max-w-3xl',
@@ -99,6 +102,6 @@ module.exports = async function exportPng(title, customSelector) {
       });
     })
     .then(() => {
-      hideElementsWithSelectors(hideElements, false);
+      localStorage.setItem('tid2png', 'no');
     });
 };
