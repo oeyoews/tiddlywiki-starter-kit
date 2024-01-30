5cacf7722ff52a95789d391fb0959d5b1bf241d1
oeyoews <jyao4783@gmail.com>
2024-01-30 17:21:20 +0800

    fix: tc-tiddler-frame dynamic for tiddlers

--- plugins/oeyoews/tiddler-fullscreen/tiddlers/action.js
+++ plugins/oeyoews/tiddler-fullscreen/tiddlers/action.js
@@ -5,12 +5,16 @@ module-type: library
 
 \*/
 module.exports = function toggleElementFullscreen(title) {
+  const prefix = '$:/state/tiddler-fullscreen/';
   try {
     const target = document.querySelector(`[data-tiddler-title="${title}"]`);
-    target.requestFullscreen();
-    if (document.fullscreenElement === null) {
+    if (document.fullscreenElement !== target) {
+      target.requestFullscreen();
+      $tw.wiki.setText(prefix + title, 'text', '', 'yes');
     } else if (document.fullscreenElement === target) {
       document.exitFullscreen();
+      // NOTE: 如果使用esc 退出， 不会更新这个状态
+      $tw.wiki.setText(prefix + title, 'text', '', 'no');
     }
   } catch (e) {
     console.warn(e);
