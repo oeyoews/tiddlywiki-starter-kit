f1e51fc8540b1b1bfd6b6099d72176acaeef43b5
oeyoews <jyao4783@gmail.com>
2024-02-01 00:27:57 +0800

    update config

--- config/plugins.mjs
+++ config/plugins.mjs
@@ -34,6 +34,7 @@ const markdowPlugins = ['tiddlywiki/markdown', 'oeyoews/markdown-kit'];
 
 // oeyoews plugins
 const oeyoewsPlugins = [
+  'mermaid-tiny',
   'tiddlywiki-tid2png',
   'tiddler-fullscreen',
   'mermaid-widget',
