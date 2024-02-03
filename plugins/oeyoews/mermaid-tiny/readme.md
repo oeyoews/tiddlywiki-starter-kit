6f0f2eb3ca29dead74b2e464af8b0d2fe2bcd86d
oeyoews <jyao4783@gmail.com>
2024-02-03 12:22:02 +0800

    update

--- plugins/oeyoews/mermaid-tiny/readme.md
+++ plugins/oeyoews/mermaid-tiny/readme.md
@@ -2,4 +2,13 @@
 
 ## Motivation
 
+builtin tiny https://cdn.jsdelivr.net/npm/mermaid@11.0.0-alpha.4/dist/mermaid.tiny.min.js
+
+<!-- ```html
+<script type="module">
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11.0.0-alpha.4/+esm'
+</script>
+``` -->
+
+
 <!-- your plugin motivation, or why you write this plugin -->
---
+++ plugins/oeyoews/tiddlywiki-prettier/tiddlers/widget.js
@@ -0,0 +1,44 @@
+/*\
+title: $:/plugins/oeyoews/tiddlywiki-prettier/widget.js
+type: application/javascript
+module-type: widget
+
+tiddlywiki-prettier widget
+
+\*/
+const { widget: Widget } = require('$:/core/modules/widgets/widget.js');
+
+class ExampleWidget extends Widget {
+  constructor(parseTreeNode, options) {
+    super(parseTreeNode, options);
+  }
+
+  render(parent, nextSibling) {
+    if (!$tw.browser) return;
+
+    this.computeAttributes();
+    this.execute();
+
+    const ssr = this.document.isTiddlyWikiFakeDom;
+    if (ssr) return;
+
+    const createElement = $tw.utils.domMaker;
+
+    const btn = createElement('button', {
+      text: 'Click me',
+      class: 'rounded p-1'
+    });
+
+    const domNode = createElement('div', {
+      // text: 'example',
+      // class: 'underline font-bold',
+      children: [btn]
+    });
+
+    parent.insertBefore(domNode, nextSibling);
+    this.domNodes.push(domNode);
+  }
+}
+
+/** @description tiddlywiki-prettier widget */
+exports['widget-xKmqEelY6V'] = ExampleWidget;
