540e8feb354e9f9bb90c5baa350000b7ad307a07
oeyoews <jyao4783@gmail.com>
2024-03-02 16:32:45 +0800

    update

--- plugins/oeyoews/neotw-vue3/readme.md
+++ plugins/oeyoews/neotw-vue3/readme.md
@@ -9,7 +9,10 @@ const vue = require('$:/plugins/oeyoews/neotw-vue3/vue.global.prod.js')
 const { ref, createApp } = vue
 ```
 
-* 此插件使用 `vue.global.prod.js`, 所以不依赖于任何构建工具，所以无法使用组合式 (composition)API 的写法，只能使用选项式 (optional)API 的写法. 使用示例参照 $:/plugins/oeyoews/neotw-vue3/widget.js
+* 此插件使用 `vue.global.prod.js`, 所以不依赖于任何构建工具，所以无法使用组合式 (composition)API 的写法，只能使用选项式 (options)API.
+* 使用示例参照 $:/plugins/oeyoews/neotw-vue3/widget.js
+
+* 更多用法请参考 [Vue3 官方文档](https://cn.vuejs.org/guide/essentials/application.html)
 
 ## Motivation
 
--- plugins/oeyoews/neotw-vue3/tiddlers/components.js
+++ plugins/oeyoews/neotw-vue3/tiddlers/components.js
@@ -45,6 +45,8 @@ module.exports = {
       sidebarText
     };
   },
+
+  /** methods 是一些用来更改状态与触发更新的函数 它们可以在模板中作为事件处理器绑定*/
   methods: {
     toggleSidebar() {
       const statusTiddler = '$:/state/notebook-sidebar';
@@ -64,11 +66,19 @@ module.exports = {
     //   return window.$tw;
     // }
   },
-  // data() { },
+
+  /** data() 返回的属性将会成为响应式的状态 并且暴露在 `this` 上 */
+  // data() {
+  //   return {};
+  // },
+
+  // JSX
   // render(h) { },
 
-  // optional apis
-  // @see-also: https://cn.vuejs.org/api/options-lifecycle.html
+  /**
+   * @description: 一些生命周期函数
+   * @see: https://cn.vuejs.org/api/options-lifecycle.html
+   */
   beforeCreate() {
     // console.log('beforeCreate');
   },
