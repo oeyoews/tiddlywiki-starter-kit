d3bb8488395aed7cfa389f09d033ee5da2c43a95
oeyoews <jyao4783@gmail.com>
2024-03-07 19:05:01 +0800

    support drag icon

--- plugins/oeyoews/neotw-vue-todo/tiddlers/todo.vue
+++ plugins/oeyoews/neotw-vue-todo/tiddlers/todo.vue
@@ -55,6 +55,22 @@
           @dblclick="startEdit(element, element.id)"
           v-show="editingIndex !== element.id"
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="opacity-0 group-hover:opacity-100"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+          >
+            <g fill="none" stroke="currentColor" stroke-width="2">
+              <circle cx="8" cy="4" r="1" transform="rotate(90 8 4)" />
+              <circle cx="16" cy="4" r="1" transform="rotate(90 16 4)" />
+              <circle cx="8" cy="12" r="1" transform="rotate(90 8 12)" />
+              <circle cx="16" cy="12" r="1" transform="rotate(90 16 12)" />
+              <circle cx="8" cy="20" r="1" transform="rotate(90 8 20)" />
+              <circle cx="16" cy="20" r="1" transform="rotate(90 16 20)" />
+            </g>
+          </svg>
           <input
             type="checkbox"
             v-model="element.done"
