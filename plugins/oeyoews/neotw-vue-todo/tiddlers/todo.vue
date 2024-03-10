aa5a065458057b71057fa7786f1d2f3cb975b687
oeyoews <jyao4783@gmail.com>
2024-03-10 12:41:28 +0800

    feat: add transition for todo

--- plugins/oeyoews/neotw-vue-todo/tiddlers/todo.vue
+++ plugins/oeyoews/neotw-vue-todo/tiddlers/todo.vue
@@ -35,79 +35,82 @@
   </div>
 
   <!-- task list -->
-  <div
+  <TransitionGroup
     v-draggable="[todos, { animation: 150 }]"
+    tag="div"
     class="list-none my-4 hover:cursor-move todo-list"
   >
-    <div class="flex justify-between items-center group" v-for="item in todos">
-      <li class="truncate todo p-1" v-show="editingIndex !== item.id">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="opacity-0 group-hover:opacity-100"
-          width="1em"
-          height="1em"
-          viewBox="0 0 24 24"
+    <template v-for="item in todos">
+      <div class="flex justify-between items-center group">
+        <li class="truncate todo p-1" v-show="editingIndex !== item.id">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="opacity-0 group-hover:opacity-100"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+          >
+            <g fill="none" stroke="currentColor" stroke-width="2">
+              <circle cx="8" cy="4" r="1" transform="rotate(90 8 4)" />
+              <circle cx="16" cy="4" r="1" transform="rotate(90 16 4)" />
+              <circle cx="8" cy="12" r="1" transform="rotate(90 8 12)" />
+              <circle cx="16" cy="12" r="1" transform="rotate(90 16 12)" />
+              <circle cx="8" cy="20" r="1" transform="rotate(90 8 20)" />
+              <circle cx="16" cy="20" r="1" transform="rotate(90 16 20)" />
+            </g>
+          </svg>
+          <input
+            type="checkbox"
+            v-model="item.done"
+            class="mr-2 cursor-pointer toggle"
+            :id="item.id"
+          />
+          <label
+            :for="item.id"
+            class="cursor-pointer"
+            :class="{
+              ['line-through']: item.done,
+              'text-gray-400': item.done
+            }"
+            >{{ item.text }}</label
+          >
+        </li>
+        <li v-show="editingIndex === item.id" class="w-full">
+          <input
+            v-model="editingText"
+            @keyup.enter="finishEdit(item.id)"
+            @blur="cancelEdit(item.id)"
+            @keyup.escape="cancelEdit(item.id)"
+            class="w-full"
+          />
+          <div class="mt-1">
+            <button @click="cancelEdit(item.id)">
+              {{ t('todo.cancel') }}
+            </button>
+            <button @click="finishEdit(item.id)">
+              {{ t('todo.save') }}
+            </button>
+          </div>
+        </li>
+
+        <div
+          v-show="item.date && editingIndex !== item.id"
+          class="text-gray-400 text-sm mx-2"
         >
-          <g fill="none" stroke="currentColor" stroke-width="2">
-            <circle cx="8" cy="4" r="1" transform="rotate(90 8 4)" />
-            <circle cx="16" cy="4" r="1" transform="rotate(90 16 4)" />
-            <circle cx="8" cy="12" r="1" transform="rotate(90 8 12)" />
-            <circle cx="16" cy="12" r="1" transform="rotate(90 16 12)" />
-            <circle cx="8" cy="20" r="1" transform="rotate(90 8 20)" />
-            <circle cx="16" cy="20" r="1" transform="rotate(90 16 20)" />
-          </g>
-        </svg>
-        <input
-          type="checkbox"
-          v-model="item.done"
-          class="mr-2 cursor-pointer toggle"
-          :id="item.id"
-        />
-        <label
-          :for="item.id"
-          class="cursor-pointer"
-          :class="{
-            ['line-through']: item.done,
-            'text-gray-400': item.done
-          }"
-          >{{ item.text }}</label
-        >
-      </li>
-      <li v-show="editingIndex === item.id" class="w-full">
-        <input
-          v-model="editingText"
-          @keyup.enter="finishEdit(item.id)"
-          @blur="cancelEdit(item.id)"
-          @keyup.escape="cancelEdit(item.id)"
-          class="w-full"
-        />
-        <div class="mt-1">
-          <button @click="cancelEdit(item.id)">
-            {{ t('todo.cancel') }}
-          </button>
-          <button @click="finishEdit(item.id)">
-            {{ t('todo.save') }}
+          {{ item.date }}
+
+          <button @click="startEdit(item, item.id)">Edit</button>
+          <button
+            @click="removeTodo(item.id)"
+            :title="t('todo.removeTooltip', { msg: 'removeTooltip' })"
+            class="p-1 hover:text-red-400 rounded-full opacity-50 group-hover:opacity-100 transition-all"
+          >
+            del
           </button>
         </div>
-      </li>
-
-      <div
-        v-show="item.date && editingIndex !== item.id"
-        class="text-gray-400 text-sm mx-2"
-      >
-        {{ item.date }}
-
-        <button @click="startEdit(item, item.id)">Edit</button>
-        <button
-          @click="removeTodo(item.id)"
-          :title="t('todo.removeTooltip', { msg: 'removeTooltip' })"
-          class="p-1 hover:text-red-400 rounded-full opacity-0 group-hover:opacity-100 transition-all"
-        >
-          del
-        </button>
       </div>
-    </div>
-  </div>
+    </template>
+  </TransitionGroup>
 
   <!-- bottom -->
   <div class="flex items-center justify-end text-sm">
