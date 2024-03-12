f6b1291072e2a36e6985f9a9d4ca99106ccd661d
oeyoews <jyao4783@gmail.com>
2024-03-12 12:36:23 +0800

    update

--- plugins/oeyoews/vue-info/tiddlers/icons.js
+++ plugins/oeyoews/vue-info/tiddlers/icons.js
@@ -4,23 +4,14 @@ type: application/javascript
 module-type: library
 
 \*/
+
+const getTemplate = require('$:/plugins/oeyoews/neotw-vue3/getTemplate');
+
 const title = `<span class="ml-2 text-sm text-gray-600 dark:text-gray-400"> <slot /> </span>`;
 
-const version = `<svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="text-purple-500"
-      >
-        <path d="m12 14 4-4"></path>
-        <path d="M3.34 19a10 10 0 1 1 17.32 0"></path>
-      </svg>`;
+const version = getTemplate(
+  '$:/plugins/oeyoews/vue-info/templates/version.vue'
+);
 
 const info = `<svg
         xmlns="http://www.w3.org/2000/svg"
