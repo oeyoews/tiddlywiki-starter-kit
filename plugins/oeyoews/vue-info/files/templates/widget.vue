a9b6a633d3c47a32a7166d328bdd236964236a61
oeyoews <jyao4783@gmail.com>
2024-03-13 13:30:24 +0800

    update

--- plugins/oeyoews/vue-info/files/templates/widget.vue
+++ plugins/oeyoews/vue-info/files/templates/widget.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center space-x-4 flex-wrap gap-2 justify-center text-sm mb-4 scale-75" @click="gotoTiddler">
     <Text text="Version" color="purple" :number="version" :icon="iversion" data-title="version" />
 
-    <Text text="Update" color="red" :number="update" :icon="iinfo" />
+    <Text text="Update" color="red" :number="update" :icon="iinfo" data-title="update" />
 
     <Text text="Plugins" color="green" :number="plugins" :icon="iplugin" data-title="plugin" />
 
