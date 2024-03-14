f939f58b9c0e97dba7bd2ba70c8f6fb3dcbb223f
oeyoews <jyao4783@gmail.com>
2024-03-15 00:01:05 +0800

    update

--- plugins/oeyoews/vue-rss/files/templates/app.vue
+++ plugins/oeyoews/vue-rss/files/templates/app.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <div v-if="loading"> loading ...</div>
-    <article v-for="(item, index) in rssItems" :key="item">
+    <article v-for="(item, index) in rssItems" :key="item"
+      class="p-2 rounded border-solid border my-2 flex flex-col justify-between text-base dark:bg-[#2d333b] hover:shadow-sm bg-transparent border-[#d0d7de] dark:border-[#444c56] hover:outline outline-[#d0d7de] hover:outline-1 dark:outline-[#444c56]">
       <h2> {{index+1}}. {{item.title}}</h2>
       <small>
         <a :href="item.link">{{item.link}}</a>
