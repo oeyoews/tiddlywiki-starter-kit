120ea49f253726bac4f3e498655c4c6ee7871c2a
oeyoews <jyao4783@gmail.com>
2024-03-15 20:45:10 +0800

    update

--- plugins/oeyoews/vue-pomodoro/files/components/Button.js
+++ plugins/oeyoews/vue-pomodoro/files/components/Button.js
@@ -11,5 +11,5 @@ module.exports = {
       this.onClick();
     }
   },
-  template: `<button @click="handleClick" class="mx-1 p-1 bg-gray-100 dark:bg-gray-700"> <slot /> </button>`
+  template: `<button @click="handleClick" class="mx-1 p-1 bg-gray-200 dark:bg-gray-700"> <slot /> </button>`
 };
