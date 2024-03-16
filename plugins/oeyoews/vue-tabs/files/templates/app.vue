a10c0034762a04f1da42eb6e07d5bb0f7d7f38c2
oeyoews <jyao4783@gmail.com>
2024-03-16 18:16:59 +0800

    update ui

--- plugins/oeyoews/vue-tabs/files/templates/app.vue
+++ plugins/oeyoews/vue-tabs/files/templates/app.vue
@@ -2,14 +2,15 @@
   <!-- title -->
   <!-- border button -->
   <!-- click to close -->
+  <!-- double brackets -->
   <!-- update list -->
   <div class="lg:flex fixed top-0 overflow-auto backdrop-blur-md hidden" @click.prevent="closeTiddler">
     <TransitionGroup>
-      <button v-for="item in data" class="bg-gray-100 p-2 hover:bg-gray-300 transition-all group rounded-none shrink-0"
-        :key="item" :data-id="item">
+      <span v-for="item in data" class="bg-gray-100 p-2 hover:bg-gray-300 transition-all group rounded-none shrink-0"
+        :key="item">
         {{item}}
-        <span class="opacity-0 group-hover:opacity-100">x</span>
-      </button>
+        <button class="bg-transparent opacity-0 hover:text-red-500 group-hover:opacity-100" :data-id="item">x</button>
+      </span>
     </TransitionGroup>
   </div>
 </template>
