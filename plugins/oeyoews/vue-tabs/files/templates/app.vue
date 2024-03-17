0b987510736f2ae54478d0f22eb6790e65b4adf2
oeyoews <jyao4783@gmail.com>
2024-03-17 23:06:50 +0800

    update

--- plugins/oeyoews/vue-tabs/files/templates/app.vue
+++ plugins/oeyoews/vue-tabs/files/templates/app.vue
@@ -21,11 +21,7 @@
             {{ item }}
             <button title="close tiddler"
               class="rounded-sm hover:bg-gray-200 dark:hover:bg-gray-700 opacity-0 group-hover:opacity-100 transition-all p-0 m-0"
-              :data-close-title="item">
-              <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1em" viewBox="0 0 24 24">
-                <path fill="currentColor"
-                  d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z" />
-              </svg>
+              :data-close-title="item" v-html="icons.closeTiddler">
             </button>
           </div>
         </TransitionGroup>
--- plugins/oeyoews/vue-tabs/tiddlers/icons.js
+++ plugins/oeyoews/vue-tabs/tiddlers/icons.js
@@ -5,6 +5,10 @@ module-type: library
 
 \*/
 const icons = {
+  closeTiddler: `<svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1em" viewBox="0 0 24 24">
+                <path fill="currentColor"
+                  d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z" />
+              </svg>`,
   reverse: `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024">
           <path fill="currentColor"
             d="M384 96a32 32 0 0 1 64 0v786.752a32 32 0 0 1-54.592 22.656L95.936 608a32 32 0 0 1 0-45.312h.128a32 32 0 0 1 45.184 0L384 805.632zm192 45.248a32 32 0 0 1 54.592-22.592L928.064 416a32 32 0 0 1 0 45.312h-.128a32 32 0 0 1-45.184 0L640 218.496V928a32 32 0 1 1-64 0z" />
