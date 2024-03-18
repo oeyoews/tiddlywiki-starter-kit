92bcd20060d9e13b152a55a5995ad177568e959c
oeyoews <jyao4783@gmail.com>
2024-03-18 11:02:43 +0800

    update kanban style #232

--- plugins/oeyoews/vue-kanban/files/templates/app.vue
+++ plugins/oeyoews/vue-kanban/files/templates/app.vue
@@ -1,31 +1,26 @@
 <template>
-  <div class="flex-1 overflow-auto">
-    <main class="p-3 inline-flex">
-      <!-- card -->
-      <div class="ml-4 flex-shrink-0 flex flex-col w-96 bg-gray-100 rounded-md h-96">
-        <h2 class="flex-shrink-0 pt-3 pb-1 px-3 text-sm font-medium text-gray-900">No Status</h2>
+  <div class="flex relative flex-1 flex-col md:flex-row min-h-0 px-4 py-2 overflow-x-auto gap-2 ">
+    <div v-for="item in [1, 2,3]">
+      <div
+        class="mr-2 w-[350px] flex flex-col overflow-y-hidden rounded-md transition-all duration-75 border border-solid border-dimmed-100 dark:border-dimmed-700">
+        <!-- title -->
+        <div class="flex-col shrink-0 flex p-2">
+          Title here
+        </div>
         <!-- list -->
-        <div class="flex-1 overflow-y-auto min-h-0">
-          <div v-for="nostatus in state.nostatus" class="pt-1 pb-3 px-3">
-            <div> {{nostatus}} </div>
-            <div> {{nostatus}} </div>
-            <div> {{nostatus}} </div>
-            <div> {{nostatus}} </div>
-            <div> {{nostatus}} </div>
-            <div> {{nostatus}} </div>
+        <div class="flex flex-col py-2 px-4 grow relative h-[500px] mb-4 overflow-y-auto">
+          <div v-for="item in state.nostatus"
+            class="relative rounded-md border border-dimmed-100 border-solid shadow-sm overflow-visible mb-2 shrink-0 flex-col dark:border-dimmed-700">
+            <div class="pt-2 border-none border-dimmed-100 pb-3 rounded-sm px-4 dark:border-dimmed-700">
+              <div class="p-4">
+                {{item}}
+              </div>
+            </div>
           </div>
         </div>
+        <button class="flex py-2 w-full px-4 gap-4 h-10">add new item</button>
       </div>
-
-      <div class="ml-4 flex-shrink-0 flex flex-col w-96 bg-gray-100 rounded-md">
-        <h2 class="flex-shrink-0 pt-3 pb-1 px-3 text-sm font-medium text-gray-900">No Status</h2>
-        <!-- list -->
-        <div class="flex-1 overflow-y-auto min-h-0">
-          <div v-for="nostatus in state.nostatus" class="pt-1 pb-3 px-3">
-            {{nostatus}}
-          </div>
-        </div>
-      </div>
-    </main>
+    </div>
   </div>
+
 </template>
