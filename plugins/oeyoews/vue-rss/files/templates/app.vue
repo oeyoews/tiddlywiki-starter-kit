4e0d579233a46afd18142ed126b43b5eedc07d4a
oeyoews <jyao4783@gmail.com>
2024-03-19 11:46:03 +0800

    feat: support banner

--- plugins/oeyoews/vue-rss/files/templates/app.vue
+++ plugins/oeyoews/vue-rss/files/templates/app.vue
@@ -43,7 +43,9 @@
       <article v-for="(item, index) in paginatedItems" :key="item"
         class="p-2 rounded border-solid border my-4 flex flex-col justify-between text-base dark:bg-[#2d333b] hover:shadow-sm bg-transparent border-[#d0d7de] dark:border-[#444c56] hover:outline outline-[#d0d7de] hover:outline-1 dark:outline-[#444c56]">
         <h2 class="mb-2 text-base"> {{ order + index }}. {{item.title}}</h2>
+        <div v-html="item.img"></div>
         <div class="flex items-center gap-2 text-sm text-gray-400 mb-2">
+          <img :src="item.src" alt="" class="size-8 object-cover rounded-full">
           <a :href="item.link" target="_blank">
             Link
           </a>
