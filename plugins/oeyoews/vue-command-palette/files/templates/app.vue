1c0ecb27bf181d13e3c94de469998d121ce6f0eb
oeyoews <jyao4783@gmail.com>
2024-03-20 15:06:04 +0800

    updae

--- plugins/oeyoews/vue-command-palette/files/templates/app.vue
+++ plugins/oeyoews/vue-command-palette/files/templates/app.vue
@@ -1,16 +1,26 @@
 <template>
-  dmeo
-  {{commands}}
-  <!-- <CmdBar :groups="commands"> -->
-  <!-- <template #header>
-      <CmdBar.Input placeholder="search fo anything" />
-    </template> -->
-  <!-- <template #content>
-      <CmdBar.VirtualList :config="listConfig">
-        <template #default="{ command }">
-          {{ command.label }}
-        </template>
-      </CmdBar.VirtualList>
-    </template> -->
-  <!-- </CmdBar> -->
+  <div>
+    <div class="backdrop-blur-lg z-[9998] fixed inset-0 bg-black/20 transition-all cursor-pointer opacity-100">
+      <div
+        class="bg-white flex-col z-[9999] transform shadow-lg p-2 mt-4 fixed left-1/2 -translate-x-1/2 rounded transition-all dark:invert w-3/4 md:w-1/2 flex">
+      </div>
+      <Command theme="custom">
+        <Command.Input placeholder="Type a command or search..." />
+        <Command.List>
+          <!-- <Command.Empty>No results found.</Command.Empty> -->
+
+          <div class="flex flex-col p-2 bg-gray-200">
+            <Command.Group heading="Letters">
+              <Command.Item>a</Command.Item>
+              <Command.Item>b</Command.Item>
+              <Command.Separator />
+              <Command.Item>c</Command.Item>
+            </Command.Group>
+
+            <Command.Item>Apple</Command.Item>
+          </div>
+        </Command.List>
+      </Command>
+    </div>
+  </div>
 </template>
--- plugins/oeyoews/vue-command-palette/tiddlers/app.js
+++ plugins/oeyoews/vue-command-palette/tiddlers/app.js
@@ -11,47 +11,12 @@ const getTemplate = require('$:/plugins/oeyoews/neotw-vue3/getTemplate.js');
 
 const cmd = '$:/plugins/oeyoews/vue-command-palette/lib/cmd.min.js';
 
-const { CmdBar } = require(cmd);
+const { Command } = require(cmd);
 
 const app = () => {
   const component = {
     setup() {
-      const commands = [
-        {
-          key: 'people',
-          commands: [
-            {
-              id: '1',
-              label: 'Command 1'
-            },
-            {
-              id: '2',
-              label: 'Command 2'
-            },
-            {
-              id: '3',
-              label: 'Command 3'
-            }
-          ]
-        },
-        {
-          key: 'places',
-          commands: [
-            {
-              id: '4',
-              label: 'Command 4'
-            },
-            {
-              id: '5',
-              label: 'Command 5'
-            },
-            {
-              id: '6',
-              label: 'Command 6'
-            }
-          ]
-        }
-      ];
+      const commands = [];
 
       return { commands };
     },
@@ -63,7 +28,7 @@ const app = () => {
     ),
 
     components: {
-      CmdBar
+      Command
     }
   };
   return component;
