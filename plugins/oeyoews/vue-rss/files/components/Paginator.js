d284ffa78f01dedd10f80e38003b49596c733978
oeyoews <jyao4783@gmail.com>
2024-03-21 13:43:14 +0800

    update

--- plugins/oeyoews/vue-rss/files/components/Paginator.js
+++ plugins/oeyoews/vue-rss/files/components/Paginator.js
@@ -15,14 +15,18 @@ module.exports = {
     }
   },
   template: `<div class="flex justify-center items-center gap-2" v-if="pages > 1">
-        <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md mx-2"
+        <button class="size-8"
           @click="changePage(currentPage - 1)" :disabled="currentPage === 1"
-          :class="{ 'cursor-not-allowed': currentPage === 1 }">Previous</button>
-        <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md mx-2"
-          @click="changePage(currentPage + 1)" :disabled="currentPage >= pages"
-          :class="{ 'cursor-not-allowed': currentPage >= pages }">Next</button>
+          :class="{ 'cursor-not-allowed': currentPage === 1 }">
+<svg xmlns="http://www.w3.org/2000/svg" width="1.78em" height="1em" viewBox="0 0 16 9"><path fill="currentColor" d="M12.5 5h-9c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h9c.28 0 .5.22.5.5s-.22.5-.5.5"/><path fill="currentColor" d="M6 8.5a.47.47 0 0 1-.35-.15l-3.5-3.5c-.2-.2-.2-.51 0-.71L5.65.65c.2-.2.51-.2.71 0c.2.2.2.51 0 .71L3.21 4.51l3.15 3.15c.2.2.2.51 0 .71c-.1.1-.23.15-.35.15Z"/></svg>
+          </button>
         <div>
           {{ currentPage }} / {{ pages}}
         </div>
+        <button class="size-8"
+          @click="changePage(currentPage + 1)" :disabled="currentPage >= pages"
+          :class="{ 'cursor-not-allowed': currentPage >= pages }">
+<svg xmlns="http://www.w3.org/2000/svg" width="1.78em" height="1em" viewBox="0 0 16 9"><path fill="currentColor" d="M12.5 5h-9c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h9c.28 0 .5.22.5.5s-.22.5-.5.5"/><path fill="currentColor" d="M10 8.5a.47.47 0 0 1-.35-.15c-.2-.2-.2-.51 0-.71l3.15-3.15l-3.15-3.15c-.2-.2-.2-.51 0-.71c.2-.2.51-.2.71 0l3.5 3.5c.2.2.2.51 0 .71l-3.5 3.5c-.1.1-.23.15-.35.15Z"/></svg>
+          </button>
       </div>`
 };
