97113c4a3c04ad8d1d17f747d0fc8c7477f40dc5
oeyoews <jyao4783@gmail.com>
2024-04-03 14:24:19 +0800

    feat: add neotw-read-files

--- config/startup.mjs
+++ config/startup.mjs
@@ -6,12 +6,12 @@ export default function startup(option) {
   return [
     '--listen',
     'port=' + port,
-    'root-tiddler=$:/core/save/all-external-js',
+    // 'root-tiddler=$:/core/save/all-external-js',
     'use-browser-cache=yes',
     'anon-username=' + base.username,
     // 'username=' + config.username,
     // 'password=' + config.password
     // https://bramchen.github.io/tw5-docs/zh-Hans/#WebServer%20Parameter%3A%20path-prefix
-    // 'path-prefix=/myapp',
+    // 'path-prefix=/src',
   ];
 }
