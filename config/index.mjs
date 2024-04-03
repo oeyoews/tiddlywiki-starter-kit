2995e8aa0a90ca68f97f72e5ba1545ec34ab049e
oeyoews <jyao4783@gmail.com>
2024-04-03 10:52:53 +0800

    add startup config

--- config/index.mjs
+++ config/index.mjs
@@ -4,9 +4,11 @@ import themes from './themes.mjs';
 import devplugins from './dev-plugins.mjs';
 import base from './base.mjs';
 import info from './info.mjs';
+import startup from './startup.mjs';
 
 // 用户配置
 export default {
+  startup,
   ...base,
   plugins,
   build,
