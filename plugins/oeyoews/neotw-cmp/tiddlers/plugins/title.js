2aba83aa476cbfbf2a72560cd1dbf7b853bb1075
oeyoews <jyao4783@gmail.com>
2024-05-28 12:13:32 +0800

    refactor: add titlePlugin

---
+++ plugins/oeyoews/neotw-cmp/tiddlers/plugins/title.js
@@ -0,0 +1,95 @@
+/*\
+title: $:/plugins/oeyoews/neotw-cmp/plugins/title.js
+type: application/javascript
+module-type: widget
+
+\*/
+
+const { debounced } = require('./utils');
+const { closeCmp } = require('./utils');
+
+const minSearchLength = $tw.wiki.getTiddlerText('$:/config/Search/MinLength');
+
+function titlePlugin(domNode) {
+  return {
+    getSources({ query }) {
+      let items = [];
+
+      if (query.length >= Number(minSearchLength - 1)) {
+        items = $tw.wiki.filterTiddlers(`[!is[system]search[${query}]]`);
+        items = items.map((item) => $tw.wiki.getTiddler(item).fields);
+      }
+
+      const length = items.length;
+
+      return debounced([
+        {
+          sourceId: 'LocalTiddlers',
+          templates: {
+            header({ item, html }) {
+              // Dynamic
+              return searchResult(item, html, { length });
+            },
+            // footer(){},
+            item({ item, html }) {
+              return previewTiddlers(item, html);
+            },
+          },
+
+          // tiddler 点击跳转
+          onSelect(e) {
+            closeCmp(domNode);
+            e.item.title && new $tw.Story().navigateTiddler(e.item.title);
+          },
+
+          getItems() {
+            return items;
+            // return items.filter(({ title }) =>
+            //   title.toLowerCase().includes(query.toLowerCase()),
+            // );
+          },
+
+          // getItemUrl({ item }) {
+          //   return `#` + encodeURIComponent(item.title);
+          // },
+
+          // getItemInputValue({ item }) {
+          //   return item.title;
+          // },
+        },
+      ]);
+    },
+  };
+}
+
+const previewTiddlers = (item, html) => {
+  // if (!item) return html`no results`;
+
+  const image = html`<div class="aut-tiddler"></div>`;
+  const linkIcon = html`<div class="aut-arrow"></div>`;
+  const tooltip = `点击跳转到${item.title}`;
+  const title = html`<b>${item.title}</b>`;
+  // const text = html`<div>${item.text}</div>`;
+  // const text = html`<div>
+  //   ${$tw.wiki.renderText(
+  //     'text/html',
+  //     'text/markdown',
+  //     `<div> ${item.text} </div>`,
+  //   )}
+  // </div>`;
+
+  return html`<div class="flex justify-between items-center" title="${tooltip}">
+    <div class="flex items-center justify-left gap-2">${image}${title}</div>
+    <div>${linkIcon}</div>
+  </div>`;
+};
+
+const searchResult = (item, html, data) => {
+  return html`<footer
+    class="mb-1 text-sm flex justify-start text-gray-500 dark:text-gray-500"
+  >
+    共有${Number(data.length)}条搜索结果
+  </footer>`;
+};
+
+module.exports = titlePlugin;
--- plugins/oeyoews/neotw-cmp/tiddlers/widget.js
+++ plugins/oeyoews/neotw-cmp/tiddlers/widget.js
@@ -13,20 +13,6 @@ class AutoCompleteWidget extends Widget {
     super(parseTreeNode, options);
   }
 
-  debouncePromise(fn, time = 300) {
-    let timerId = undefined;
-
-    return function debounced(...args) {
-      if (timerId) {
-        clearTimeout(timerId);
-      }
-
-      return new Promise((resolve) => {
-        timerId = setTimeout(() => resolve(fn(...args)), time);
-      });
-    };
-  }
-
   render(parent, nextSibling) {
     if (!$tw.browser) return;
 
@@ -36,25 +22,14 @@ class AutoCompleteWidget extends Widget {
     const ssr = this.document.isTiddlyWikiFakeDom;
     if (ssr) return;
 
+    const titlePlugin = require('./plugins/title');
+    const { closeCmp, openCmp } = require('./plugins/utils');
+
     const createElement = $tw.utils.domMaker;
     const domNode = document.createElement('div');
 
-    const openCmp = () => {
-      domNode.style.display = '';
-      const input = window.document.querySelector('#autocomplete-0-input');
-      if (input) {
-        input.focus();
-      }
-      this.document.body.style.overflow = 'hidden';
-    };
-
-    const closeCmp = () => {
-      this.document.body.style.overflow = '';
-      domNode.style.display = 'none';
-    };
-
     // 注册事件
-    $tw.rootWidget.addEventListener('open-cmp', openCmp);
+    $tw.rootWidget.addEventListener('open-cmp', () => openCmp(domNode));
 
     const aut = require('./autocomplete');
 
@@ -72,106 +47,15 @@ class AutoCompleteWidget extends Widget {
 
     layout.appendChild(app);
 
-    const debounced = this.debouncePromise(
-      (items) => Promise.resolve(items),
-      300,
-    );
-
-    const minSearchLength = $tw.wiki.getTiddlerText(
-      '$:/config/Search/MinLength',
-    );
-
-    const previewTiddlers = (item, html) => {
-      // if (!item) return html`no results`;
-
-      const image = html`<div class="aut-tiddler"></div>`;
-      const linkIcon = html`<div class="aut-arrow"></div>`;
-      const tooltip = `点击跳转到${item.title}`;
-      const title = html`<b>${item.title}</b>`;
-      // const text = html`<div>${item.text}</div>`;
-      // const text = html`<div>
-      //   ${$tw.wiki.renderText(
-      //     'text/html',
-      //     'text/markdown',
-      //     `<div> ${item.text} </div>`,
-      //   )}
-      // </div>`;
-
-      return html`<div
-        class="flex justify-between items-center"
-        title="${tooltip}"
-      >
-        <div class="flex items-center justify-left gap-2">${image}${title}</div>
-        <div>${linkIcon}</div>
-      </div>`;
-    };
-
-    const searchResult = (item, html, data) => {
-      return html`<footer
-        class="mb-1 text-sm flex justify-start text-gray-500 dark:text-gray-500"
-      >
-        共有${Number(data.length)}条搜索结果
-      </footer>`;
-    };
-
     aut.autocomplete({
       container: app,
       placeholder: 'Search ...',
       autoFocus: true,
       openOnFocus: true,
       debug: false,
-      plugins: [],
+      plugins: [titlePlugin(domNode)],
       ignoreCompositionEvents: true, // IME friendly
       defaultActiveItemId: 0,
-      getSources({ query }) {
-        let items = [];
-
-        if (query.length < Number(minSearchLength - 1)) {
-          return [];
-        }
-
-        items = $tw.wiki.filterTiddlers(`[!is[system]search[${query}]]`);
-        items = items.map((item) => $tw.wiki.getTiddler(item).fields);
-
-        const length = items.length;
-
-        return debounced([
-          {
-            sourceId: 'LocalTiddlers',
-            templates: {
-              header({ item, html }) {
-                // Dynamic
-                return searchResult(item, html, { length });
-              },
-              // footer(){},
-              item({ item, html }) {
-                return previewTiddlers(item, html);
-              },
-            },
-
-            // tiddler 点击跳转
-            onSelect(e) {
-              closeCmp();
-              e.item.title && new $tw.Story().navigateTiddler(e.item.title);
-            },
-
-            getItems() {
-              return items;
-              // return items.filter(({ title }) =>
-              //   title.toLowerCase().includes(query.toLowerCase()),
-              // );
-            },
-
-            // getItemUrl({ item }) {
-            //   return `#` + encodeURIComponent(item.title);
-            // },
-
-            // getItemInputValue({ item }) {
-            //   return item.title;
-            // },
-          },
-        ]);
-      },
     });
 
     const masklayer = createElement('div', {
@@ -185,9 +69,9 @@ class AutoCompleteWidget extends Widget {
     parent.insertBefore(domNode, nextSibling);
     this.domNodes.push(domNode);
 
-    closeCmp();
+    closeCmp(domNode);
     masklayer.addEventListener('click', () => {
-      closeCmp();
+      closeCmp(domNode);
     });
   }
 }
