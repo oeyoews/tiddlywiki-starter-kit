0bbe528783c51e8a359de06bd455cde57db2036e
oeyoews <jyao4783@gmail.com>
2024-06-23 14:04:28 +0800

    feat: support avatar for qrcode

--- plugins/oeyoews/qrcode/tiddlers/widget.js
+++ plugins/oeyoews/qrcode/tiddlers/widget.js
@@ -13,11 +13,33 @@ qrcode widget
 const QRCode = require('qrcode.min.js');
 const { widget: Widget } = require('$:/core/modules/widgets/widget.js');
 
+/**
+ * @description The type of content to render in the QR code.
+ * @enum {'url' | 'raw' | 'rendered' | 'text'}
+ */
+const RenderType = {
+  /** @type {'url'} */
+  URL: 'url',
+  /** @type {'raw'} */
+  RAW: 'raw',
+  /** @type {'rendered'} */
+  RENDERED: 'rendered',
+  /** @type {'text'} */
+  TEXT: 'text',
+};
+
+/**
+ * @typedef {Object} QRCodeWidgetAttributes
+ * @property {string} [text] - The text to encode in the QR code.
+ * @property {number} [width=512] - The width of the QR code.
+ * @property {string} [avatarSrc] - The URL of the avatar image.
+ * @property {RenderType} renderType - The type of content to render in the QR code.
+ */
+
 class QRCodeWidget extends Widget {
   constructor(parseTreeNode, options) {
     super(parseTreeNode, options);
-    this.svgString = null;
-    this.type = 'image/svg+xml';
+    this.type = 'image/png';
   }
 
   render(parent, nextSibling) {
@@ -35,26 +57,24 @@ class QRCodeWidget extends Widget {
     const {
       renderType,
       text = href,
-      width = 512
-      // size
-    } = this.attributes;
+      width = 512,
+      avatarSrc, // 头像图片的 URL
+    } = /** @type {QRCodeWidgetAttributes} */ (this.attributes);
     let textcontent = text;
 
     if (storyTiddler) {
       switch (renderType) {
-        // 常用的是 url 类型
-        case 'url':
+        case RenderType.URL:
           textcontent = currentTiddlerUrl;
           break;
-        // rendered 类型除了支持了 wikitext 变量的渲染，其他的和 raw 没有什么区别，都是纯文本
-        case 'raw':
+        case RenderType.RAW:
           textcontent = $tw.wiki.getTiddlerText(storyTiddler);
           break;
         case 'rendered':
           textcontent = $tw.wiki.renderText(
             'text/plain-formatted',
             'text/vnd.tiddlywiki',
-            `${$tw.wiki.getTiddlerText(storyTiddler)}`
+            `${$tw.wiki.getTiddlerText(storyTiddler)}`,
           );
           break;
         case 'text':
@@ -68,27 +88,70 @@ class QRCodeWidget extends Widget {
     if (!textcontent) {
       return;
     }
-    QRCode.toString(
+
+    const canvas = document.createElement('canvas');
+    QRCode.toCanvas(
+      canvas,
       textcontent,
       {
-        type: 'terminal',
-        width
+        width,
       },
-      (_, svgString) => {
-        this.svgString = svgString;
-      }
+      (error) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+
+        if (avatarSrc) {
+          const context = canvas.getContext('2d');
+          const avatar = new Image();
+          avatar.crossOrigin = 'Anonymous'; // 允许跨域
+          avatar.src = avatarSrc;
+
+          avatar.onload = () => {
+            const avatarSize = width / 5; // 头像大小为二维码大小的1/5
+            const avatarX = (canvas.width - avatarSize) / 2;
+            const avatarY = (canvas.height - avatarSize) / 2;
+            // 创建圆形剪切路径
+            context.save();
+            context.beginPath();
+            context.arc(
+              avatarX + avatarSize / 2,
+              avatarY + avatarSize / 2,
+              avatarSize / 2,
+              0,
+              Math.PI * 2,
+              false,
+            );
+            context.clip();
+
+            // 绘制头像
+            context.drawImage(avatar, avatarX, avatarY, avatarSize, avatarSize);
+            context.restore();
+
+            const domNode = $tw.utils.domMaker('img', {
+              class: 'spotlight w-0 md:w-32',
+              attributes: {
+                src: canvas.toDataURL(this.type),
+              },
+            });
+
+            parent.insertBefore(domNode, nextSibling);
+            this.domNodes.push(domNode);
+          };
+        } else {
+          const domNode = $tw.utils.domMaker('img', {
+            class: 'spotlight w-0 md:w-32',
+            attributes: {
+              src: canvas.toDataURL(this.type),
+            },
+          });
+
+          parent.insertBefore(domNode, nextSibling);
+          this.domNodes.push(domNode);
+        }
+      },
     );
-
-    const svghashtext = encodeURIComponent(this.svgString);
-    const domNode = $tw.utils.domMaker('img', {
-      class: 'spotlight w-0 md:w-32',
-      attributes: {
-        src: `data:${this.type},${svghashtext}`
-      }
-    });
-
-    parent.insertBefore(domNode, nextSibling);
-    this.domNodes.push(domNode);
   }
 }
 
@@ -97,5 +160,6 @@ class QRCodeWidget extends Widget {
  * @param text
  * @param width
  * @param renderType
+ * @param avatarSrc 头像图片的 URL
  */
 exports.qrcode = QRCodeWidget;
