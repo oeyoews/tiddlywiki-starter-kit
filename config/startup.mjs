95c142025d3af105bcf3e9610c6b35fdcc8c3bc4
oeyoews <jyao4783@gmail.com>
2024-07-05 22:11:57 +0800

    fix: update tiddlywiki version

--- config/startup.mjs
+++ config/startup.mjs
@@ -9,8 +9,6 @@ import base from './base.mjs';
 export default function startup(option) {
   const { port } = option;
 
-  // 'username=' + config.username,
-  // 'password=' + config.password
   // 'path-prefix=/src', // https://bramchen.github.io/tw5-docs/zh-Hans/#WebServer%20Parameter%3A%20path-prefix
   return [
     '--listen',
@@ -18,5 +16,7 @@ export default function startup(option) {
     'root-tiddler=$:/core/save/all-external-js',
     'use-browser-cache=yes',
     'anon-username=' + base.username,
+    `username=${base.username}`,
+    `password=${base.password}`,
   ];
 }
