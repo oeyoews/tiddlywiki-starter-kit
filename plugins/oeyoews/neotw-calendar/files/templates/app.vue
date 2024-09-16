e868742f042c01a6a731006ad49dd803b4aa4c36
oeyoews <jyao4783@gmail.com>
2024-09-16 16:54:31 +0800

    archive: some unused plugins

--- plugins/oeyoews/neotw-calendar/files/templates/app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <vue-cal class="vuecal--date-picker" xsmall hide-view-selector :time="false" :transitions="false" active-view="month"
-    :disable-views="['week']" style="width: 210px;height: 230px" local="zh-cn">
-  </vue-cal>
-</template>
