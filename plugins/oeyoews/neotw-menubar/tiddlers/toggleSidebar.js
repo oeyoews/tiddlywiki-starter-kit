0c424e70954deed402af9ae24e81fed86622d5b5
oeyoews <jyao4783@gmail.com>
2024-09-30 20:42:06 +0800

    feat: sidebar support neotw-play-sound event

--- plugins/oeyoews/neotw-menubar/tiddlers/toggleSidebar.js
+++ plugins/oeyoews/neotw-menubar/tiddlers/toggleSidebar.js
@@ -8,6 +8,10 @@ module.exports = () => {
   const opt = {
     suppressTimestamp: true,
   };
+  // 触发切换侧边栏声音
+  $tw.rootWidget.dispatchEvent({
+    type: 'neotw-play-sound',
+  });
   if ($tw.wiki.getTiddlerText(`$:/state/notebook-sidebar`) == 'yes') {
     $tw.wiki.setText(`$:/state/notebook-sidebar`, 'text', null, 'no', opt);
   } else {
