088eac63bae28e5a2366d965390a48fe1f9fcd8f
oeyoews <jyao4783@gmail.com>
2024-10-01 23:07:43 +0800

    feat: support hooks for play-sound

--- plugins/oeyoews/neotw-play-sound/tiddlers/startup.js
+++ plugins/oeyoews/neotw-play-sound/tiddlers/startup.js
@@ -31,7 +31,7 @@ function debounce(fn, delay, immediate = false) {
 let currentAudio = null;
 function playSound(event) {
   const pluginTitle = '$:/plugins/oeyoews/neotw-play-sound/';
-  const { audioTiddler } = event.paramObject || {
+  const { audioTiddler } = event?.paramObject || {
     audioTiddler: pluginTitle + 'menu-open.mp3',
   };
 
@@ -47,8 +47,36 @@ function playSound(event) {
 
 const debouncePlaySound = debounce(playSound, 200, true);
 
+exports.platforms = ['browser'];
+exports.synchronous = true;
+exports.after = ['startup'];
+
 exports.startup = function () {
   $tw.rootWidget.addEventListener('neotw-play-sound', (event) => {
     debouncePlaySound(event);
+  });
+
+  const hooks = $tw.wiki.getTiddler(
+    '$:/config/eyoews/neotw-play-sound/config',
+  ).fields;
+  const enableHooks = [];
+
+  Object.entries(hooks).forEach((hook) => {
+    if (hook[0].startsWith('t') && hook[1] === 'yes') {
+      enableHooks.push(hook[0]);
+      console.log(hook);
+    }
+  });
+
+  // 为了性能，修改配置后需要重启, 不在这里每次重新检测值的变化
+  enableHooks.forEach((hook) => {
+    $tw.hooks.addHook(hook, function (event) {
+      playSound({
+        paramObject: {
+          audioTiddler: '$:/plugins/oeyoews/neotw-play-sound/sounds/bite.mp3',
+        },
+      });
+      return event;
+    });
   });
 };
