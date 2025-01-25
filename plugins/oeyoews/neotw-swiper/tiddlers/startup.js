e106de0dbb8bd2517ae83bfd139a3712bcfa22c7
oeyoews <jyao4783@gmail.com>
2025-01-25 15:44:06 +0800

    plugin: add clean storylist plugin

--- plugins/oeyoews/neotw-swiper/tiddlers/startup.js
+++ plugins/oeyoews/neotw-swiper/tiddlers/startup.js
@@ -9,7 +9,5 @@ exports.platforms = ['browser'];
 exports.after = ['startup'];
 exports.synchronous = true;
 exports.startup = () => {
-  const d = require('./swiper-element-bundle.min.js');
-  window.d = d;
-  console.log('加载');
+  require('./swiper-element-bundle.min.js');
 };
