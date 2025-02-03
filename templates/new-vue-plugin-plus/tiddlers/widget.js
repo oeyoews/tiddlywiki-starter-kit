bfc5c6cacdc396204b41e790f4e91cfdb32759e6
oeyoews <jyao4783@gmail.com>
2025-02-03 11:39:58 +0800

    feat: add vue-plus plugin template

--- templates/new-vue-plugin-plus/tiddlers/widget.js
+++ templates/new-vue-plugin-plus/tiddlers/widget.js
@@ -31,10 +31,12 @@ class ExampleWidget extends Widget {
     const { createApp } = window.Vue;
     const component = require('./app');
     const domNode = this.document.createElement('div');
+    const TiddlyWikiVue = require('./plugins/TiddlyWikiVue');
 
     try {
       /** @type {{ use: Function, component: (string, Object) }} */
       const app = createApp(component());
+      app.use(TiddlyWikiVue);
 
       app.config.errorHandler = (err) => {
         const text = `[Vue3](${app.version}): ` + err.message;
