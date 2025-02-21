b1bbc2df31b92074c4967904b18d2a5b38f7e97b
oeyoews <jyao4783@gmail.com>
2025-02-21 16:23:39 +0800

    feat: support recursion for importer

--- plugins/oeyoews/markdown-importer/tiddlers/importer.js
+++ plugins/oeyoews/markdown-importer/tiddlers/importer.js
@@ -16,29 +16,29 @@ function parseFrontMatter(content) {
   if (!content.startsWith('---')) return content;
   return content.replace(/^---\n[\s\S]*?\n---\n*/, '');
 }
+async function readMarkdownFolder(dirHandle = null) {
+  if (!dirHandle) {
+    dirHandle = await window.showDirectoryPicker();
+  }
 
-async function readMarkdownFolder() {
-  // 显示文件夹选择器
-  const dirHandle = await window.showDirectoryPicker();
   const mdFiles = [];
 
-  // 遍历目录
-  // TODO：考虑命名重复弹窗提示
-  // TODO: 进度条提示， 数量提示
   for await (const entry of dirHandle.values()) {
     if (entry.kind === 'file' && entry.name.endsWith('.md')) {
       const file = await entry.getFile();
-      let text = await file.text(); // 读取文件内容
+      let text = await file.text();
       text = parseFrontMatter(text);
-      const modified = new Date(file.lastModified); // 转换为日期对象
+      const modified = new Date(file.lastModified);
 
-      let fields = {
+      mdFiles.push({
         title: file.name.slice(0, -3),
         text,
         modified,
-      };
-
-      mdFiles.push(fields);
+      });
+    } else if (entry.kind === 'directory') {
+      // 递归遍历子目录
+      const subFiles = await readMarkdownFolder(entry);
+      mdFiles.push(...subFiles);
     }
   }
 
