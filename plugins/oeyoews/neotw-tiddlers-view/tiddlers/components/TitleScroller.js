cf30f7901a0160e40a6561729bab805dcc2163a6
oeyoews <jyao4783@gmail.com>
2025-02-26 18:56:06 +0800

    style: update css

--- plugins/oeyoews/neotw-tiddlers-view/tiddlers/components/TitleScroller.js
+++ plugins/oeyoews/neotw-tiddlers-view/tiddlers/components/TitleScroller.js
@@ -9,16 +9,12 @@ Title scroller component
 module.exports = {
   name: 'TitleScroller',
 
-  template: `
-    <div class="title-scroller">
-      <div class="scroll-container">
+  template: `<div class="scroll-container">
         <transition name="scroll" mode="out-in">
           <div v-if="currentTitle" :key="currentTitle" class="title-item" @click="goToTiddler(currentTitle)">
             {{ currentTitle.slice(0, 50) }}
           </div>
-        </transition>
-      </div>
-    </div>`,
+        </transition></div>`,
 
   props: {
     filter: {
