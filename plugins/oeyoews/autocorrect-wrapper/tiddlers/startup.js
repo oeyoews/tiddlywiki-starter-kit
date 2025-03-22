c3cb9d96f31561f28699f9eea8ebd113247dad76
oeyoews <jyao4783@gmail.com>
2025-03-22 21:02:18 +0800

    feat: add autocorrect-wrapper

---
+++ plugins/oeyoews/autocorrect-wrapper/tiddlers/startup.js
@@ -0,0 +1,20 @@
+/*\
+title: $:/plugins/oeyoews/autocorrect-wrapper/startup.js
+type: application/javascript
+module-type: startup
+
+autocorrect-wrapper startup
+
+\*/
+
+exports.name = 'autocorrect-wrapper-startup-hook';
+exports.platforms = ['browser'];
+exports.after = ['startup'];
+exports.synchronous = true;
+
+exports.startup = () => {
+  const action = require('./action');
+  $tw.rootWidget.addEventListener('om-tiddler-format', (event) => {
+    action(event?.paramObject?.title || event.tiddlerTitle);
+  });
+};
