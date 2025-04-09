3979acdce430b2ba59ede86c46874d03ab6e65d6
oeyoews <jyao4783@gmail.com>
2025-04-09 19:33:04 +0800

    feat: add Pacifico font package

--- config/index.mjs
+++ config/index.mjs
@@ -1,10 +1,10 @@
-import build from './build.mjs';
-import plugins from './plugins.mjs';
-import themes from './themes.mjs';
-import devplugins from './dev-plugins.mjs';
 import base from './base.mjs';
+import build from './build.mjs';
+import devplugins from './dev-plugins.mjs';
 import info from './info.mjs';
+import plugins from './plugins.mjs';
 import startup from './startup.mjs';
+import themes from './themes.mjs';
 
 /** 用户配置 */
 export default {
